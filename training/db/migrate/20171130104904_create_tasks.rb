8c83d02ccaeb21d4ae8cd60e85f755a0e864b441
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-11-30 20:51:23 +0900

    登録のみ可能なタスクモデルとmigrationを作成

---
+++ training/db/migrate/20171130104904_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name, null: false
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
