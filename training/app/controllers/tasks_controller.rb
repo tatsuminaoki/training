63cb7d13dd03dec6eaa669c56475ed32995de8b3
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-02 19:10:52 +0900

    タスクの作成機能を実装

--- training/app/controllers/tasks_controller.rb
+++ training/app/controllers/tasks_controller.rb
@@ -2,4 +2,25 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new()
+  end
+
+  def create
+    @task = Task.create_task(task_params)
+    if @task.present?
+      redirect_to @task, notice: '作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(
+      :name, :description
+    )
+  end
 end
--- training/app/models/task.rb
+++ training/app/models/task.rb
@@ -1,7 +1,9 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
 
-  def self.create_task(name, description = nil)
-    create(name: name, description: description)
+  def self.create_task(params)
+    create(name: params[:name],
+           description: params[:description]
+          )
   end
 end
