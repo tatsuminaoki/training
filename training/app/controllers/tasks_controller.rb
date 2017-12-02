433f62afff6c7cacd1bce8d25efcb9935e87cb65
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-02 19:57:40 +0900

    詳細画面の実装

--- training/app/controllers/tasks_controller.rb
+++ training/app/controllers/tasks_controller.rb
@@ -3,6 +3,16 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  # todo page移動ボタンの実装
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    # todo 編集画面の実装
+  end
+
   def new
     @task = Task.new()
   end
@@ -11,8 +21,10 @@ class TasksController < ApplicationController
     @task = Task.create_task(task_params)
     if @task.present?
       redirect_to @task, notice: '作成しました'
+      # todo flashの実装
     else
       render :new
+      # todo 異常系のテスト
     end
   end
 
