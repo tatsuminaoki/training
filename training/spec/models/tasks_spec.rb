1b3e258ef5ae413744491b66eccd8a5e7741b7ec
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-04 21:39:17 +0900

    Taskモデルのテストを作成

---
+++ training/spec/models/tasks_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  describe 'validation' do
+    describe 'name' do
+      let(:task) { FactoryBot.create(:task, name: name) }
+      let!(:task2) { Task.create!(name: 'hoge', description: 'aaa') }
+      subject { task.valid? }
+
+      context '入力が正しい場合' do
+        let(:name) { 'test' }
+        it { is_expected.to be true }
+      end
+
+      context '空欄の場合' do
+        let(:name) { '' }
+        it { is_expected.to be false }
+      end
+
+      context '長さが255以上の場合' do
+        let(:name) { 'a' * 256 }
+        it { is_expected.to be false }
+      end
+    end
+  end
+end
