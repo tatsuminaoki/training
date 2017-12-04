ae55c20605dd992ce2030bf9643e6508d0b78341
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-04 22:05:08 +0900

    コントローラーテストを追記（必要なgemを追加）

--- training/spec/controllers/tasks_controller_spec.rb
+++ training/spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
+  describe 'GET #index' do
+    it '@tasks にタスク情報を持っている' do
+      task = FactoryBot.create(:task)
+      get :index
+      expect(assigns(:tasks)[0]).to eq task
+    end
 
+    it 'index テンプレートを表示する' do
+      get 'index'
+      expect(response).to render_template :index
+    end
+  end
 end
