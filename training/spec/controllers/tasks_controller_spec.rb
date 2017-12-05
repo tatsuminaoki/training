54ab130ecde7910cd9a27d09cfbfdad0bb2e81f3
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-06 07:35:25 +0900

    コントローラーのテストを作成

--- training/spec/controllers/tasks_controller_spec.rb
+++ training/spec/controllers/tasks_controller_spec.rb
@@ -2,15 +2,120 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
   describe 'GET #index' do
+    let!(:task) { FactoryBot.create(:task) }
     it '@tasks にタスク情報を持っている' do
-      task = FactoryBot.create(:task)
       get :index
       expect(assigns(:tasks)[0]).to eq task
     end
 
     it 'index テンプレートを表示する' do
-      get 'index'
+      get :index
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:task) { FactoryBot.create(:task) }
+    let(:params) { {id: task.id} }
+
+    it '@task にタスク情報を持っている' do
+      get :show, params: params
+      expect(assigns(:task)).to eq task
+    end
+
+    it 'show テンプレートを表示する' do
+      get :show, params: params
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #edit' do
+    let(:task) { FactoryBot.create(:task) }
+    let(:params) { {id: task.id} }
+
+    it '@task にタスク情報を持っている' do
+      get :edit, params: params
+      expect(assigns(:task)).to eq task
+    end
+
+    it 'edit テンプレートを表示する' do
+      get :edit, params: params
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'GET #new' do
+    it 'new テンプレートを表示する' do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'データが正しい場合' do
+      let(:params) { {task: FactoryBot.attributes_for(:task)} }
+
+      it 'タスクが新たに作成される' do
+        expect{
+          post :create, params: params
+        }.to change(Task, :count).by(1)
+      end
+    end
+
+    context 'データが不正な場合' do
+      let(:params) { {task: FactoryBot.attributes_for(:task, name: '')} }
+
+      it 'タスクは作成されない' do
+        expect{
+          post :create, params: params
+        }.not_to change(Task, :count)
+      end
+
+      it 'newテンプレートを表示する' do
+        get :create, params: params
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'POST #update' do
+    let(:task) { FactoryBot.create(:task) }
+    let(:params) { {task: FactoryBot.attributes_for(:task, name: name), id: task.id } }
+
+    context 'データが正しい場合' do
+      let(:name) {'hoge'}
+
+      it 'タスクが更新される' do
+        expect(Task.find(task.id)).to eq task
+        patch :update, params: params
+        expect(Task.find(task.id).name).to eq name
+      end
+    end
+
+    context 'データが不正な場合' do
+      let(:name) {''}
+
+      it 'タスクは更新されない' do
+        expect(Task.find(task.id)).to eq task
+        patch :update, params: params
+        expect(Task.find(task.id).name).not_to eq name
+      end
+
+      it 'editテンプレートを表示する' do
+        put :update, params: params
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:task) { FactoryBot.create(:task) }
+    let(:params) { {id: task.id} }
+
+    it 'タスクを削除する' do
+      expect(Task.find(task.id)).to eq task
+      delete :destroy, params: params
+      expect{ Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
