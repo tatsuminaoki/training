0a672df1b0bed2e95e8307594e7532794865fde8
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-08 21:03:51 +0900

    バリデーションの設定とテストの作成

--- training/app/models/task.rb
+++ training/app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
+  validates :user_id, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :priority, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :status, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :label_id, :numericality => { :greater_than_or_equal_to => 0, :allow_blank => true }
 end
--- training/spec/models/tasks_spec.rb
+++ training/spec/models/tasks_spec.rb
@@ -33,5 +33,139 @@ RSpec.describe Task, type: :model do
         end
       end
     end
+
+    describe 'user_id' do
+      let(:task) { FactoryBot.build(:task, user_id: user_id) }
+      subject { task.valid? }
+
+      # TODO : User機能実装時にIDが存在することを検証する
+      context '入力が正しい場合' do
+        let(:user_id) { 1 }
+        it { is_expected.to be true }
+      end
+
+      context '空欄の場合' do
+        let(:user_id) { '' }
+        it { is_expected.to be false }
+      end
+
+      context '数値でない場合' do
+        let(:user_id) { 'abc' }
+        it { is_expected.to be false }
+      end
+
+      context '負の数値の場合' do
+        let(:user_id) { -1 }
+        it { is_expected.to be false }
+      end
+    end
+
+    describe 'priority' do
+      let(:task) { FactoryBot.build(:task, priority: priority) }
+      subject { task.valid? }
+
+      context '入力が正しい場合' do
+        let(:priority) { 1 }
+        it { is_expected.to be true }
+      end
+
+      context '空欄の場合' do
+        let(:priority) { '' }
+        it { is_expected.to be false }
+      end
+
+      context '数値でない場合' do
+        let(:priority) { 'abc' }
+        it { is_expected.to be false }
+      end
+
+      context '負の数値の場合' do
+        let(:priority) { -1 }
+        it { is_expected.to be false }
+      end
+    end
+
+    describe 'status' do
+      let(:task) { FactoryBot.build(:task, status: status) }
+      subject { task.valid? }
+
+      context '入力が正しい場合' do
+        let(:status) { 1 }
+        it { is_expected.to be true }
+      end
+
+      context '空欄の場合' do
+        let(:status) { '' }
+        it { is_expected.to be false }
+      end
+
+      context '数値でない場合' do
+        let(:status) { 'abc' }
+        it { is_expected.to be false }
+      end
+
+      context '負の数値の場合' do
+        let(:status) { -1 }
+        it { is_expected.to be false }
+      end
+    end
+
+    describe 'label_id' do
+      let(:task) { FactoryBot.build(:task, label_id: label_id) }
+      subject { task.valid? }
+
+      context '入力が正しい場合' do
+
+        # TODO : Label機能実装時にIDが存在することを検証する
+        context '数値が設定されている場合' do
+          let(:label_id) { 1 }
+          it { is_expected.to be true }
+        end
+
+        context '空欄の場合' do
+          let(:label_id) { '' }
+          it { is_expected.to be true }
+        end
+      end
+
+      context '数値でない場合' do
+        let(:label_id) { 'abc' }
+        it { is_expected.to be false }
+      end
+
+      context '負の数値の場合' do
+        let(:label_id) { -1 }
+        it { is_expected.to be false }
+      end
+    end
+
+    describe 'end_data' do
+      let(:task) { FactoryBot.build(:task, end_data: end_data) }
+      subject { task.valid? }
+
+      context '入力が正しい場合' do
+        context '日付が設定されている場合' do
+          let(:end_data) { Time.now }
+          it { is_expected.to be true }
+        end
+
+        context '空欄の場合' do
+          let(:end_data) { '' }
+          it { is_expected.to be true }
+        end
+      end
+
+      context '日付でない場合' do
+        context '数値の場合' do
+          let(:end_data) { 123 }
+          it { is_expected.to be false }
+        end
+
+        context '文字列の場合' do
+          let(:end_data) { 'abc' }
+          it { is_expected.to be false }
+        end
+      end
+    end
   end
 end
