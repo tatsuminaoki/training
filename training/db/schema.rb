c0f14ca729196cde655e92fd4c9ab317bc18e10a
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-08 15:56:21 +0900

    Taskテーブルにカラムを追加

--- training/db/schema.rb
+++ training/db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130104904) do
+ActiveRecord::Schema.define(version: 20171208052520) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id", null: false, unsigned: true
     t.string "name", null: false
     t.text "description"
+    t.integer "priority", limit: 1, null: false, unsigned: true
+    t.integer "status", limit: 1, null: false, unsigned: true
+    t.integer "label_id", unsigned: true
+    t.date "end_data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
