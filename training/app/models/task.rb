89854359dad81a421f774314be23fa8d9ca7c086
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-12 12:18:58 +0900

    ハッシュロケット記法からシンボル記法に統一

--- training/app/models/task.rb
+++ training/app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
-  validates :user_id, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
-  validates :priority, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
-  validates :status, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
-  validates :label_id, :numericality => { :greater_than_or_equal_to => 0, :allow_blank => true }
+  validates :user_id, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :priority, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :status, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :label_id, numericality: { greater_than_or_equal_to: 0, allow_blank: true }
   validate :end_date_valid
 
   private
