baed5631fbdacd00514720b2c706d9b1c7765bb1
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-14 19:08:34 +0900

    マイグレーションファイルを追加

--- training/db/schema.rb
+++ training/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208052520) do
+ActiveRecord::Schema.define(version: 20171214095443) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id", null: false, unsigned: true
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20171208052520) do
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
