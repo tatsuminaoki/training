ad7096f2a248354ffd0ff9f3a8a85f90100f0615
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-22 20:02:01 +0900

    管理画面を管理ユーザーのみ利用できる様に機能追加

--- training/app/controllers/application_controller.rb
+++ training/app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include LoginHelper
 
+  def require_admin_role
+    redirect_to root_path, notice: I18n.t('application.controller.messages.role_error') unless admin_role?
+  end
+
   private def require_login
     redirect_to logins_new_path unless logged_in?
   end
