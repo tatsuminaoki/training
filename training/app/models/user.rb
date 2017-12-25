dff2c7f059eaa9d98b3fafc589e2d7610ecfde4c
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-25 17:51:02 +0900

    ユーザーロールに関するバグを発見したので修正

--- training/app/models/user.rb
+++ training/app/models/user.rb
@@ -12,4 +12,16 @@ class User < ApplicationRecord
 
   enum role: { normal: 0, admin: 1 }
   validates :role, presence: true, inclusion: { in: User.roles.keys }
+  validate :change_role_valid?
+
+  private def change_role_valid?
+    return true if User.admin.count > 1
+    return true if self.admin?
+
+    if User.admin.first.id == id
+      errors.add(:role, I18n.t('errors.messages.role.require_least_one'))
+      return false
+    end
+    true
+  end
 end
