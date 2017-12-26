9207e085d4a79b570e4e0d75888569522675bea2
kaede-kuroda <kaede_kuroda@fablic.co.jp>
2017-12-26 21:21:05 +0900

    エラーページを実装

--- training/app/controllers/application_controller.rb
+++ training/app/controllers/application_controller.rb
@@ -1,13 +1,34 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+  skip_before_action :require_login, only: [:routing_error]
   protect_from_forgery with: :exception
   include LoginHelper
+
+  rescue_from Exception,                        with: :_render_500
+  rescue_from ActiveRecord::RecordNotFound,     with: :_render_404
+  rescue_from ActionController::RoutingError,   with: :_render_404
+
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
 
   def require_admin_role
     redirect_to root_path, notice: I18n.t('application.controller.messages.role_error') unless admin_role?
   end
 
-  private def require_login
+  private
+
+  def require_login
     redirect_to logins_new_path unless logged_in?
+  end
+
+  def _render_404(e = nil)
+    Rails.logger.error "404 error: #{e.message}" if e
+    render template: 'errors/error_404', status: 404
+  end
+
+  def _render_500(e = nil)
+    Rails.logger.error "500 error: #{e.message}" if e
+    render template: 'errors/error_500', status: 500
   end
 end
--- training/config/routes.rb
+++ training/config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   get 'logins/new'
   post 'logins/login'
   delete 'logins/logout'
+
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
 end
