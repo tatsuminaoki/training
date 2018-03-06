666a1dd1880ce9e946ba664dce237e538d22e796
ShoheiMitani <mitani49@gmail.com>
2018-03-06 14:10:59 +0900

    Add create action in task registration page

--- todo_app/app/controllers/tasks_controller.rb
+++ todo_app/app/controllers/tasks_controller.rb
@@ -1,8 +1,25 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
+
   def new
     @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to action: 'index'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description, :deadline, :status, :priority)
   end
 end
