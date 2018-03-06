5524051374fd6d5131f10184059e3cbc28cd496c
ShoheiMitani <mitani49@gmail.com>
2018-03-06 18:23:01 +0900

    Add settings of Capybara for feature test

--- todo_app/spec/spec_helper.rb
+++ todo_app/spec/spec_helper.rb
@@ -13,7 +13,18 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/rspec'
+require 'selenium-webdriver'
+
+Capybara.configure do |config|
+  config.run_server = false
+  config.default_driver = :selenium
+  config.default_max_wait_time = 10
+  config.app_host = 'http://0.0.0.0:3000/' # localhost(rails s)
+end
+
 RSpec.configure do |config|
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -44,6 +55,7 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  config.include Capybara::DSL
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
@@ -93,4 +105,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
 end
