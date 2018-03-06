afd2059f35784de0570c8a3c10e8db685400405b
ShoheiMitani <mitani49@gmail.com>
2018-03-06 20:26:44 +0900

    Create helper class in order to return button name.

--- todo_app/app/helpers/tasks_helper.rb
+++ todo_app/app/helpers/tasks_helper.rb
@@ -1,2 +1,11 @@
 module TasksHelper
+  def submit_btn_name
+    puts request.path_info
+    puts request.url
+    if request.path_info == new_task_path
+      return '登録'
+    else
+      return '更新'
+    end
+  end
 end
