1055172d1e918e1b4e3965dd1c143f5446d677d5
ShoheiMitani <mitani49@gmail.com>
2018-03-06 14:44:51 +0900

    Create task edit page and form template page.

--- todo_app/app/controllers/tasks_controller.rb
+++ todo_app/app/controllers/tasks_controller.rb
@@ -18,6 +18,20 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to action: 'index'
+    else
+      render 'edit'
+    end
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :description, :deadline, :status, :priority)
