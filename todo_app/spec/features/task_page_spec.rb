e195dabd901732fa50f779aa9cb2a47469c02fa3
ShoheiMitani <mitani49@gmail.com>
2018-03-08 14:23:26 +0900

    Add top page feature test

--- todo_app/spec/features/task_page_spec.rb
+++ todo_app/spec/features/task_page_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe 'タスク一覧画面を表示する', type: :feature do
-  before { create(:task, title: "Rspec test 1" ) }
-  after do
-    task = Task.find_by(title: "Rspec test 1")
-    task.destroy
-  end
+  before { create(:task, title: 'Rspec test 1') }
 
   it '表示確認' do
     visit '/'
@@ -20,72 +16,94 @@ describe 'タスク一覧画面を表示する', type: :feature do
 
   it '登録した1件のタスクがテーブルに表示されていること' do
     visit '/'
-    expect(page).to have_css('table#task_table tbody tr', :count=>1)
+    expect(page).to have_css('table#task_table tbody tr', count: 1)
   end
 
   it 'タイトル列に参照画面のリンク表示' do
     visit '/'
-    first_record = find(:css, 'table#task_table').all('tbody tr')[0]
-    expect(first_record).to have_selector('a', text: 'Rspec test 1')
+    tr = first(:css, 'table#task_table tbody tr')
+    expect(tr).to have_selector('a', text: 'Rspec test 1')
   end
 
   it '編集画面のリンク表示' do
     visit '/'
-    first_record = find(:css, 'table#task_table').all('tbody tr')[0]
-    expect(first_record).to have_selector('a.edit-button')
+    tr = first(:css, 'table#task_table tbody tr')
+    expect(tr).to have_selector('a.edit-button')
   end
 
   it '削除ボタンの表示' do
     visit '/'
-    first_record = find(:css, 'table#task_table').all('tbody tr')[0]
-    expect(first_record).to have_selector('a.trash-button')
+    tr = first(:css, 'table#task_table tbody tr')
+    expect(tr).to have_selector('a.trash-button')
   end
 end
 
-# describe '複数レコードの表示する', type: :feature do
-#
-#   it '件数の確認' do
-#
-#   end
-# end
-#
-# describe 'タスク登録画面に遷移する', type: :feature do
-#   it 'タスク登録画面への遷移' do
-#     visit '/'
-#     click_on 'add_task_button'
-#     expect(page).to have_selector(:css, "#add_task")
-#   end
-# end
+describe '複数レコードの表示する', type: :feature do
+  before do
+    (1..10).to_a.each {|i| create(:task, title: "Rspec test #{i}" )}
+  end
 
+  it '件数の確認' do
+    visit '/'
+    expect(page).to have_css('table#task_table tbody tr', count: 10)
+  end
 
-=begin
-describe 'タスク参照画面に遷移する', type: :feature do
-  let!(:task) { create(:task) }
+  it 'id順で昇順ソートされていることの確認' do
+    visit '/'
+    all('table#task_table tbody tr').each.with_index do |td, idx|
+      # 作成時に登録順でインクリメントしているので、idでソートされていると名前も昇順になっている
+      expect(td).to have_selector('a', text: "Rspec test #{idx+1}")
+    end
+  end
+
+end
+
+describe 'タスク一覧画面から別画面に遷移する', type: :feature do
+  before { create(:task, title: 'Rspec test 1' ) }
 
   it 'タスク登録画面への遷移' do
     visit '/'
-    click_on 'open_task_registration'
-    expect(page).to have_selector(:css, "#show_task")
+    click_on 'add_task_button'
+    expect(page).to have_selector(:css, '#add_task')
   end
-end
-
-describe 'タスク編集画面に遷移する', type: :feature do
-  let!(:task) { create(:task) }
 
   it 'タスク登録画面への遷移' do
     visit '/'
-    click_on 'open_task_registration'
-    expect(page).to have_selector(:css, "#todo_app_task_registration")
+    first(:css, 'table#task_table tbody tr').find_link('Rspec test 1').click
+    expect(page).to have_selector(:css, '#show_task')
   end
-end
 
-describe 'タスクを削除する', type: :feature do
-  let!(:task) { create(:task) }
+  before { create(:task, title: 'Rspec test 1' ) }
 
   it 'タスク登録画面への遷移' do
     visit '/'
-    click_on 'open_task_registration'
-    expect(page).to have_selector(:css, "#todo_app_task_registration")
+    first(:css, 'table#task_table tbody tr').find('td a.edit-button').click
+    expect(page).to have_selector(:css, '#edit_task')
+  end
+
+end
+
+describe 'タスクの削除処理', type: :feature do
+  before { create(:task, title: 'Rspec test 1' ) }
+
+  it 'タスク処理をキャンセルする' do
+    visit '/'
+
+    expect(page).to have_css('table#task_table tbody tr', count: 1)
+    first(:css, 'table#task_table tbody tr').find('td a.trash-button').click
+    page.dismiss_confirm
+
+    expect(page).to have_css('table#task_table tbody tr', count: 1)
+  end
+
+  it 'タスク処理を実行する' do
+    visit '/'
+
+    expect(page).to have_css('table#task_table tbody tr', count: 1)
+    first(:css, 'table#task_table tbody tr').find('td a.trash-button').click
+    page.accept_confirm
+
+    expect(page).to have_css('.alert-success')
+    expect(page).to have_css('table#task_table tbody tr', :count=>0)
   end
 end
-=end
