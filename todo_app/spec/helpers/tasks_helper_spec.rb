23f502882c651faf5d3215858fa4f242a8cc0c80
ShoheiMitani <mitani49@gmail.com>
2018-03-08 20:47:00 +0900

    Add tasks helper test code and remove same test code in show task page spec

---
+++ todo_app/spec/helpers/tasks_helper_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe TasksHelper, type: :helper do
+  describe 'プルダウンのリスト確認' do
+    context 'ステータスの場合' do
+      it '3件であること' do
+        expect(status_pull_down.size).to eq 3
+      end
+
+      it '未着手 / 進行中 / 完了が存在すること' do
+        expected = [%w(未着手 not_start), %w(進行中 progress), %w(完了 done)]
+        expect(status_pull_down).to eq expected
+      end
+    end
+
+    context '優先度の場合' do
+      it '5件であること' do
+        expect(priority_pull_down.size).to eq 5
+      end
+
+      it '低い / 通常 / 高い / 急いで / 今すぐが存在すること' do
+        expected = [%w(低い low), %w(通常 normal), %w(高い high), %w(急いで quickly), %w(今すぐ right_now)]
+        expect(priority_pull_down).to eq expected
+      end
+    end
+  end
+
+  describe '名称の取得' do
+    context 'ステータス表示名のパターンテスト' do
+      it '未着手と表示されること' do
+        expect(status_value('not_start')).to eq Task.human_attribute_name('status.not_start')
+      end
+
+      it '進行中と表示されること' do
+        expect(status_value('progress')).to eq Task.human_attribute_name('status.progress')
+      end
+
+      it '完了と表示されること' do
+        expect(status_value('done')).to eq Task.human_attribute_name('status.done')
+      end
+    end
+
+    context '優先順位表示名のパターンテスト' do
+      it '低いと表示されること' do
+        expect(priority_value('low')).to eq Task.human_attribute_name('priority.low')
+      end
+
+      it '通常と表示されること' do
+        expect(priority_value('normal')).to eq Task.human_attribute_name('priority.normal')
+      end
+
+      it '高いと表示されること' do
+        expect(priority_value('high')).to eq Task.human_attribute_name('priority.high')
+      end
+
+      it '急いでと表示されること' do
+        expect(priority_value('quickly')).to eq Task.human_attribute_name('priority.quickly')
+      end
+
+      it '今すぐと表示されること' do
+        expect(priority_value('right_now')).to eq Task.human_attribute_name('priority.right_now')
+      end
+    end
+  end
+end
