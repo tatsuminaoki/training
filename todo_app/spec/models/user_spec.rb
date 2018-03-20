132425a89f89099f47aa22511ac6428b96510bb9
ShoheiMitani <mitani49@gmail.com>
2018-03-20 15:54:07 +0900

    Add test code

--- todo_app/spec/models/user_spec.rb
+++ todo_app/spec/models/user_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 describe User, type: :model do
   describe 'インスタンスの状態' do
     context '有効な場合' do
-      it '名前があれば有効な状態であること' do
+      it '名前、パスワード、確認パスワードがあれば有効な状態であること' do
         user = build(:user)
         expect(user).to be_valid
       end
 
-      it '名前が50文字以下であれば有効な状態であること' do
+      it '名前が20文字以下であれば有効な状態であること' do
         user = build(:user, name: 'a' * 20)
+        expect(user).to be_valid
+      end
+
+      it 'パスワードが72文字以下であれば有効な状態であること' do
+        user = build(:user, password: 'a' * 72, password_confirmation: 'a' * 72)
         expect(user).to be_valid
       end
     end
@@ -25,6 +30,18 @@ describe User, type: :model do
         user = build(:user, name: 'a' * 21)
         expect(user).to be_invalid
         expect(user.errors[:name][0]).to eq I18n.t('errors.messages.too_long', count: 20)
+      end
+
+      it 'パスワードがなければ無効な状態であること' do
+        user = build(:user, password: nil)
+        expect(user).to be_invalid
+        expect(user.errors[:password][0]).to eq I18n.t('errors.messages.empty')
+      end
+
+      it 'パスワードが73文字以上の場合、無効な状態であること' do
+        user = build(:user, password: 'a' * 73)
+        expect(user).to be_invalid
+        expect(user.errors[:password][0]).to eq I18n.t('errors.messages.too_long', count: 72)
       end
     end
   end
