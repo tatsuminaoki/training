f0485744d6bb056e32584847dbe6cac48694888a
ShoheiMitani <mitani49@gmail.com>
2018-03-20 15:41:22 +0900

    Add seed data

--- todo_app/db/seeds.rb
+++ todo_app/db/seeds.rb
@@ -1,3 +1,5 @@
+user = User.create(name: 'admin')
+
 1000.times do |i|
   Task.create!(
       title: "Task #{i}",
@@ -5,5 +7,5 @@
       deadline: Time.new('2018-01-01 00:00:00').getlocal + (i * 3600),
       status: %w[not_start progress done][rand(0..2)],
       priority: %w[low normal high quickly right_now][rand(0..4)],
-  )
+      user_id: user.id)
 end
