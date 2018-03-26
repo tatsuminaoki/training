0f2c073646fd209b775521d783fb23ea6f93da9b
ShoheiMitani <mitani49@gmail.com>
2018-03-26 20:24:06 +0900

    Use update_all instead of update

--- todo_app/db/migrate/20180326105139_change_user_id_column_not_allow_nil.rb
+++ todo_app/db/migrate/20180326105139_change_user_id_column_not_allow_nil.rb
@@ -4,7 +4,7 @@ class ChangeUserIdColumnNotAllowNil < ActiveRecord::Migration[5.1]
     if Task.count > 1
       user = User.find_by(name: 'administrator')
       user ||= User.create(name: 'administrator', password: 'foobar', password_confirmation: 'foobar')
-      Task.where(user_id: nil).each { |task| task.update(user_id: user.id) }
+      Task.where(user_id: nil).update_all(user_id: user.id)
     end
 
     change_column :tasks, :user_id, :bigint, null: false
