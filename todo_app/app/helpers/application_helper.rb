c69289c3800dd42df2685d6c04af065a4e596b3b
ShoheiMitani <mitani49@gmail.com>
2018-03-28 15:23:36 +0900

    Fix bug which wrong button name is displayed after validation error.

--- todo_app/app/helpers/application_helper.rb
+++ todo_app/app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
   def active_action(*action)
     return 'active' if action.include?(params[:action])
   end
+
+  def submit_btn_name(type: :new)
+    return I18n.t('helpers.submit.create') if type == :new
+    return I18n.t('helpers.submit.update') if type == :edit
+  end
 end
