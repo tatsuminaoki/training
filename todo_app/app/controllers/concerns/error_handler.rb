2d0054e965b75c9e27bc8851be5ad9b20074d76e
ShoheiMitani <mitani49@gmail.com>
2018-03-30 11:35:07 +0900

    Use error handler instead of errors controller

--- todo_app/app/controllers/concerns/error_handler.rb
+++ todo_app/app/controllers/concerns/error_handler.rb
@@ -1,18 +1,10 @@
 # frozen_string_literal: true
 
-class ErrorsController < ApplicationController
+module ErrorHandler
   rescue_from Exception, with: :render_500
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
   rescue_from ActionView::MissingTemplate, with: :render_404
-
-  def routing_error
-    raise ActionController::RoutingError, params[:path]
-  end
-
-  def other_error
-    raise Exception, params[:path]
-  end
 
   def render_404
     render file: Rails.root.join('public', '404.html'), layout: false, status: :not_found, content_type: 'text/html'
