38d8ed34427a53508fac9c4c8a98cfd8a3c50077
katsuhiro.oshima <katsuhiro.oshima@rakuten.com>
2018-04-09 17:03:23 +0900

    Added task model

---
+++ tms/db/migrate/20180409074220_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+      t.text :description
+      t.datetime :due_date, null: false
+      t.integer :priority, null: false, limit: 1
+      t.integer :status, default: 0, null: false, limit: 1
+      t.integer :user_id, null: false
+
+      t.timestamps
+    end
+  end
+end
