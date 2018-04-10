1f4b9d58677e51eab77b6418a56b0b17ef321088
katsuhiro.oshima <katsuhiro.oshima@rakuten.com>
2018-04-10 11:44:03 +0900

    Modified root setting from '/' to 'tasks#index'

--- tms/config/routes.rb
+++ tms/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root setting
+  root to: 'tasks#index'
 end
