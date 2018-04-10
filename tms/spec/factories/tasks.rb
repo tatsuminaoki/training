27ea779411ae538246791fd57c699d73b0edcab3
katsuhiro.oshima <katsuhiro.oshima@rakuten.com>
2018-04-10 15:05:05 +0900

    Prepare for creating test data

---
+++ tms/spec/factories/tasks.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :task do
+    title "Setup DEV ENV"
+    description "Setup development environment on localhost"
+    due_date "2018-04-17"
+    priority 0    
+    status 0    
+    user_id 1
+  end
+end
