293e915c650a495b294815cc39400186d8ee54b5
katsuhiro.oshima <katsuhiro.oshima@rakuten.com>
2018-04-12 17:27:17 +0900

    Adapted i18n to task feature spec

--- tms/spec/features/tasks_spec.rb
+++ tms/spec/features/tasks_spec.rb
@@ -4,16 +4,16 @@ RSpec.feature "Tasks", type: :feature do
   scenario "user create a new task" do
     visit root_path
     expect {
-      click_link "New Task"
-      fill_in "Title", with: "Setup DEV ENV"
-      fill_in "Description", with: "Setup development environment on localhost."
-      fill_in "User", with: 1
-      fill_in "Priority", with: 0
-      fill_in "Status", with: 0
-      fill_in "Due date", with: "2018-04-17"
-      click_button "Create Task"
+      click_link I18n.t("page.task.link.new")
+      fill_in I18n.t('page.task.thead.title'), with: "Setup DEV ENV"
+      fill_in I18n.t('page.task.thead.description'), with: "Setup development environment on localhost."
+      fill_in I18n.t('page.task.thead.user'), with: 1
+      fill_in I18n.t('page.task.thead.priority'), with: 0
+      fill_in I18n.t('page.task.thead.status'), with: 0
+      fill_in I18n.t('page.task.thead.due_date'), with: "2018-04-17"
+      click_button I18n.t("helpers.submit.create")
 
-      expect(page).to have_content "Task was successfully created"
+      expect(page).to have_content I18n.t("flash.task.create")
     }.to change(Task, :count).by(1)
   end
 end
