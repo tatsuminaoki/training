65810c3e1a27635456ae74456e2669b4fa81c8da
katsuhiro.oshima <katsuhiro.oshima@rakuten.com>
2018-04-16 14:31:51 +0900

    Added index to tasks table

--- tms/db/schema.rb
+++ tms/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409074220) do
+ActiveRecord::Schema.define(version: 20180416020933) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title", null: false
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20180409074220) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["status"], name: "index_tasks_on_status"
+    t.index ["title"], name: "index_tasks_on_title"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
 end
