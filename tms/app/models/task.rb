bfca4b6182ee01dd228da1f60afbd2e8672cd9ff
katsuhiro.oshima <katsuhiro.oshima@rakuten.com>
2018-04-16 15:13:40 +0900

    Modified validation of task

--- tms/app/models/task.rb
+++ tms/app/models/task.rb
@@ -17,5 +17,8 @@ class Task < ApplicationRecord
             }
   validates :user_id,
             presence: true,
-            numericality: { only_integer: true }
+            numericality: {
+              only_integer: true,
+              greater_than: 0
+            }
 end
