08043010e8875e02e0fe9443978895167be0eae7
katsuhiro.oshima <katsuhiro.oshima@rakuten.com>
2018-04-16 14:32:20 +0900

    Added validation to task

--- tms/app/models/task.rb
+++ tms/app/models/task.rb
@@ -1,2 +1,21 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :due_date, presence: true
+  validates :priority,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0,
+              less_than_or_equal_to: 2
+            }
+  validates :status,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0,
+              less_than_or_equal_to: 2
+            }
+  validates :user_id,
+            presence: true,
+            numericality: { only_integer: true }
 end
