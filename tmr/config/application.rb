efb91fe42de4a71171d0dff7b5d2341e602b5c11
Yoshiyuki YamaZaki <yoshiyuki.yamazaki@fablic.co.jp>
2018-04-17 14:49:00 +0900

    Step 9 i18n 機能による多言語化（日本語・英語）

--- tmr/config/application.rb
+++ tmr/config/application.rb
@@ -10,6 +10,8 @@ module Tmr
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    Rails.application.config.i18n.available_locales = [:en, :ja]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
