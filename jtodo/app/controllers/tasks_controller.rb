f6b18845e004aafb27b933304f60684d70a155bb
unsu0707 <unsu0707@gmail.com>
2018-04-17 17:42:22 +0900

    Added sorting link on due_date in index

--- jtodo/app/controllers/tasks_controller.rb
+++ jtodo/app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.order(:created_at)
+    @tasks = Task.order(params[:sort])
   end
 
   # GET /tasks/1
