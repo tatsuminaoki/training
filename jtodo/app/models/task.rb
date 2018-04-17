3da29f1b5bee18d10f9925ba455332489231e3e4
unsu0707 <unsu0707@gmail.com>
2018-04-17 15:51:04 +0900

    Added validates in task model

--- jtodo/app/models/task.rb
+++ jtodo/app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  validates :title,     presence: true, length: { maximum: 255 }
+  validates :status,    inclusion: { in: 0..2 }
+  validates :priority,  inclusion: { in: 0..2 }
 end
