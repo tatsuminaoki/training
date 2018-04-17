d2536f70b34836210100c48e2f7c58cd62fc891c
Yoshiyuki YamaZaki <yoshiyuki.yamazaki@fablic.co.jp>
2018-04-17 15:01:44 +0900

    Step 10 タイムゾーンを東京に設定

--- tmr/config/application.rb
+++ tmr/config/application.rb
@@ -10,8 +10,13 @@ module Tmr
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    # i18n
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     Rails.application.config.i18n.available_locales = [:en, :ja]
+
+    # timezone
+    config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
