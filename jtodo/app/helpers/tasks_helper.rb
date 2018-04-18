aafb6be13ffc474e1421f0ffa5144a817b3a96c9
unsu0707 <unsu0707@gmail.com>
2018-04-18 12:33:48 +0900

    Added method for generate link_to by sorting status

--- jtodo/app/helpers/tasks_helper.rb
+++ jtodo/app/helpers/tasks_helper.rb
@@ -1,2 +1,15 @@
 module TasksHelper
+
+  # Method 'link_sort' : switch desc/asc for link_to sort
+  def link_sort label_name
+    link_to_elements = ['▼',label_name+' desc', 'sort_'+label_name]
+    if params.has_key?(:sort) then
+      if params[:sort].include?(label_name) && params[:sort].include?('desc') then
+        link_to_elements[0] = '▲';
+        link_to_elements[1] = label_name;
+      end
+    end
+    link_to link_to_elements[0], { sort: link_to_elements[1] }, { :id => link_to_elements[2] }
+  end
+
 end
