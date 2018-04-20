0edd5e0c96a00ec3fd8bcb4fc6499c35e6163720
unsu0707 <unsu0707@gmail.com>
2018-04-20 15:40:26 +0900

    モデルにEnum, Validates, そしてSearch Methodを追加

--- jtodo/app/models/task.rb
+++ jtodo/app/models/task.rb
@@ -1,5 +1,15 @@
 class Task < ApplicationRecord
+  enum status: [:waiting, :working, :done]
+  enum priority: [:low, :middle, :high]
   validates :title,     presence: true, length: { maximum: 255 }
-  validates :status,    inclusion: { in: 0..2 }
-  validates :priority,  inclusion: { in: 0..2 }
+  validates :status,    inclusion: { in: Task.statuses.keys }
+  validates :priority,  inclusion: { in: Task.priorities.keys }
+
+  def self.search(search)
+    if search
+      self.where('title LIKE :search OR description LIKE :search', search: "%#{search}%")
+    else
+      self
+    end
+  end
 end
