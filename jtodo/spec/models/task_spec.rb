2c79873c634899c80ccd99e1f3c8b3d53e4c8ad9
unsu0707 <unsu0707@gmail.com>
2018-04-20 18:00:08 +0900

    Modified model spec

--- jtodo/spec/models/task_spec.rb
+++ jtodo/spec/models/task_spec.rb
@@ -2,46 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   it 'Valid task' do
-    task = Task.new(
-      title: 'Valid Title',
-      description: 'Valid Description',
-      status: 0,
-      priority: 0,
-      due_date: 1.day.since
-    )
+    task = build(:task)
     expect(task).to be_valid
   end
   it 'Invalid title' do
-    task = Task.new(
-      title: nil,
-      description: 'Valid Description',
-      status: 0,
-      priority: 0,
-      due_date: 1.day.since
-    )
-    expect(task).to_not be_valid
+    task = build(:task, title: nil)
+    expect(task).to be_invalid
     expect(task.errors[:title]).to be_present
   end
   it 'Invalid status' do
-    task = Task.new(
-      title: 'Valid Title',
-      description: 'Valid Description',
-      status: 3,
-      priority: 0,
-      due_date: 1.day.since
-    )
-    expect(task).to_not be_valid
+    task = build(:task, status: nil)
+    expect(task).to be_invalid
     expect(task.errors[:status]).to be_present
   end
   it 'Invalid priority' do
-    task = Task.new(
-      title: 'Valid Title',
-      description: 'Valid Description',
-      status: 0,
-      priority: 3,
-      due_date: 1.day.since
-    )
-    expect(task).to_not be_valid
+    task = build(:task, priority: nil)
+    expect(task).to be_invalid
     expect(task.errors[:priority]).to be_present
   end
 end
