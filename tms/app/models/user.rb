7b1ae0ef47877491a748a39cf627bec055077b1b
katsuhiro.oshima <katsuhiro.oshima@rakuten.com>
2018-04-20 15:43:00 +0900

    Created user model

---
+++ tms/app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_many :tasks
+
+  validates :name,
+            presence: true,
+            uniqueness: true
+  validates :password,
+            presence: true,
+            length: { minimum: 6 }
+end
