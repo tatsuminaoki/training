b68d9c961a7f1d628e6e6d84bb2ce33ae577c574
katsuhiro.oshima <katsuhiro.oshima@rakuten.com>
2018-04-20 15:40:51 +0900

    Implemented solution of N+1 issue

--- tms/app/controllers/tasks_controller.rb
+++ tms/app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.search(params)
+    @tasks = Task.includes(:user).search(params)
   end
 
   # GET /tasks/1
