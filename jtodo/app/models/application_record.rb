bf50843d8008a7c20197ac6b2f079c8b7153b241
unsu0707 <unsu0707@gmail.com>
2018-04-20 15:37:07 +0900

    Class Attributesのenumの名前とリストを返すメソッドを追加

--- jtodo/app/models/application_record.rb
+++ jtodo/app/models/application_record.rb
@@ -1,3 +1,11 @@
-class ApplicationRecord < ActiveRecord::Base
+class ApplicationRecord < ActiveRecord::Base   
   self.abstract_class = true
+
+  def human_enum_name param
+    self.class.human_attribute_name("#{param}.#{self[param]}")
+  end
+  
+  def self.human_enum_list param
+    self.send("#{param}".pluralize).keys.collect { |key| [I18n.t("activerecord.attributes.task/#{param}.#{key.downcase}"), key] }
+  end
 end
