b8753f526d3d82cfe348dabba781c0ea9b7574ab
unsu0707 <unsu0707@gmail.com>
2018-04-23 10:02:45 +0900

    モデルSpecのタイトルを日本語に修正

--- jtodo/spec/models/task_spec.rb
+++ jtodo/spec/models/task_spec.rb
@@ -1,32 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it 'Valid task' do
+  it '正常な値で登録できる' do
     task = build(:task)
     expect(task).to be_valid
   end
-  it 'Invalid title' do
+  it '不正なタイトルで登録するとタイトルエラーが発生する' do
     task = build(:task, title: nil)
     expect(task).to be_invalid
     expect(task.errors[:title]).to be_present
   end
-  it 'Invalid status' do
+  it '不正なステータスで登録するとステータスエラーが発生する' do
     task = build(:task, status: nil)
     expect(task).to be_invalid
     expect(task.errors[:status]).to be_present
   end
-  it 'Invalid priority' do
+  it '不正な優先度で登録すると優先度エラーが発生する' do
     task = build(:task, priority: nil)
     expect(task).to be_invalid
     expect(task.errors[:priority]).to be_present
   end
-  it 'Search could find a task from title' do
+  it 'タイトルでキーワードを検索できる' do
     create(:task, title: 'Rspec Search Test')
     @search_result = Task.search('Rspec')
     expect(@search_result).to exist
     expect(@search_result[0].title).to eq('Rspec Search Test')
   end
-  it 'Search could find a task from description' do
+  it '説明文でキーワードを検索できる' do
     create(:task, description: 'Rspec Search Test')
     @search_result = Task.search('Rspec')
     expect(@search_result).to exist
