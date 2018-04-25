91b8291a3ba589f74002dc0e28f641a1744dfc03
unsu0707 <unsu0707@gmail.com>
2018-04-25 11:32:11 +0900

    スペックのファクトリーを修正

--- jtodo/spec/factories/users.rb
+++ jtodo/spec/factories/users.rb
@@ -1,7 +1,16 @@
 FactoryBot.define do
   factory :user do
-    name "MyString"
-    password "MyString"
+    sequence(:name) { |n| "user_test #{n}" }
+    password 'userpass'
     is_admin false
+    factory :invalid_name do
+      name nil
+    end
+    factory :invalid_password do
+      password nil
+    end
+    factory :short_password do
+      password 'short'
+    end
   end
 end
