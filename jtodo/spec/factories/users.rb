fd136abeea35c5bd08507fc42b2ebd871a684dc1
unsu0707 <unsu0707@gmail.com>
2018-04-25 11:36:57 +0900

    使わないファクトリーを削除

--- jtodo/spec/factories/users.rb
+++ jtodo/spec/factories/users.rb
@@ -3,14 +3,5 @@ FactoryBot.define do
     sequence(:name) { |n| "user_test #{n}" }
     password 'userpass'
     is_admin false
-    factory :invalid_name do
-      name nil
-    end
-    factory :invalid_password do
-      password nil
-    end
-    factory :short_password do
-      password 'short'
-    end
   end
 end
