034065c6cde55ce25dd4e62a4ae39c02c2f52969
unsu0707 <unsu0707@gmail.com>
2018-04-25 17:25:04 +0900

    マイグレーションエラーを解決。passwordをpassword_digestに修正

--- jtodo/db/migrate/20180424035350_change_tasks.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTasks < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :tasks, :user, foreign_key: true
-  end
-end
--- jtodo/db/schema.rb
+++ jtodo/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_075407) do
+ActiveRecord::Schema.define(version: 2018_04_25_073124) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2018_04_24_075407) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.boolean "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
