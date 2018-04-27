1c59dabc366becae2dfea9ebbb641ab3f3a4c2bc
unsu0707 <unsu0707@gmail.com>
2018-04-27 15:23:32 +0900

    ユーザーが削除されるときにユーザーのタスクも削除する

--- jtodo/app/models/user.rb
+++ jtodo/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   validates :name,     presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
   has_secure_password
