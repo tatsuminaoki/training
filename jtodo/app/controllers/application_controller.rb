163575d7370e254f3353d3db9491fb116bc2585c
unsu0707 <unsu0707@gmail.com>
2018-05-07 10:14:09 +0900

    500エラー処理部分コメント消す

--- jtodo/app/controllers/application_controller.rb
+++ jtodo/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
-  #rescue_from Exception, with: :render_500
+  rescue_from Exception, with: :render_500
 
   def render_404
     render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
