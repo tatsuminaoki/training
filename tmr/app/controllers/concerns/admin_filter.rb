acee6e611e173718a7f4f128b3f360efcf91c97c
Yoshiyuki YamaZaki <yoshiyuki.yamazaki@fablic.co.jp>
2018-05-10 17:37:37 +0900

    メソッド名に ? を追加

--- tmr/app/controllers/concerns/admin_filter.rb
+++ tmr/app/controllers/concerns/admin_filter.rb
@@ -2,10 +2,10 @@ class AdminFilter
   def before(controller)
     user = controller.session[:user]
     # 管理者URLを隠すため404とする
-    raise ApplicationController::NotFound unless is_admin(user)
+    raise ApplicationController::NotFound unless is_admin?(user)
   end
 
-  def is_admin(user)
+  def is_admin?(user)
     ActiveRecord::Type::Boolean.new.cast(user['admin_flag'])
   end
 end
