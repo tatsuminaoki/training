402e1dc38736e7bd0b425a3b0f9a8cfbc87aaaed
shungo.a.ito <shungo.a.ito@rakuten.com>
2018-06-14 15:14:00 +0900

    タスクの一覧、作成、詳細、編集画面を作成

---
+++ task_management/app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+  protect_from_forgery except: :new
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+
+    @task = Task.find(params[:id])
+    p @task
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    if @task.save
+      redirect_to :action => "index"
+    else
+      render :action => "new"
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(tasks_params)
+      redirect_to :action => "show", id: params[:id]
+    else
+      render :action => "edit"
+    end
+  end
+
+  def tasks_params
+    params.require(:task).permit(:task_name,:description)
+  end
+end
--- task_management/config/routes.rb
+++ task_management/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+  get 'tasks/index'
+  get 'tasks/show/:id', to: 'tasks#show'
+  get 'tasks/edit/:id', to: 'tasks#edit'
+  get 'tasks/new'
+  
+  post 'tasks/create', as:"tasks"
+  post 'tasks/edit/:id', to: 'tasks#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
