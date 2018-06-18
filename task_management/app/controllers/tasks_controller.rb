ceacae0a1fc060e9be2389a15c2789b67454943c
shungo.a.ito <shungo.a.ito@rakuten.com>
2018-06-18 16:20:12 +0900

    i18nで日本語部分を共通化

--- task_management/app/controllers/tasks_controller.rb
+++ task_management/app/controllers/tasks_controller.rb
@@ -19,27 +19,27 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(tasks_params)
     if @task.save
-      redirect_to ({action: 'index'}), notice: 'タスクを登録しました'
+      redirect_to ({action: 'index'}), notice: t('flash.success_create')
     else
-      redirect_to ({action: 'new'}), alert: 'タスクの登録に失敗しました'
+      redirect_to ({action: 'new'}), alert: t('flash.failure_create')
     end
   end
 
   def update
     @task = Task.find(params[:id])
     if @task.update_attributes(tasks_params)
-      redirect_to ({action: 'show'}), id: params[:id], notice: 'タスクを更新しました'
+      redirect_to ({action: 'show'}), id: params[:id], notice: t('flash.success_update')
     else
-      redirect_to ({action: 'edit'}), alert: 'タスクの更新に失敗しました'
+      redirect_to ({action: 'edit'}), alert: t('flash.failure_update')
     end
   end
 
   def destroy
     @task = Task.find(params[:id])
     if @task.destroy
-      redirect_to ({action: 'index'}), notice: "タスク：#{@task.task_name}を削除しました"
+      redirect_to ({action: 'index'}), notice: t('flash.success_delete', :task => @task.task_name)
     else
-      redirect_to ({action: 'show'}), id: params[:id], alert: "タスク：#{@task.task_name}の削除に失敗しました"
+      redirect_to ({action: 'show'}), id: params[:id], alert: t('flash.failure_delete', :task =>@task.task_name)
     end
   end
 
--- task_management/spec/features/tasks_spec.rb
+++ task_management/spec/features/tasks_spec.rb
@@ -8,77 +8,77 @@ RSpec.feature "Tasks", type: :feature do
 
   scenario '一覧画面からタスク登録画面に遷移' do
     visit root_path
-    click_button 'new'
+    click_button I18n.t('button.new')
     expect(current_url).to eq 'http://localhost:3000/tasks/new'
     expect(page).to have_field 'task_task_name'
     expect(page).to have_field 'task_description'
-    expect(page).to have_button 'Create Task'
+    expect(page).to have_button I18n.t('helpers.submit.create')
   end
   
   scenario 'タスクの登録' do
     visit 'http://localhost:3000/tasks/new'
     fill_in 'task_task_name', with: "#{@task.task_name}"
     fill_in 'task_description', with: "#{@task.description}"
-    click_button 'Create Task'
-
+    click_button I18n.t('helpers.submit.create')
+    
     expect(current_url).to eq 'http://localhost:3000/'
-    expect(page).to have_content 'タスクを登録しました'
-    expect(page).to have_content "タスク：#{@task.task_name}"
+    expect(page).to have_content I18n.t('flash.success_create')
+    expect(page).to have_content I18n.t('view.task_name', :task => @task.task_name)
   end
-
+  
   scenario '一覧画面からタスク詳細画面への遷移' do
     visit root_path
-    click_on "タスク：#{@task.task_name}"
+    click_on I18n.t('view.task_name', :task => @task.task_name)
     
     expect(current_url).to eq "http://localhost:3000/tasks/show/#{@task.id}"
-    expect(page).not_to have_content 'タスクを更新しました'
-    expect(page).to have_button 'home'
-    expect(page).to have_button 'edit'
-    expect(page).to have_button 'delete'
-    expect(page).to have_content "タスク：#{@task.task_name}"
-    expect(page).to have_content "詳細　：#{@task.description}"
+    expect(page).not_to have_content I18n.t('flash.success_update')
+    expect(page).to have_button I18n.t('button.home')
+    expect(page).to have_button I18n.t('button.edit')
+    expect(page).to have_button I18n.t('button.delete')
+    expect(page).to have_content I18n.t('view.task_name', :task => @task.task_name)
+    expect(page).to have_content I18n.t('view.description', :task => @task.description)
   end
 
   scenario 'タスク詳細画面から一覧画面に遷移' do
     visit "http://localhost:3000/tasks/show/#{@task.id}"
-    click_button 'home'
+    click_button I18n.t('button.home')
 
     expect(current_url).to eq 'http://localhost:3000/'
-    expect(page).not_to have_content 'タスクを登録しました'
-    expect(page).not_to have_content "タスク：#{@task.task_name}を削除しました"
-    expect(page).to have_button 'new'
-    expect(page).to have_content "タスク：#{@task.task_name}"
+    expect(page).not_to have_content I18n.t('flash.success_create')
+    expect(page).not_to have_content I18n.t('flash.success_delete', :task => @task.task_name)
+    expect(page).to have_button I18n.t('button.new')
+    expect(page).to have_content I18n.t('view.task_name', :task => @task.task_name)
   end
 
   scenario 'タスク詳細画面からタスク編集画面に遷移' do
     visit "http://localhost:3000/tasks/show/#{@task.id}"
-    click_button 'edit'
+    click_button I18n.t('button.edit')
     
     expect(current_url).to eq "http://localhost:3000/tasks/edit/#{@task.id}" 
     expect(page).to have_field 'task_task_name', with: @task.task_name
     expect(page).to have_field 'task_description', with: @task.description
-    expect(page).to have_button 'Update Task'
+    expect(page).to have_button I18n.t('helpers.submit.update')
   end
 
   scenario 'タスクの編集' do
     visit "http://localhost:3000/tasks/edit/#{@task.id}"
     fill_in 'task_task_name', with: "#{@task.task_name}(edited)"
     fill_in 'task_description', with: "#{@task.description}(edited)"
-    click_button 'Update Task'
+    click_button I18n.t('helpers.submit.update')
 
     expect(current_url).to eq "http://localhost:3000/tasks/show/#{@task.id}"
-    expect(page).to have_content 'タスクを更新しました'
-    expect(page).to have_content "タスク：#{@task.task_name}(edited)"
-    expect(page).to have_content "詳細　：#{@task.description}(edited)"
+    expect(page).to have_content I18n.t('flash.success_update')
+    expect(page).to have_content I18n.t('view.task_name', :task => @task.task_name)
+    expect(page).to have_content I18n.t('view.description', :task => @task.description)
   end
 
   scenario 'タスクの削除' do
     visit "http://localhost:3000/tasks/show/#{@task.id}"
-    click_button 'delete'
+    click_button I18n.t('button.delete')
     
     expect(current_url).to eq 'http://localhost:3000/'
-    expect(page).to have_content "タスク：#{@task.task_name}を削除しました"
+    expect(page).to have_content I18n.t('flash.success_delete', :task => @task.task_name)
     visit current_path
-    expect(page).not_to have_content "タスク：#{@task.task_name}"
+    expect(page).not_to have_content I18n.t('view.task_name', :task => @task.task_name)
   end
 end
