ad2e6a99d6df9ff1a79393e0bcb4a7461cb3ad97
shungo.a.ito <shungo.a.ito@rakuten.com>
2018-06-19 11:18:31 +0900

    日本語の辞書ファイルの無駄な部分を削除して、tメソッドの書き方をI18n.tに変更

--- task_management/app/controllers/tasks_controller.rb
+++ task_management/app/controllers/tasks_controller.rb
@@ -19,27 +19,27 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(tasks_params)
     if @task.save
-      redirect_to ({action: 'index'}), notice: t('flash.success_create')
+      redirect_to ({action: 'index'}), notice: I18n.t('flash.success_create')
     else
-      redirect_to ({action: 'new'}), alert: t('flash.failure_create')
+      redirect_to ({action: 'new'}), alert: I18n.t('flash.failure_create')
     end
   end
 
   def update
     @task = Task.find(params[:id])
     if @task.update_attributes(tasks_params)
-      redirect_to ({action: 'show'}), id: params[:id], notice: t('flash.success_update')
+      redirect_to ({action: 'show'}), id: params[:id], notice: I18n.t('flash.success_update')
     else
-      redirect_to ({action: 'edit'}), alert: t('flash.failure_update')
+      redirect_to ({action: 'edit'}), alert: I18n.t('flash.failure_update')
     end
   end
 
   def destroy
     @task = Task.find(params[:id])
     if @task.destroy
-      redirect_to ({action: 'index'}), notice: t('flash.success_delete', :task => @task.task_name)
+      redirect_to ({action: 'index'}), notice: I18n.t('flash.success_delete', :task => @task.task_name)
     else
-      redirect_to ({action: 'show'}), id: params[:id], alert: t('flash.failure_delete', :task =>@task.task_name)
+      redirect_to ({action: 'show'}), id: params[:id], alert: I18n.t('flash.failure_delete', :task =>@task.task_name)
     end
   end
 
