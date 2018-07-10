7da78412f3b04ca6885aefeb22384d6de9945e77
shungo.a.ito <shungo.a.ito@rakuten.com>
2018-07-10 15:08:02 +0900

    ログイン画面とログイン時の処理を作成

---
+++ task_management/app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(user_name: params[:user_name])
+    if user && user.authenticate(params[:password])
+      log_in user
+      redirect_to root_path, notice: I18n.t('flash.success_log_in')
+    else
+      redirect_to ({action: 'new'}), alert: I18n.t('flash.failure_log_in')
+    end
+  end
+end
