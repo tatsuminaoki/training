b85eca0eb600f97c0a0600c5722fc09376d0c2ed
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-11 10:06:28 +0900

    [add]タスクモデルの簡単なテスト追加
    バリデーション設定後(ステップ10)に確り書く予定

--- tasclear/spec/models/task_spec.rb
+++ tasclear/spec/models/task_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'タスク名と内容があれば有効な状態であること' do
+    task = Task.new(
+      name: '家事',
+      content: '掃除、洗濯'
+    )
+    expect(task).to be_valid
+  end
 end
