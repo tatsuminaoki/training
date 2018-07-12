1a842a6e1f048e0aba4767af4f56c41db5947e46
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-12 10:02:21 +0900

    [change]複数行のカンマ区切りで最終行にカンマ追加
    fablicop指摘事項対応

--- tasclear/spec/models/task_spec.rb
+++ tasclear/spec/models/task_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Task, type: :model do
   it 'タスク名と内容があれば有効な状態であること' do
     task = Task.new(
       name: '家事',
-      content: '掃除、洗濯'
+      content: '掃除、洗濯',
     )
     expect(task).to be_valid
   end
