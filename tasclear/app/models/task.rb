ff77b57a39ba16c43ed3677f4b2233bec929027a
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-17 11:20:24 +0900

    [add]タスクへのバリデーション設定
    タスク名：空では無く20文字以内、内容：200文字以内

--- tasclear/app/models/task.rb
+++ tasclear/app/models/task.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  validates :name,
+    presence: true,
+    length: { maximum: 20 }
+  validates :content, length: { maximum: 200 }
 end
