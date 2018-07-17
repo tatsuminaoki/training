bc03d9efd61f8aba6fb2c44aa58c8d30f0f0c3f0
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-17 13:18:42 +0900

    [add]バリデーション追加に伴い、モデルスペック追加

--- tasclear/spec/models/task_spec.rb
+++ tasclear/spec/models/task_spec.rb
@@ -10,4 +10,39 @@ RSpec.describe Task, type: :model do
     )
     expect(task).to be_valid
   end
+  it 'タスク名がなければ無効な状態であること' do
+    task = Task.new(
+      name: '',
+      content: '掃除、洗濯',
+    )
+    expect(task).to be_invalid
+  end
+  it 'タスク名が20文字であれば有効な状態であること' do
+    task = Task.new(
+      name: 'あ' * 20,
+      content: '掃除、洗濯',
+    )
+    expect(task).to be_valid
+  end
+  it 'タスク名が21文字であれば無効な状態であること' do
+    task = Task.new(
+      name: 'あ' * 21,
+      content: '掃除、洗濯',
+    )
+    expect(task).to be_invalid
+  end
+  it '内容が200文字であれば有効な状態であること' do
+    task = Task.new(
+      name: '家事',
+      content: 'あ' * 200,
+    )
+    expect(task).to be_valid
+  end
+  it '内容が201文字であれば無効な状態であること' do
+    task = Task.new(
+      name: '家事',
+      content: 'あ' * 201,
+    )
+    expect(task).to be_invalid
+  end
 end
