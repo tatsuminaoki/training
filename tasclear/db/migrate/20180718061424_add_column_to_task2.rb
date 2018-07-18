7874f4e5400d19dce01cd2da9c2b0c95f371323f
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-18 15:18:25 +0900

    [add]タスクテーブルにステータスカラム追加

---
+++ tasclear/db/migrate/20180718061424_add_column_to_task2.rb
@@ -0,0 +1,5 @@
+class AddColumnToTask2 < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :status, :integer, default: 0, null: false
+  end
+end
--- tasclear/db/schema.rb
+++ tasclear/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_060620) do
+ActiveRecord::Schema.define(version: 2018_07_18_061424) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_07_17_060620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "deadline"
+    t.integer "status", default: 0, null: false
   end
 
 end
