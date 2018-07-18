3775fac2ba0926e39fa70831d7f414c1f0737ff0
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-18 17:37:12 +0900

    [add]タスクのステータスのCRUD機能と日本語化

--- tasclear/app/controllers/tasks_controller.rb
+++ tasclear/app/controllers/tasks_controller.rb
@@ -46,7 +46,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :content, :deadline)
+    params.require(:task).permit(:name, :content, :deadline, :status)
   end
 
   def set_task
