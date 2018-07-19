3c291b27a46f01395b99945d76f340cb5e7cf62d
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-19 11:03:50 +0900

    [add]コントローラに記載していたタスクの検索・ソートロジックをモデルに移植
    ロジックをモデルに集約するため

--- tasclear/app/controllers/tasks_controller.rb
+++ tasclear/app/controllers/tasks_controller.rb
@@ -4,16 +4,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
 
   def index
-    search_name = params[:search_name]
-    search_status = params[:search_status]
-    @tasks = Task.all
-    @tasks = @tasks.where(['name LIKE ?', "%#{search_name}%"]) if search_name.present?
-    @tasks = @tasks.where(status: search_status) if search_status.present?
-    @tasks = if params.key?(:sort)
-               @tasks.order(deadline: params[:sort].to_sym, created_at: :desc)
-             else
-               @tasks.order(created_at: :desc)
-             end
+    @tasks = Task.search_and_order(params)
   end
 
   def new
--- tasclear/app/models/task.rb
+++ tasclear/app/models/task.rb
@@ -5,5 +5,19 @@ class Task < ApplicationRecord
     presence: true,
     length: { maximum: 20 }
   validates :content, length: { maximum: 200 }
+
   enum status: { to_do: 0, doing: 1, done: 2 }
+
+  def self.search_and_order(params)
+    search_name = params[:search_name]
+    search_status = params[:search_status]
+    tasks = Task.all
+    tasks = tasks.where(['name LIKE ?', "%#{search_name}%"]) if search_name.present?
+    tasks = tasks.where(status: search_status) if search_status.present?
+    if params.key?(:sort)
+      tasks.order(deadline: params[:sort].to_sym, created_at: :desc)
+    else
+      tasks.order(created_at: :desc)
+    end
+  end
 end
