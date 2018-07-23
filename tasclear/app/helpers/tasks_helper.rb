c2dd37c209a3a2fd3924bfa652d6c80a53111950
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-23 12:57:23 +0900

    [change]同じ機能を持つヘルパーメソッドの集約

--- tasclear/app/helpers/tasks_helper.rb
+++ tasclear/app/helpers/tasks_helper.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 module TasksHelper
-  def link_sort_deadline(label_name)
-    if params[:sort_deadline] == 'asc'
+  def link_sort(label_name, category)
+    if category == 'priority'
+      if params[:sort_priority] == 'desc'
+        link_to label_name, root_path(sort_priority: 'asc')
+      else
+        link_to label_name, root_path(sort_priority: 'desc')
+      end
+    elsif params[:sort_deadline] == 'asc'
       link_to label_name, root_path(sort_deadline: 'desc')
     else
       link_to label_name, root_path(sort_deadline: 'asc')
-    end
-  end
-
-  def link_sort_priority(label_name)
-    if params[:sort_priority] == 'desc'
-      link_to label_name, root_path(sort_priority: 'asc')
-    else
-      link_to label_name, root_path(sort_priority: 'desc')
     end
   end
 end
