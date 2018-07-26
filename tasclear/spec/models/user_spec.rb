d279a3ea59758c5282a0e3f75ee9d3ebb4989a11
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-26 10:58:13 +0900

    [change]ユーザモデルスペックをcontext blockを使って整理

--- tasclear/spec/models/user_spec.rb
+++ tasclear/spec/models/user_spec.rb
@@ -4,62 +4,85 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'バリデーション' do
-    it '名前、メールアドレス、パスワードがあれば有効な状態であること' do
-      user = build(:user)
-      expect(user).to be_valid
+    let(:name) { 'ほげ' }
+    let(:email) { 'hoge@sample.com' }
+    let(:password_digest) { 'password' }
+    let(:user) { build(:user, name: name, email: email, password_digest: password_digest) }
+    subject { user }
+
+    context '有効' do
+      context '名前、メールアドレス、パスワードが指定される' do
+        it { is_expected.to be_valid }
+      end
+
+      context '名前が30文字' do
+        let(:name) { 'あ' * 30 }
+        it { is_expected.to be_valid }
+      end
+
+      context 'メールアドレスが255文字' do
+        let(:email) { 'a@a.' + 'a' * 251 }
+        it { is_expected.to be_valid }
+      end
+
+      context 'パスワードが6文字' do
+        let(:password_digest) { 'a' * 6 }
+        it { is_expected.to be_valid }
+      end
+
+      context 'パスワードが30文字' do
+        let(:password_digest) { 'a' * 30 }
+        it { is_expected.to be_valid }
+      end
     end
-    it '名前が無ければ無効な状態であること' do
-      user = build(:user, name: '')
-      expect(user).to be_invalid
-    end
-    it '名前が30文字であれば有効な状態であること' do
-      user = build(:user, name: 'あ' * 30)
-      expect(user).to be_valid
-    end
-    it '名前が31文字であれば無効な状態であること' do
-      user = build(:user, name: 'あ' * 31)
-      expect(user).to be_invalid
-    end
-    it 'メールアドレスが無ければ無効な状態であること' do
-      user = build(:user, email: '')
-      expect(user).to be_invalid
-    end
-    it 'メールアドレスが重複していれば無効な状態であること' do
-      create(:user, email: 'aaa@example.com')
-      user = build(:user, email: 'aaa@example.com')
-      expect(user).to be_invalid
-    end
-    it 'メールアドレスが正しい形式で無ければ無効な状態であること' do
-      user = build(:user, email: 'aaa')
-      expect(user).to be_invalid
-    end
-    it 'メールアドレスが255文字であれば有効な状態であること' do
-      user = build(:user, email: 'a@a.' + 'a' * 251)
-      expect(user).to be_valid
-    end
-    it 'メールアドレスが266文字であれば無効な状態であること' do
-      user = build(:user, email: 'a@a.' + 'a' * 252)
-      expect(user).to be_invalid
-    end
-    it 'パスワードが無ければ無効な状態であること' do
-      user = build(:user, password_digest: '')
-      expect(user).to be_invalid
-    end
-    it 'パスワードが6文字であれば有効な状態であること' do
-      user = build(:user, password_digest: 'a' * 6)
-      expect(user).to be_valid
-    end
-    it 'パスワードが5文字であれば無効な状態であること' do
-      user = build(:user, password_digest: 'a' * 5)
-      expect(user).to be_invalid
-    end
-    it 'パスワードが30文字であれば有効な状態であること' do
-      user = build(:user, password_digest: 'a' * 30)
-      expect(user).to be_valid
-    end
-    it 'パスワードが31文字であれば無効な状態であること' do
-      user = build(:user, password_digest: 'a' * 31)
-      expect(user).to be_invalid
+
+    context '無効' do
+      context '名前が指定されない' do
+        let(:name) { '' }
+        it { is_expected.to be_invalid }
+      end
+
+      context '名前が31文字' do
+        let(:name) { 'あ' * 31 }
+        it { is_expected.to be_invalid }
+      end
+
+      context 'メールアドレスが指定されない' do
+        let(:email) { '' }
+        it { is_expected.to be_invalid }
+      end
+
+      context 'メールアドレスが重複' do
+        before do
+          create(:user, email: email)
+        end
+        it { is_expected.to be_invalid }
+      end
+
+      context 'メールアドレスが正しい形式ではない' do
+        let(:email) { 'hogehoge' }
+        it { is_expected.to be_invalid }
+      end
+
+      context 'メールアドレスが256文字' do
+        let(:email) { 'a@a.' + 'a' * 252 }
+        it { is_expected.to be_invalid }
+      end
+
+      context 'パスワードが指定されない' do
+        let(:password_digest) { '' }
+        it { is_expected.to be_invalid }
+      end
+
+      context 'パスワードが5文字' do
+        let(:password_digest) { 'a' * 5 }
+        it { is_expected.to be_invalid }
+      end
+
+      context 'パスワードが31文字' do
+        let(:password_digest) { 'a' * 31 }
+        it { is_expected.to be_invalid }
+      end
     end
   end
 end
