7d18e8a19ad854393ad6d6812b839cf8f4e8c842
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-30 16:25:16 +0900

    [add]ユーザのCRUD機能追加

---
+++ tasclear/app/controllers/admin/users_controller.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: t('flash.user.create_success')
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def show
+    @tasks = @user.tasks
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: t('flash.user.update_success')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: t('flash.user.destroy_success')
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
