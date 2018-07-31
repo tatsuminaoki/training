d12379524cbbadd7da21385cdaae516bfe6c0754
yusuke_miyazaki <ysk1180@gmail.com>
2018-07-31 09:42:26 +0900

    [change]ネスとしたmodule/classのスタイル変更

--- tasclear/app/controllers/admin/users_controller.rb
+++ tasclear/app/controllers/admin/users_controller.rb
@@ -1,52 +1,54 @@
 # frozen_string_literal: true
 
-class Admin::UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
+module Admin
+  class UsersController < ApplicationController
+    before_action :set_user, only: %i[show edit update destroy]
 
-  def index
-    @users = User.all
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to admin_users_path, notice: t('flash.user.create_success')
-    else
-      render :new
+    def index
+      @users = User.all
     end
-  end
 
-  def edit
-  end
-
-  def show
-    @tasks = @user.tasks
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to admin_users_path, notice: t('flash.user.update_success')
-    else
-      render :edit
+    def new
+      @user = User.new
     end
-  end
 
-  def destroy
-    @user.destroy
-    redirect_to admin_users_path, notice: t('flash.user.destroy_success')
-  end
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to admin_users_path, notice: t('flash.user.create_success')
+      else
+        render :new
+      end
+    end
 
-  private
+    def edit
+    end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
-  end
+    def show
+      @tasks = @user.tasks
+    end
 
-  def set_user
-    @user = User.find(params[:id])
+    def update
+      if @user.update(user_params)
+        redirect_to admin_users_path, notice: t('flash.user.update_success')
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @user.destroy
+      redirect_to admin_users_path, notice: t('flash.user.destroy_success')
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
   end
 end
