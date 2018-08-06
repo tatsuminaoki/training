874d1ed55b5b781b7b3f8e33f787beb2c839dddc
yusuke_miyazaki <ysk1180@gmail.com>
2018-08-06 15:56:21 +0900

    [add]ユーザの新規登録機能追加

---
+++ tasclear/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save!
+    redirect_to root_path, notice: t('flash.user.register_success')
+  rescue StandardError
+    render :new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
--- tasclear/config/routes.rb
+++ tasclear/config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks, except: %i[index]
   resources :sessions, only: %i[new create destroy]
+  resources :users, only: %i[new create]
 end
