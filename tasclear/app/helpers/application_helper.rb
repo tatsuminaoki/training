b1e39652510d995d57a107e3128bda0c48283229
yusuke_miyazaki <ysk1180@gmail.com>
2018-08-07 16:33:44 +0900

    [fix]ラベルの選択項目に自分以外のラベルが出てしまうバグの修正

--- tasclear/app/helpers/application_helper.rb
+++ tasclear/app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def user_labels
+    labels = []
+    Label.all.includes(:tasks).each do |label|
+      label.tasks.each do |task|
+        labels << label.name if task.user == current_user
+      end
+    end
+    labels
+  end
 end
