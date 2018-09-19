2817828e01f9c224d0d072b01b97f4b50aa53b46
KyuSoo Kim <anzel21@naver.com>
2018-09-19 13:53:34 +0900

    [add] i18nの初期設定と画面のタイトル変更

--- KTask/config/application.rb
+++ KTask/config/application.rb
@@ -12,6 +12,9 @@ module KTask
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # Load default locale i18n
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
