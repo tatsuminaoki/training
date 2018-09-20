2940c0a5f25d2d2613d0b70deb7eed0cbe1712a5
KyuSoo Kim <anzel21@naver.com>
2018-09-20 11:05:42 +0900

    [change] フィーチャスペックの国際化

--- KTask/spec/features/tasks_spec.rb
+++ KTask/spec/features/tasks_spec.rb
@@ -6,11 +6,11 @@ RSpec.feature 'Tasks', type: :feature do
   scenario '新しいタスクを作成' do
     expect do
       visit root_path
-      click_link '作成'
-      fill_in 'Title', with: 'First content'
-      fill_in 'Content', with: 'Rspec test'
-      click_button 'Create Task'
-      expect(page).to have_content('Task was successfully create.')
+      click_link I18n.t('tasks.index.new_task')
+      fill_in I18n.t('activerecord.attributes.task.title'), with: 'First content'
+      fill_in I18n.t('activerecord.attributes.task.content'), with: 'Rspec test'
+      click_button I18n.t('helpers.submit.create')
+      expect(page).to have_content I18n.t('flash.task.create')
       expect(page).to have_content('First content')
       expect(page).to have_content('Rspec test')
     end.to change { Task.count }.by(1)
@@ -18,16 +18,17 @@ RSpec.feature 'Tasks', type: :feature do
 
   scenario 'タスクの修正' do
     visit root_path
-    click_link '作成'
-    fill_in 'Title', with: 'Before modify'
-    fill_in 'Content', with: 'Rspec test'
-    click_button 'Create Task'
+    click_link I18n.t('tasks.index.new_task')
+    fill_in I18n.t('activerecord.attributes.task.title'), with: 'Before modify'
+    fill_in I18n.t('activerecord.attributes.task.content'), with: 'Rspec test'
+    click_button I18n.t('helpers.submit.create')
+    click_link I18n.t('tasks.show.back')
     expect do
-      click_link 'Edit'
-      fill_in 'Title', with: 'After modify'
-      fill_in 'Content', with: 'Sleepy morning'
-      click_button 'Update Task'
-      expect(page).to have_content 'Task was successfully updated.'
+      click_link I18n.t('tasks.index.edit')
+      fill_in I18n.t('activerecord.attributes.task.title'), with: 'After modify'
+      fill_in I18n.t('activerecord.attributes.task.content'), with: 'Sleepy morning'
+      click_button I18n.t('helpers.submit.update')
+      expect(page).to have_content I18n.t('flash.task.update')
       expect(page).to have_content 'After modify'
       expect(page).to have_content 'Sleepy morning'
     end.to change { Task.count }.by(0)
@@ -35,13 +36,14 @@ RSpec.feature 'Tasks', type: :feature do
 
   scenario 'タスクの削除' do
     visit root_path
-    click_link '作成'
-    fill_in 'Title', with: 'Delete Test'
-    fill_in 'Content', with: 'Delete this'
-    click_button 'Create Task'
+    click_link I18n.t('tasks.index.new_task')
+    fill_in I18n.t('activerecord.attributes.task.title'), with: 'Delete Test'
+    fill_in I18n.t('activerecord.attributes.task.content'), with: 'Delete this'
+    click_button I18n.t('helpers.submit.create')
+    click_link I18n.t('tasks.show.back')
     expect do
-      click_link '削除'
-      expect(page).to have_content 'Task was successfully destroyed.'
+      click_link I18n.t('tasks.index.delete')
+      expect(page).to have_content I18n.t('flash.task.delete')
     end.to change { Task.count }.by(-1)
   end
 end
--- KTask/spec/models/task_spec.rb
+++ KTask/spec/models/task_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Task, type: :model do
   it 'タスクの有効性検査' do
     task = Task.new(
     title: 'spec test',
-    content: 'this is spec test'
+    content: 'this is spec test',
     )
     expect(task).to be_valid
   end
