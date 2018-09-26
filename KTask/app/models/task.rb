2ade7ad00c2c82c787fe7f54a88ea70e965564e4
KyuSoo Kim <anzel21@naver.com>
2018-09-26 14:31:45 +0900

    [add] タスクのバリデーション追加

--- KTask/app/models/task.rb
+++ KTask/app/models/task.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  enum status: [:yet, :do, :done]
+  enum status: %i[yet do done]
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :content, presence: true, length: { maximum: 255 }
+  validates :status, presence: true
 end
