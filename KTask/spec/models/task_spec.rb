d3a9c5e600f01b2465d9a5132f5d9be520bf4323
KyuSoo Kim <anzel21@naver.com>
2018-09-26 16:26:20 +0900

    [add] バリデーションのモデルスペック追加

--- KTask/spec/models/task_spec.rb
+++ KTask/spec/models/task_spec.rb
@@ -7,7 +7,28 @@ RSpec.describe Task, type: :model do
     task = Task.new(
     title: 'spec test',
     content: 'this is spec test',
+    status: 1,
     )
     expect(task).to be_valid
+  end
+  it 'タスクのタイトル空白検査' do
+    task = Task.new(title: '', content: 'title validation test', status: 0)
+    expect(task).not_to be_valid
+  end
+  it 'タイトル文字の制限テスト' do
+    test_title = ''
+    for i in 0..21 do
+      test_title += 'あ'
+    end
+    task = Task.new(title: test_title, content: 'this is validation test', status: 0)
+    expect(task).not_to be_valid
+  end
+  it 'タスクの内容空白検査' do
+    task = Task.new(title: 'test', content: '', status: 2)
+    expect(task).not_to be_valid
+  end
+  it 'タスクの状態空白検査' do
+    task = Task.new(title: 'test', content: 'status validation test', status: '')
+    expect(task).not_to be_valid
   end
 end
