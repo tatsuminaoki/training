5b737b6e4744633fed43653d3c37c485cabb3792
KyuSoo Kim <anzel21@naver.com>
2018-09-26 15:07:34 +0900

    [change] DBの制約マイグレーション作成

--- KTask/db/schema.rb
+++ KTask/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_042727) do
+ActiveRecord::Schema.define(version: 2018_09_26_055133) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
-    t.string "title"
-    t.string "content"
-    t.integer "status"
-    t.datetime "end_time"
+    t.string "title", limit: 20, null: false
+    t.string "content", null: false
+    t.integer "status", null: false
+    t.datetime "end_time", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
