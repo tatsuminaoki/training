574f6b957a15d042c61bdd0e1cdad351096f30ae
KyuSoo Kim <anzel21@naver.com>
2018-09-27 15:28:09 +0900

    [change] モデルスペックの修正

--- KTask/spec/models/task_spec.rb
+++ KTask/spec/models/task_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it 'タスクの有効' do
-    task = FactoryBot.create(:task)
-    expect(task).to be_valid
-  end
   context 'パラメータが正しい場合' do
+    it 'タスクの有効' do
+      task = FactoryBot.create(:task)
+      expect(task).to be_valid
+    end
     it 'タスクのタイトルが入れている' do
       task = FactoryBot.create(:task, title: 'Test content')
       expect(task).to be_valid
@@ -24,15 +24,15 @@ RSpec.describe Task, type: :model do
   context 'パラメータが正しくない場合' do
     it 'タスクのタイトルが入れていない' do
       task = FactoryBot.create(:task, title: nil)
-      expect(task).to be_valid
+      expect(task).not_to be_valid
     end
     it 'タイトル文字の制限を超える' do
       task = FactoryBot.create(:task, title: 'あ' * 21)
-      expect(task).to be_valid
+      expect(task).not_to be_valid
     end
     it 'タスクの内容が入れていない' do
       task = FactoryBot.create(:task, content: nil)
-      expect(task).to be_valid
+      expect(task).not_to be_valid
     end
   end
 end
