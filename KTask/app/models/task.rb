751daeae9c9f2b90577209695b9e12afd2d9dd4f
KyuSoo Kim <anzel21@naver.com>
2018-10-03 11:49:16 +0900

    [add] モデルに検索の機能具現＆コントローラの修正

--- KTask/app/models/task.rb
+++ KTask/app/models/task.rb
@@ -5,4 +5,17 @@ class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 20 }
   validates :content, presence: true, length: { maximum: 255 }
   validates :status, presence: true
+
+  def self.search_and_order(params)
+    sort = params[:sort]
+    sort = 'created_at' if sort.nil?
+    direction = params[:direction]
+    direction = 'desc' if direction.nil?
+    search_title = params[:search_title]
+    search_status = params[:search_status]
+    tasks = Task.all
+    tasks = tasks.where('title LIKE ?', "%#{search_title}%") if search_title.present?
+    tasks = tasks.where(status: search_status) if search_status.present?
+    tasks.order("#{sort} #{direction}")
+  end
 end
