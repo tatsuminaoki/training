63ce3c67704a6387e7b67a35804c7c9abf7e3a64
KyuSoo Kim <anzel21@naver.com>
2018-10-10 13:27:15 +0900

    [add] ユーザモデルのバリデーション追加

--- KTask/app/models/user.rb
+++ KTask/app/models/user.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { maximum: 12 }
 end
