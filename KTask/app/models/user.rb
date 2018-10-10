9a2e0278bdccb8ccbc53ac06b831e6a7884fcb6d
KyuSoo Kim <anzel21@naver.com>
2018-10-10 15:52:50 +0900

    [add] ユーザ＆タスクの関係を接続のためのインデックス追加/データバース修正

--- KTask/app/models/user.rb
+++ KTask/app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { maximum: 12 }
+
+  has_many :tasks
 end
