f0ab2adda78df94430dff6dd4476564caf786a42
KyuSoo Kim <anzel21@naver.com>
2018-10-11 10:18:17 +0900

    [add&change] パスワードセキュリティ導入(bcrypt Gem)＆カラム名変更

--- KTask/db/schema.rb
+++ KTask/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_064351) do
+ActiveRecord::Schema.define(version: 2018_10_11_010912) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", limit: 20, null: false
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2018_10_10_064351) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password", limit: 12, null: false
+    t.string "password_digest", limit: 12, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
