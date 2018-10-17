3ca49366d1946ed77ee574857a4721906cbad783
KyuSoo Kim <anzel21@naver.com>
2018-10-17 13:11:16 +0900

    [add] ログイン機能実装・フラッシュメッセージ日本語化

--- KTask/app/controllers/sessions_controller.rb
+++ KTask/app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: t('flash.session.login_success')
+    else
+      flash.now[:danger] = t('flash.session.login_failed')
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
--- KTask/app/models/user.rb
+++ KTask/app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: { maximum: 50 }
-  validates :password_digest, presence: true, length: { maximum: 12 }
+  validates :password, presence: true, length: { maximum: 12 }
 
   has_many :tasks
   has_secure_password
