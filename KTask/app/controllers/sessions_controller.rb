027722e3523f097e94436ae7f6c731b7bf26c0c2
KyuSoo Kim <anzel21@naver.com>
2018-10-22 10:34:41 +0900

    [change] &演算子の変更

--- KTask/app/controllers/sessions_controller.rb
+++ KTask/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to index_url, notice: t('flash.session.login_success')
     else
