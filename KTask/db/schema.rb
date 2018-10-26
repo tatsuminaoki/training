351071ad250136b891e44c327224a3dad2c123b3
KyuSoo Kim <anzel21@naver.com>
2018-10-26 15:32:25 +0900

    [add] ユーザロールカラム追加

--- KTask/db/schema.rb
+++ KTask/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_001554) do
+ActiveRecord::Schema.define(version: 2018_10_26_062320) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", limit: 20, null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_10_19_001554) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0, null: false
   end
 
   add_foreign_key "tasks", "users"
