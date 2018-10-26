c0e009c47778519176d1a9c63301c07a024be9e6
KyuSoo Kim <anzel21@naver.com>
2018-10-26 15:39:01 +0900

    [add] ユーザロールenum追加

--- KTask/app/models/user.rb
+++ KTask/app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+ enum role: %i[normal admin]
+
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: { maximum: 50 }
   validates :password, presence: true, length: { maximum: 12 }
