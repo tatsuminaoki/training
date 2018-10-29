2315c6106b6b8587c5bd538557b890c630897f55
KyuSoo Kim <anzel21@naver.com>
2018-10-29 10:50:12 +0900

    [add] ユーザロール追加

--- KTask/app/models/user.rb
+++ KTask/app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: { maximum: 50 }
   validates :password, presence: true, length: { maximum: 12 }
+  validates :role, presence: true
 
   has_many :tasks, dependent: :destroy
   has_secure_password
