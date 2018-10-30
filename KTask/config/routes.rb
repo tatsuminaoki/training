96665bdb03bba57eb097dd212dd02a198fd6775b
KyuSoo Kim <anzel21@naver.com>
2018-10-30 10:13:44 +0900

    [change] 初期画面のユーザ登録を一般ユーザの登録に分離

--- KTask/config/routes.rb
+++ KTask/config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'index' => 'tasks#index'
   resources :tasks
   resources :sessions, only: %i[new create destroy]
+  #一般ユーザのrouting
+  resources :users
   root 'sessions#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
