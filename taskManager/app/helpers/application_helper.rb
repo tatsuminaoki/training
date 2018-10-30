edd895821eeebb96803d7f6e59c7409e27c1f551
Iizuka <hajime.a.iizuka@rakuten.com>
2018-10-30 17:45:22 +0900

    カラムソートのアイコン忘れていたので追加

--- taskManager/app/helpers/application_helper.rb
+++ taskManager/app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   def sortable(column:, title: nil)
     title ||= column.titleize
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    css = (column == sort_column) ? "fa fa-sort-#{direction}" : "fa fa-sort"
     # TODO:CSSの切り替えも実施すること
-    link_to title, {:sort => column, :direction => direction}
+    link_to title, {:sort => column, :direction => direction}, {:class => css}
   end
 end
