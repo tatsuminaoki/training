1a21dff0b21b331e17e24ca356664eb02cbb3307
KyuSoo Kim <anzel21@naver.com>
2018-10-31 15:53:27 +0900

    [change] before_action名変更

--- KTask/app/controllers/admin/users_controller.rb
+++ KTask/app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@
 module Admin
   class UsersController < ApplicationController
     before_action :set_user, only: %i[show edit update destroy]
-    before_action :auth_check, only: %i[show edit update destroy]
+    before_action :check_auth, only: %i[show edit update destroy]
 
     def index
       case current_user.role
