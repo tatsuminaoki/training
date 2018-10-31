36ea06b52739f0d65dce1130d34dbe44a49e4e0b
Iizuka <hajime.a.iizuka@rakuten.com>
2018-10-31 17:28:19 +0900

    レビュー後の修正

--- taskManager/app/helpers/list_helper.rb
+++ taskManager/app/helpers/list_helper.rb
@@ -1,37 +1,20 @@
 module ListHelper
   def task_status_message(status)
     status_str = I18n.t status, scope: "enum.task.status"
-    case status
-    when "waiting" then
-      label_str = "label-success"
-    when "working" then
-      label_str = "label-primary"
-    else
-      label_str = "label-danger"
-    end
-
-    %(<span class="label #{label_str}">#{status_str}</span>).html_safe
+    raw %(<span class="label label-#{status}">#{status_str}</span>)
   end
 
   def task_priority_message(priority)
     priority_str = I18n.t priority, scope: "enum.task.priority"
-    case priority
-    when "low" then
-      label_str = "label-success"
-    when "common" then
-      label_str = "label-primary"
-    else
-      label_str = "label-danger"
-    end
-    %(<span class="label #{label_str}">#{priority_str}</span>).html_safe
+    raw %(<span class="label label-#{priority}">#{priority_str}</span>)
   end
 
   def label_message(label_name)
-    %(<span class="label label-warning">#{label_name}</span>).html_safe
+    raw %(<span class="label label-warning">#{label_name}</span>)
   end
 
   def description_message(description)
-    %(<pre>#{description}</pre>).html_safe
+    raw %(<pre>#{description}</pre>)
   end
 
   def action_contents
