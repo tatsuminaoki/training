1ab73078a5fb522a64262a01d6108c4ed88f2f64
itinao <itinao@gmail.com>
2019-01-21 11:47:35 +0900

    add models

---
+++ todoapp/db/migrate/20190121015950_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email, :null => false, :limit => 64, index: { unique: true }
+      t.string :encrypted_password, :null => false
+      t.string :name, :null => false, :limit => 64
+      t.references :group, :null => true, foreign_key: true               
+      t.unsigned_integer :role, :null => false, :limit => 1
+
+      t.timestamps null:false
+    end
+  end
+end
