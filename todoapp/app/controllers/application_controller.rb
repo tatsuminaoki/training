a8b55118d56dc2ddf475e75949169bd216e63e62
itinao <itinao@gmail.com>
2019-01-22 16:23:41 +0900

    add CRUD

--- todoapp/app/controllers/application_controller.rb
+++ todoapp/app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+  before_action :login_required
+
+  private
+
+  def current_user
+    # TODO: あとでログインできるようにするよ
+    user = User.find_by(email: 'aaaa@gmail.com')
+    if user
+      return @current_user = user
+    end
+
+    params = {
+        email: 'aaaa@gmail.com',
+        encrypted_password: 'aaa',
+        name: 'aaa',
+        group_id: nil,
+        role: 1
+    }
+    user = User.new(params)
+    user.save!
+
+    @current_user = user
+  end
+
+  def login_required
+    redirect_to login_path unless current_user
+  end
 end
--- todoapp/app/models/task.rb
+++ todoapp/app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+  STATUS_NEW_TASK = 1
+  STATUS_WORKING = 2
+  STATUS_COMPLETED = 3
+  enum status: {
+    new_task: STATUS_NEW_TASK,
+    working: STATUS_WORKING,
+    completed: STATUS_COMPLETED
+  }
+
+  belongs_to :user
+
 end
