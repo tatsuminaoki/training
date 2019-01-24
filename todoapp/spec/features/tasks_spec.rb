f0db0b5b8724411e63dd7270e303043b823c2180
itinao <itinao@gmail.com>
2019-01-24 16:47:58 +0900

    指摘対応

--- todoapp/spec/features/tasks_spec.rb
+++ todoapp/spec/features/tasks_spec.rb
@@ -29,20 +29,18 @@ describe 'タスク管理機能', type: :feature do
   end
 
   describe '新規作成機能' do
-    let(:current_date) { Time.now }
-
     before do
       visit new_task_path
 
       # タスクのフォーム入力
       fill_in :task_title, with: 'たすくだよ'
       fill_in :task_description, with: 'たすくのせつめいだよ'
-      fill_in :task_end_at, with: current_date.to_date
+      fill_in :task_end_at, with: '2100-01-01'
 
       # フォームの内容をチェック
       expect(page).to have_field :task_title, with: 'たすくだよ'
       expect(page).to have_field :task_description, with: 'たすくのせつめいだよ'
-      expect(page).to have_field :task_end_at, with: current_date.to_date
+      expect(page).to have_field :task_end_at, with: '2100-01-01'
 
       # 作成
       click_button 'Create Task'
@@ -52,7 +50,7 @@ describe 'タスク管理機能', type: :feature do
       example '登録後の画面で内容が正常に表示される' do
         expect(page).to have_content 'たすくだよ'
         expect(page).to have_content 'たすくのせつめいだよ'
-        expect(page).to have_content current_date.strftime('%Y年%m月%d日')
+        expect(page).to have_content '2100年01月01日'
       end
     end
   end
