0de983e1df4e3e5e8de2c8a08c61c9712d3c9683
itinao <itinao@gmail.com>
2019-01-24 14:55:07 +0900

    add error-page

--- todoapp/app/controllers/application_controller.rb
+++ todoapp/app/controllers/application_controller.rb
@@ -1,6 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :login_required
 
+  unless Rails.env.development?
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from Exception, with: :render_500
+  end
+
+  def render_404
+    render template: 'errors/error_404',
+           status: 404,
+           layout: 'application',
+           content_type: 'text/html'
+  end
+
+  def render_500
+    render template: 'errors/error_500',
+           status: 500,
+           layout: 'application',
+           content_type: 'text/html'
+  end
+
   private
 
   def current_user
