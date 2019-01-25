4423bd7dd69509d572174eec3b951dcfe8b1feba
hoooori <hideyasu.horiuchi@gmail.com>
2019-01-25 09:48:59 +0900

    バリデーションを設定。featureとmodelのテストを追加。

--- task_app/app/models/task.rb
+++ task_app/app/models/task.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description, presence: true, length: { maximum: 200 }
 end
