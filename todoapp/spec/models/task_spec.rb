db6c9bf235b4fbfc6616472c87e93eeedc83bc4d
itinao <itinao@gmail.com>
2019-01-25 17:53:26 +0900

    指摘対応

--- todoapp/spec/models/task_spec.rb
+++ todoapp/spec/models/task_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Task, type: :model do
-  let(:user_a) { create(:user, name: 'ユーザーA') }
+  let(:user_a) { build(:user, name: 'ユーザーA') }
 
   describe 'titleのバリデーションチェック' do
     context '何も入力されてないとき' do
@@ -13,42 +13,49 @@ describe Task, type: :model do
     end
     context '1文字入力されてる時' do
       example '登録可能' do
-        task = create(:task, user: user_a, title: '1')
+        task = build(:task, user: user_a, title: '1')
         expect(task).to be_valid
       end
     end
     context '64文字入力されてる時' do
       example '登録可能' do
-        task = create(
-            :task,
-            user: user_a,
-            title: '1234567890123456789012345678901234567890123456789012345678901234')
+        task = build(:task, user: user_a, title: 'a' * 64)
         expect(task).to be_valid
       end
     end
     context '65文字入力されてる時' do
       example 'エラーになる' do
         expect{
-          create(
-              :task,
-              user: user_a,
-              title: '12345678901234567890123456789012345678901234567890123456789012345')
+          create(:task, user: user_a, title: 'a' * 65)
         }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end
   end
 
   describe 'end_atのバリデーションチェック' do
-    context '過去日付が入力された時' do
+    context '過去日付が入力された時1' do
       example 'エラーになる' do
         expect{
           create(:task, user: user_a, end_at: '2019-01-01 23:59:59')
         }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end
+    context '過去日付が入力された時2' do
+      example 'エラーになる' do
+        expect{
+          create(:task, user: user_a, end_at: Time.now.to_date - 1)
+        }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+    context '現在日付が入力された時' do
+      example '登録可能' do
+        task = build(:task, user: user_a, end_at: Time.now.to_date)
+        expect(task).to be_valid
+      end
+    end
     context '未来日付が入力された時' do
       example '登録可能' do
-        task = create(:task, user: user_a, end_at: '2100-01-01 23:59:59')
+        task = build(:task, user: user_a, end_at: '2100-01-01 23:59:59')
         expect(task).to be_valid
       end
     end
