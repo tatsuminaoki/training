8699bee3ae01ef17faf3cfaf209aed836dd4b3d3
itinao <itinao@gmail.com>
2019-02-05 15:46:42 +0900

    add seeds

--- todoapp/db/seeds.rb
+++ todoapp/db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+userobj = {
+  _email: ['aaaaa', 0, '@gmail.com'],
+  encrypted_password: 'passwaaada',
+  name: 'おなまえ',
+  group_id: nil,
+  role: 1
+}
+
+taskobj = {
+  title: 'たすくだよ',
+  description: 'たすくだよー',
+  status: 1
+}
+
+# add users
+10.times do |i|
+  userobj[:_email][1] = i
+  user = User.create(
+    email: userobj[:_email].join,
+    encrypted_password: userobj[:encrypted_password],
+    name: userobj[:name],
+    group_id: userobj[:group_id],
+    role: userobj[:role]
+  )
+
+  # add tasks
+  10.times do |_i|
+    Task.create(
+      user_id: user.id,
+      title: taskobj[:title],
+      description: taskobj[:description],
+      status: taskobj[:status]
+    )
+  end
+end
