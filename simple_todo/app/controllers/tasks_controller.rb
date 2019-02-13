3df7ceb43967c331cdd113beeea99c7f3355df79
Sangjin Oh <sangjin_oh@fablic.co.jp>
2019-02-13 13:08:49 +0900

    remove unnecessary file/comment

--- simple_todo/app/controllers/tasks_controller.rb
+++ simple_todo/app/controllers/tasks_controller.rb
@@ -13,50 +13,36 @@ class TasksController < ApplicationController
   end
 
   def edit
-      @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def create
     @task = Task.new(task_params)
-
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_to @task, notice: 'Task was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      redirect_to @task, notice: 'Task was successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      redirect_to tasks_url, notice: 'Task was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
       params.require(:task).permit(:title, :description, :user_id, :limit, :priority, :status)
     end
--- simple_todo/app/views/tasks/_task.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! task, :id, :title, :description, :user_id, :limit, :priority, :status, :created_at, :updated_at
-json.url task_url(task, format: :json)
