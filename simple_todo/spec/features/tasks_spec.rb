35be4d15841e23015df37ad6c33e8791d7215a55
Sangjin Oh <sangjin_oh@fablic.co.jp>
2019-02-14 16:04:58 +0900

    add factorybot/add test

--- simple_todo/spec/features/tasks_spec.rb
+++ simple_todo/spec/features/tasks_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "tasks", type: :feature do
 
   background do
-    Task.create!(title: 'test title', description: 'desc', user_id: 1, limit: '2019-12-13 08:13:54', priority: 1, status: 1)
+    task = FactoryBot.create(:task)
   end
 
   scenario "task list" do
@@ -32,6 +32,25 @@ RSpec.feature "tasks", type: :feature do
     expect(page).to have_content 'Task was successfully created.'
   end
 
+  scenario "task add count" do
+    visit new_task_path
+
+    expect{
+      fill_in 'Title', with: 'test Title'
+      fill_in 'Description', with: 'test Description'
+      fill_in 'User', with: '1'
+      select '2024', from: 'task_limit_1i'
+      select 'December', from: 'task_limit_2i'
+      select '29', from: 'task_limit_3i'
+      select '22', from: 'task_limit_4i'
+      select '59', from: 'task_limit_5i'
+      select '5', from: 'task_priority'
+      fill_in 'Status', with: '1'
+
+      click_button 'Create Task'
+    }.to change{Task.count}.by(1)
+  end
+
   scenario "task edit" do
     visit tasks_path
     click_link 'Edit'
@@ -52,5 +71,8 @@ RSpec.feature "tasks", type: :feature do
     expect(page).to have_content 'Task was successfully destroyed.'
   end
 
-
+  scenario "task delete count" do
+    visit tasks_path
+    expect{click_link 'Destroy'}.to change{Task.count}.by(-1)
+  end
 end
