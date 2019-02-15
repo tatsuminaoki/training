7a54000fe05e1a0880178b2463cbd1dd60e77c4e
Sangjin Oh <sangjin_oh@fablic.co.jp>
2019-02-15 17:25:21 +0900

    add locale / custom error pages

--- simple_todo/config/routes.rb
+++ simple_todo/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root 'tasks#index'
+  
 
   resources :tasks
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
+
 end
