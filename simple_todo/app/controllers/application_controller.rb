9783391878bf84a5b0cad5756a37eedfa963ac0d
Sangjin Oh <sangjin_oh@fablic.co.jp>
2019-02-18 12:45:29 +0900

    fix error page

--- simple_todo/app/controllers/application_controller.rb
+++ simple_todo/app/controllers/application_controller.rb
@@ -1,21 +1,3 @@
 class ApplicationController < ActionController::Base
-    rescue_from Exception,                      with: :_render_500
-    rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
-    rescue_from ActionController::RoutingError, with: :_render_404
 
-  def routing_error
-    raise ActionController::RoutingError, params[:path]
-  end
-
-  private
-
-  def _render_404(e = nil)
-    logger.info "Rendering 404 with exception: #{e.message}" if e
-    render 'errors/error_404', status: :not_found
-  end
-
-  def _render_500(e = nil)
-    logger.error "Rendering 500 with exception: #{e.message}" if e
-    render 'errors/error_500', status: :internal_server_error  
-  end
 end
