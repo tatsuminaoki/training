e0878693da7bf58f9862189be4523c1be7a105aa
Sangjin Oh <sangjin_oh@fablic.co.jp>
2019-02-19 12:05:44 +0900

    add validation/db migration

--- simple_todo/app/models/task.rb
+++ simple_todo/app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   default_scope -> {order(created_at: :desc)}
+
+  validates :title, presence:  { message: " must be given please" }
 end
+    
---
+++ simple_todo/db/migrate/20190219024640_change_column_to_tasks.rb
@@ -0,0 +1,4 @@
+class ChangeColumnToTasks < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
