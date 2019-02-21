ab087e0d5c68a9f227fcbaf8ca5ab3d090a72a18
Sangjin Oh <sangjin_oh@fablic.co.jp>
2019-02-21 16:23:53 +0900

    fix validation comment

--- simple_todo/spec/models/task_spec.rb
+++ simple_todo/spec/models/task_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
     
-    it 'input all require contents' do
+    it 'valid if task has all required contents' do
       task = create(:task)
       expect(task).to be_valid
     end
 
-    it 'missing require contents ng' do
+    it 'invalid if one or more required contents are missing' do
       task = Task.new(
         title: '',
         description: '',
@@ -18,32 +18,32 @@ RSpec.describe Task, type: :model do
       expect(task).to be_invalid
     end
 
-    it 'title max length is 40' do
+    it 'valid if title length is 40' do
       task = build(:task, title: 'a'*40)
       expect(task).to be_valid
     end
 
-    it 'title max length is 41' do
+    it 'invalid if title is too long' do
       task = build(:task, title: 'a'*41)
       expect(task).to be_invalid
     end
 
-    it 'description length is 200' do
+    it 'valid if description length is 200' do
       task = build(:task, description: 'a'*200)
       expect(task).to be_valid
     end
 
-    it 'description length is 201' do
+    it 'invalid if description is too long' do
       task = build(:task, description: 'a'*201)
       expect(task).to be_invalid
     end
 
-    it 'task limit ok' do
+    it 'valid if task limit is future' do
       task = build(:task, limit: '2019-03-28 12:29:00')
       expect(task).to be_valid
     end
 
-    it 'task limit is past' do
+    it 'invalid if task limit is past' do
       task = build(:task, limit: Date.today - 1)
       expect(task).to be_invalid
     end
