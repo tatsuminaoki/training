359a5d9a4125053cf3b731d429db0554e3fe3fad
yui-yoshida <yui.0618.kj8@gmail.com>
2019-02-28 14:05:29 +0900

    WIP factory botにてテストデータの挿入

--- task_list/spec/features/tasks_spec.rb
+++ task_list/spec/features/tasks_spec.rb
@@ -1,39 +1,60 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-	scenario "新規タスクの作成" do
+	scenario "root_pathから投稿ページに遷移すること" do
 		visit root_path
 		click_link "投稿"
-		fill_in "Name", with: "Math"
-		fill_in "Priority", with: "1"
-		fill_in "Status", with: "1"
-		click_button "Create Task"
+		expect(page).to have_content "タスク投稿"
+  end
+	scenario "新規タスクの作成" do
+		visit new_task_path
+		create(:task)
+		click_button "登録する"
 		expect(page).to have_content "タスクを作成しました！"
 		expect(page).to have_content "Math"
 	end
-	scenario "タスクの編集" do
+	scenario "root_pathから編集ページに遷移すること" do
+		visit new_task_path
+		fill_in "タスク名", with: "Math"
+		fill_in "優先順位", with: "1"
+		fill_in "ステータス", with: "1"
+		click_button "登録する"
 		visit root_path
-		click_link "投稿"
-		fill_in "Name", with: "Math"
-		fill_in "Priority", with: "1"
-		fill_in "Status", with: "1"
-		click_button "Create Task"
 		click_link "詳細"
 		click_link "編集"
-		fill_in "Name", with: "English"
-		fill_in "Priority", with: "1"
-		fill_in "Status", with: "1"
-		click_button "Update Task"
+		expect(page).to have_content "編集画面"
+  end
+	scenario "タスクの編集" do
+		visit new_task_path
+		fill_in "タスク名", with: "Math"
+		fill_in "優先順位", with: "1"
+		fill_in "ステータス", with: "1"
+		click_button "登録する"
+		click_link "詳細"
+		click_link "編集"
+		fill_in "タスク名", with: "English"
+		fill_in "優先順位", with: "1"
+		fill_in "ステータス", with: "1"
+		click_button "更新する"
 		expect(page).to have_content "タスクを編集しました！"
 		expect(page).to have_content "English"
 	end
-	scenario "タスクの削除" do
+	scenario "root_pathから削除ページに遷移すること" do
+		visit new_task_path
+		fill_in "タスク名", with: "Math"
+		fill_in "優先順位", with: "1"
+		fill_in "ステータス", with: "1"
+		click_button "登録する"
 		visit root_path
-		click_link "投稿"
-		fill_in "Name", with: "Math"
-		fill_in "Priority", with: "1"
-		fill_in "Status", with: "1"
-		click_button "Create Task"
+		click_link "詳細"
+		expect(page).to have_content "削除"
+  end
+	scenario "タスクの削除" do
+		visit new_task_path
+		fill_in "タスク名", with: "Math"
+		fill_in "優先順位", with: "1"
+		fill_in "ステータス", with: "1"
+		click_button "登録する"
 		click_link "詳細"
 		click_link "削除"
 		expect(page).to have_content "タスクを削除しました！"
