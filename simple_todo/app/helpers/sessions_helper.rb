40b68f1c13a8bcd86dd2d842f9f5793730e3f46e
Sangjin Oh <sangjin_oh@fablic.co.jp>
2019-02-28 15:49:51 +0900

    add login view/contollers

--- simple_todo/app/helpers/sessions_helper.rb
+++ simple_todo/app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def user_signed_in?
+    !current_user.nil?
+  end
 end
