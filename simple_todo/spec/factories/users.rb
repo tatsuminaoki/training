fcdce16248cdb8ade18c84f6e6723c5e8221ddfe
Sangjin Oh <sangjin_oh@fablic.co.jp>
2019-02-28 12:50:32 +0900

    add bcrypt/ change calumn name to password_digest

--- simple_todo/spec/factories/users.rb
+++ simple_todo/spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "test user #{n}"}
     sequence(:email) { |n| "test#{n}@test.com"}
-    password {'password'}
+    password_digest {'password'}
     admin_flag {0}
   end
 end
