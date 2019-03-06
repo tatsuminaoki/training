9a1e937fe0af414808e5ee48b1bdd597cdd35cd2
yui-yoshida <yui.0618.kj8@gmail.com>
2019-03-06 12:00:10 +0900

    [STEP10]タスク一覧を作成日付順に並び替え

--- task_list/spec/features/tasks_spec.rb
+++ task_list/spec/features/tasks_spec.rb
@@ -43,4 +43,12 @@ RSpec.feature "Tasks", type: :feature do
     click_link '削除'
     expect(page).to have_content 'タスクを削除しました！'
   end
+  scenario 'タスク一覧が作成日時の順番で並ぶこと' do
+    Task.create(name: 'first', priority: 1, status: 1)
+    Task.create(name: 'second', priority: 1, status: 1, created_at: Time.current + 1.days)
+    visit tasks_path
+    task = all('table td')
+    task_0 = task[0]
+    expect(task_0).to have_content 'second'
+  end
 end
