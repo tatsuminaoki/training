6123c3095c43f6c5b972c551e2c3090df7aea003
yui-yoshida <yui.0618.kj8@gmail.com>
2019-03-20 09:33:59 +0900

    userテーブルのemailにもuniq制限を追加

--- task_list/db/schema.rb
+++ task_list/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_031450) do
+ActiveRecord::Schema.define(version: 2019_03_20_002512) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "name", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_03_19_031450) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "tasks", "users"
