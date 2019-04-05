cafc9ba82e471c83cc49335d213658120d888eaa
ToshisukeOshikata <toshisuke_oshikata@fablic.co.jp>
2019-04-05 16:38:50 +0900

    タスクモデルを生成

---
+++ task_management/db/migrate/20190405073722_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string, :task_name
+      t.string :contents
+
+      t.timestamps
+    end
+  end
+end
