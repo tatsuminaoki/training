ec210a77e9d8011d5ca8c179fff0863626b65d10
ToshisukeOshikata <toshisuke_oshikata@fablic.co.jp>
2019-04-05 15:09:07 +0900

    マイグレーションファイルを修正
    プライマリーが適切に設定されていなかったのを修正

--- task_management/db/migrate/20190405032501_create_tasks.rb
+++ task_management/db/migrate/20190405032501_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks, id: false do |t|
-      t.integer :task_id, 'INTEGER PRIMARY KEY AUTO_INCREMENT'
+      t.column :task_id, 'INTEGER PRIMARY KEY AUTO_INCREMENT'
       t.string :task_name
       t.string :contents
 
