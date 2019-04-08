f99fe5953abcb13ef25ff2676253ce73d930dbb9
ToshisukeOshikata <toshisuke_oshikata@fablic.co.jp>
2019-04-08 15:59:46 +0900

    新規作成機能追加
    タスクの新規作成機能を追加

--- task_management/app/controllers/tasks_controller.rb
+++ task_management/app/controllers/tasks_controller.rb
@@ -2,4 +2,22 @@ class TasksController < ApplicationController
   def list
     @tasks = Task.all
   end
+
+  def new_task
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new_task'
+    end
+  end
+
+  def task_params
+    params.require(:task).permit(:task_name, :contents)
+  end
+
 end
