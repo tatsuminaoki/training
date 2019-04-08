64fa4181511000632179d9d91b300fad5fb204d4
toyonaga <shuhei_toyonaga@fablic.co.jp>
2019-04-08 12:48:13 +0900

    タスクのコントローラを作成する

---
+++ tudu/app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+  # 一覧
+  def index
+  end
+
+  # TODO: 新規作成
+  def new
+  end
+
+  # TODO: 新規作成保存
+  def create
+  end
+
+  # TODO: 編集
+  def edit
+  end
+
+  # TODO: 編集保存
+  def update
+  end
+
+  # TODO: 削除
+  def destroy
+  end
+end
