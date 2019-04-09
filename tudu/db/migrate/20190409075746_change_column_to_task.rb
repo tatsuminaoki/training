34aaefe8eb5c87b9d090dea69965651ce108f300
toyonaga <shuhei_toyonaga@fablic.co.jp>
2019-04-09 17:12:06 +0900

    データベースにnull制約の追加

---
+++ tudu/db/migrate/20190409075746_change_column_to_task.rb
@@ -0,0 +1,16 @@
+class ChangeColumnToTask < ActiveRecord::Migration[5.2]
+  ### def change
+  ###   change_column :tasks, :name, :string, limit: 50, null: false
+  ###   change_column :tasks, :content, :text, null: false
+  ### end
+
+  def up
+    change_column :tasks, :name, :string, limit: 50, null: false
+    change_column :tasks, :content, :text, null: false
+  end
+
+  def down
+    change_column :tasks, :name, :string, limit: 50, null: true
+    change_column :tasks, :content, :text, null: true
+  end
+end
--- tudu/db/schema.rb
+++ tudu/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_074017) do
+ActiveRecord::Schema.define(version: 2019_04_09_075746) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name", limit: 50
-    t.text "content"
+    t.string "name", limit: 50, null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
