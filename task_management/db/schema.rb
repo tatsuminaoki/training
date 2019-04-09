bd791ebf7fc489cd3270dd71ab12da591bc91b16
ToshisukeOshikata <toshisuke_oshikata@fablic.co.jp>
2019-04-09 09:57:20 +0900

    プルリクで指摘のあった点を修正
    タスク名をuniq制約及びDEFAULT NULL不許可に修正。

--- task_management/db/schema.rb
+++ task_management/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_073722) do
+ActiveRecord::Schema.define(version: 2019_04_09_004451) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "task_name"
