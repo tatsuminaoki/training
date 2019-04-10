1f0db3b662ccf98346c87dce6af8fbdbbcf1467c
toyonaga <shuhei_toyonaga@fablic.co.jp>
2019-04-10 12:14:58 +0900

    終了期限を登録するように修正

--- tudu/app/controllers/tasks_controller.rb
+++ tudu/app/controllers/tasks_controller.rb
@@ -56,7 +56,7 @@ class TasksController < ApplicationController
 
     def task_params
       params.require(:task).permit(
-        :name, :content
+        :name, :content, :expire_date
       )
     end
 end
