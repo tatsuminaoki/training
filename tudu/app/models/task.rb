c5ca16a1a437b33c1150b6969aad51cd0afa06c4
toyonaga <shuhei_toyonaga@fablic.co.jp>
2019-04-10 10:11:18 +0900

    バリデートメッセージを自分で指定したものを適用

--- tudu/app/models/task.rb
+++ tudu/app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  validates :name, 
+    presence: { message: I18n.translate("validates.presence") },
+    length: { maximum: 50, message: I18n.translate("validates.length") }
+  validates :content, 
+    presence: { message: I18n.translate("validates.presence") },
+    length: { maximum: 500, message: I18n.translate("validates.length") }
 end
