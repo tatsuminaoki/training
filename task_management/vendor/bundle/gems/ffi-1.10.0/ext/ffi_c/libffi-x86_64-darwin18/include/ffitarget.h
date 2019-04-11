71e421e9e6345a1a748a7a2bdd57dc5b489f5b01
ToshisukeOshikata <toshisuke_oshikata@fablic.co.jp>
2019-04-11 16:00:41 +0900

    .gitignore編集
    vender以下のフォルダを無視

--- task_management/vendor/bundle/gems/ffi-1.10.0/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/ts-toshisuke.oshikat/training/training/task_management/vendor/bundle/gems/ffi-1.10.0/ext/ffi_c/libffi/src/x86/ffitarget.h
--- task_management/vendor/bundle/ruby/2.6.0/gems/ffi-1.10.0/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/ts-toshisuke.oshikat/training/training/task_management/vendor/bundle/ruby/2.6.0/gems/ffi-1.10.0/ext/ffi_c/libffi/src/x86/ffitarget.h
