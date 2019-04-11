c8db02fcfc1c83fe5dfa42a5701c32e8667a4830
ToshisukeOshikata <toshisuke_oshikata@fablic.co.jp>
2019-04-11 17:12:06 +0900

    git addし忘れ

---
+++ task_management/app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    task_id = params[:id]
+    @task = Task.find(task_id)
+  end
+
+  def edit
+    task_id = params[:id]
+    @task = Task.find(task_id)
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice] = "新規タスクを追加しました"
+      redirect_to root_path
+    else
+      render 'new_task'
+    end
+  end
+
+  def update
+    task_id = params[:id]
+    task = Task.find(task_id)
+    @task = task.update(task_params)
+    if @task
+      flash[:notice] = "タスクを編集しました"
+      redirect_to task_path(task_id)
+    else
+      render 'edit_task'
+    end
+  end
+
+  def destroy
+    task_id = params[:id]
+    task = Task.find(task_id)
+    task.destroy
+    flash[:notice] = "タスクを削除しました"
+    redirect_to root_path
+  end
+
+  def task_params
+    params.require(:task).permit(:task_name, :contents)
+  end
+
+end
