13bbc8dcb879c030ec92d6aedee263a4fecb89f0
ToshisukeOshikata <toshisuke_oshikata@fablic.co.jp>
2019-04-11 17:08:08 +0900

    .gitignore適用
    キャッシュ削除処理。vendorフォルダ以下無視

--- task_management/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root 'tasks#index'
-  resources :tasks
-  get '*path', to: 'application#error_404'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
--- task_management/test/controllers/tasks_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TasksControllerTest < ActionDispatch::IntegrationTest
-  test "should get list" do
-    get tasks_list_url
-    assert_response :success
-  end
-
-end
