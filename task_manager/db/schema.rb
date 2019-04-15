936a12d08acf6fee75e319a53e2f9f68e413cc6d
katoken <kentaro_kato@fablic.co.jp>
2019-04-15 11:22:37 +0900

    rails g model task name:string description:text

--- task_manager/db/schema.rb
+++ task_manager/db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_04_15_021153) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
