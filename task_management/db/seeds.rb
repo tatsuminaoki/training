95d01c90c09fd8c0534b72f3f673e38c0188a23d
ToshisukeOshikata <toshisuke_oshikata@fablic.co.jp>
2019-04-15 16:55:17 +0900

    seedsファイル追記

--- task_management/db/seeds.rb
+++ task_management/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  user_name: 'oshikata',
+  password: '111111',
+)
