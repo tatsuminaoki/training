7b9c842b879b76e734ca7cf2698cb545c8601752
ToshisukeOshikata <toshisuke_oshikata@fablic.co.jp>
2019-04-15 16:16:46 +0900

    ページネーション導入
    絞り込みにも導入

--- task_management/app/controllers/tasks_controller.rb
+++ task_management/app/controllers/tasks_controller.rb
@@ -7,9 +7,9 @@ class TasksController < ApplicationController
     items_limit = 5
 
     if task_name.present?
-      @tasks = Task.where(task_name: task_name)
+      @tasks = Task.where(task_name: task_name).page(params[:page]).per(items_limit)
     elsif status_id.present?
-      @tasks = Task.where(status_id: status_id)
+      @tasks = Task.where(status_id: status_id).page(params[:page]).per(items_limit)
     else
       @tasks = Task.page(params[:page]).per(items_limit)
     end
