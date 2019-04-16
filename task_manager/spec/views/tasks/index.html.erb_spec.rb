b13e37de4a4fe5137e3b232f98bb0cc97a7e0c60
katoken <kentaro_kato@fablic.co.jp>
2019-04-16 16:59:23 +0900

    set fablicop and bundle exec rubocop -a

--- task_manager/spec/views/tasks/index.html.erb_spec.rb
+++ task_manager/spec/views/tasks/index.html.erb_spec.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'tasks/index', type: :view do
   before(:each) do
     assign(:tasks, [
-      Task.create!(),
-      Task.create!()
-    ])
+             Task.create!(),
+             Task.create!(),
+           ])
   end
 
   it 'renders a list of tasks' do
