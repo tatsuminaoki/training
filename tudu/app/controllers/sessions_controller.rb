9134b5b7edb317dfa4d031796996f485f9901b76
toyonaga <shuhei_toyonaga@fablic.co.jp>
2019-04-17 14:15:30 +0900

    ログインの画面を作成

--- tudu/app/controllers/sessions_controller.rb
+++ tudu/app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # TODO: ログイン処理
+
+      redirect_to root_path
+    else
+      flash.now[:danger] = t('session.login.failed')
+      render 'new'
+    end
   end
 
   def destroy
