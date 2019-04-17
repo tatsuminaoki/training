ac45a027aaa35fe8cae8293fa8d5e7236a660ef6
toyonaga <shuhei_toyonaga@fablic.co.jp>
2019-04-17 11:01:25 +0900

    不要なファイルを削除する

--- tudu/config/routes.rb
+++ tudu/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :tasks
 
   get '*path', controller: 'application', action: 'render_404'
