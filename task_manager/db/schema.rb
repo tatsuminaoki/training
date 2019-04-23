85bb0477d28d1fab5b04aee9e161232f5d081fed
kenkaton <kentaro_kato@fablic.co.jp>
2019-04-23 15:25:38 +0900

    add index to tasks table

--- task_manager/db/schema.rb
+++ task_manager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_082510) do
+ActiveRecord::Schema.define(version: 2019_04_23_062058) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_04_22_082510) do
     t.date "due_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_tasks_on_created_at"
+    t.index ["due_date"], name: "index_tasks_on_due_date"
   end
 
 end
