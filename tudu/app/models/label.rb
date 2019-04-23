379e23383ec3fa1bd67c3b6fdb7d7a2a7636b6eb
toyonaga <shuhei_toyonaga@fablic.co.jp>
2019-04-23 10:31:34 +0900

    ラベルテーブルのuniqueのバリデーションを作成

--- tudu/app/models/label.rb
+++ tudu/app/models/label.rb
@@ -1,4 +1,12 @@
 class Label < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :tasks
+
+  validates :name,
+    presence: { message: I18n.translate("validates.presence") },
+    length: { maximum: 20, message: I18n.translate("validates.length") }
+
+  validates_uniqueness_of :name,
+    scope: [:user_id],
+    message: I18n.translate("validates.unique")
 end
