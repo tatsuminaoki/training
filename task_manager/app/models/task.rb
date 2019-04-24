e65b8803e1ce1a674a875c209c93ae2a74511c4b
kenkaton <kentaro_kato@fablic.co.jp>
2019-04-24 11:15:02 +0900

    add a task due_date validate with in the past

--- task_manager/app/models/task.rb
+++ task_manager/app/models/task.rb
@@ -2,4 +2,11 @@
 
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 64 }
+  validate :due_date_cannot_be_in_the_past
+
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date.past?
+      errors.add(:due_date, I18n.t('errors.messages.in_the_future'))
+    end
+  end
 end
