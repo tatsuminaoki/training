1242879c721366628bd09e63a3294e7c45c6aae7
kenkaton <kentaro_kato@fablic.co.jp>
2019-05-08 16:14:52 +0900

    add second user by seeds data

--- task_manager/db/seeds.rb
+++ task_manager/db/seeds.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
-User.create!(
-  name: '名前テスト',
-)
+User.create!([
+               {
+                 name: '名前テスト',
+               },
+               {
+                 name: 'test name',
+               },
+             ])
