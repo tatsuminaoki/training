2a17c785f62463557e630a56f0cd5f63cae8d13b
kenkaton <kentaro_kato@fablic.co.jp>
2019-05-09 10:55:21 +0900

    fix schema

--- task_manager/db/schema.rb
+++ task_manager/db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2019_05_07_045026) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
   end
 
   add_foreign_key "tasks", "users"
