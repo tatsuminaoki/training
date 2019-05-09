fc1d77b12eee8f2cd622a58c0dd49eac699aa039
kenkaton <kentaro_kato@fablic.co.jp>
2019-05-09 10:36:59 +0900

    change user_id column position

--- task_manager/db/migrate/20190507045026_add_user_to_tasks.rb
+++ task_manager/db/migrate/20190507045026_add_user_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddUserToTasks < ActiveRecord::Migration[5.2]
   def change
-    add_reference :tasks, :user, foreign_key: true
+    add_reference :tasks, :user, foreign_key: true, after: :id
   end
 end
