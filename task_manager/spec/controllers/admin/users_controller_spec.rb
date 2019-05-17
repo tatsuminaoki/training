bb47f18823b20219d810ad573cb943c56849630a
kenkaton <kentaro_kato@fablic.co.jp>
2019-05-17 12:55:47 +0900

    make admin user spec about controller and routes

---
+++ task_manager/spec/controllers/admin/users_controller_spec.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Admin::UsersController, type: :controller do
+  before do
+    user = create(:user)
+    log_in user
+  end
+  # This should return the minimal set of attributes required to create a valid
+  # User. As you add validations to User, be sure to
+  # adjust the attributes here as well.
+  let(:valid_attributes) {
+    {
+      name: 'user name',
+      password: 'password',
+      password_digest: User.digest('password'),
+    }
+  }
+
+  let(:invalid_attributes) {
+    {
+      name: '',
+    }
+  }
+
+  # This should return the minimal set of values that should be in the session
+  # in order to pass any filters (e.g. authentication) defined in
+  # UsersController. Be sure to keep this updated too.
+  let(:valid_session) { {} }
+
+  describe 'GET #index' do
+    it 'returns a success response' do
+      User.create! valid_attributes
+      get :index, params: {}, session: valid_session
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #new' do
+    it 'returns a success response' do
+      get :new, params: {}, session: valid_session
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'returns a success response' do
+      user = User.create! valid_attributes
+      get :edit, params: { id: user.to_param }, session: valid_session
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new User' do
+        expect {
+          post :create, params: { user: valid_attributes }, session: valid_session
+        }.to change(User, :count).by(1)
+      end
+
+      it 'redirects to the created user' do
+        post :create, params: { user: valid_attributes }, session: valid_session
+        expect(response).to redirect_to(admin_users_path)
+      end
+    end
+
+    context 'with invalid params' do
+      it "returns a success response (i.e. to display the 'new' template)" do
+        post :create, params: { user: invalid_attributes }, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    context 'with valid params' do
+      let(:new_attributes) {
+        { name: 'new user name', password: 'password', password_digest: User.digest('password') }
+      }
+
+      it 'updates the requested user' do
+        user = User.create! valid_attributes
+        put :update, params: { id: user.to_param, user: new_attributes }, session: valid_session
+        expect { user.reload }.to(
+          change(user, :name).from('user name').to('new user name'),
+        )
+      end
+
+      it 'redirects to the user' do
+        user = User.create! valid_attributes
+        put :update, params: { id: user.to_param, user: valid_attributes }, session: valid_session
+        expect(response).to redirect_to(admin_users_path)
+      end
+    end
+
+    context 'with invalid params' do
+      it "returns a success response (i.e. to display the 'edit' template)" do
+        user = User.create! valid_attributes
+        put :update, params: { id: user.to_param, user: invalid_attributes }, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroys the requested user' do
+      user = User.create! valid_attributes
+      expect {
+        delete :destroy, params: { id: user.to_param }, session: valid_session
+      }.to change(User, :count).by(-1)
+    end
+
+    it 'redirects to the users list' do
+      user = User.create! valid_attributes
+      delete :destroy, params: { id: user.to_param }, session: valid_session
+      expect(response).to redirect_to(admin_users_path)
+    end
+  end
+end
--- task_manager/spec/routing/users_routing_spec.rb
+++ task_manager/spec/routing/users_routing_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe Admin::UsersController, type: :routing do
+RSpec.describe UsersController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
       expect(get: '/users').to_not route_to('users#index')
@@ -34,38 +34,6 @@ RSpec.describe Admin::UsersController, type: :routing do
 
     it 'routes to #destroy' do
       expect(delete: '/users/1').to route_to('users#destroy', id: '1')
-    end
-
-    it 'routes to #index' do
-      expect(get: '/admin/users').to route_to('admin/users#index')
-    end
-
-    it 'routes to #new' do
-      expect(get: '/admin/users/new').to route_to('admin/users#new')
-    end
-
-    it 'routes to #show' do
-      expect(get: '/admin/users/1').to_not route_to('admin/users#show', id: '1')
-    end
-
-    it 'routes to #edit' do
-      expect(get: '/admin/users/1/edit').to route_to('admin/users#edit', id: '1')
-    end
-
-    it 'routes to #create' do
-      expect(post: '/admin/users').to route_to('admin/users#create')
-    end
-
-    it 'routes to #update via PUT' do
-      expect(put: '/admin/users/1').to route_to('admin/users#update', id: '1')
-    end
-
-    it 'routes to #update via PATCH' do
-      expect(patch: '/admin/users/1').to route_to('admin/users#update', id: '1')
-    end
-
-    it 'routes to #destroy' do
-      expect(delete: '/admin/users/1').to route_to('admin/users#destroy', id: '1')
     end
   end
 end
