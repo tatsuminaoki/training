e4a60351f391adb8a3e1be0a6dc6cd8b322b360b
masahiko kawai <masahiko_kawai@fablic.co.jp>
2019-05-22 16:00:29 +0900

    エラーページの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  rescue_from Exception, with: :render_error
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  private
+
+  def render_not_found
+    render file: "public/404.html", layout: false, status: 404
+  end
+
+  def render_error(e)
+    pp '#'*100
+    pp e
+    render file: "public/500.html", layout: false, status: 500
+  end
 end
