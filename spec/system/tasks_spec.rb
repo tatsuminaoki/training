76d0b175b65df83ce168a3fa968a9c9d691652ac
masahiko kawai <masahiko_kawai@fablic.co.jp>
2019-05-27 15:57:23 +0900

    i18nを利用して文言をあてる
    1. Gemfile.lockのコミット漏れを追加
    2. i18nのディレクトリ構成を変更
    3. system spec のE2Eも修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -8,38 +8,38 @@ RSpec.describe 'Tasks', type: :system do
 
     expect(page).to have_content('Tasks')
 
-    click_on 'New Task'
+    click_on '新規'
 
     fill_in 'タスク名', with: 'task name'
     fill_in '説明', with: 'hoge'
-    select 'work_in_progress', from: 'ステータス'
+    select '着手', from: 'ステータス'
 
     click_on '登録する'
 
     expect(page).to have_content('タスクの登録が完了しました。')
     expect(page).to have_content('task name')
     expect(page).to have_content('hoge')
-    expect(page).to have_content('work_in_progress')
+    expect(page).to have_content('着手')
 
-    click_on 'Edit'
+    click_on '編集'
 
     expect(page).to have_content('Editing Task')
     fill_in 'タスク名', with: 'update'
     fill_in '説明', with: 'hoge-update'
-    select 'completed', from: 'ステータス'
+    select '完了', from: 'ステータス'
 
     click_on '更新する'
 
     expect(page).to have_content('タスクの更新が完了しました。')
     expect(page).to have_content('update')
     expect(page).to have_content('hoge-update')
-    expect(page).to have_content('completed')
+    expect(page).to have_content('完了')
 
-    click_on 'Back'
+    click_on '戻る'
 
     expect(page).to have_content('Tasks')
     expect(page).to have_content('update')
     expect(page).to have_content('hoge-update')
-    expect(page).to have_content('completed')
+    expect(page).to have_content('完了')
   end
 end
