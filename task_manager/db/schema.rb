ae44ad212b3c3bd4763d84c66f12e879ee0db81c
kenkaton <kentaro_kato@fablic.co.jp>
2019-05-27 12:20:35 +0900

    merge automatically generated acts_as_taggable_on gem migration file

--- task_manager/db/schema.rb
+++ task_manager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_044044) do
+ActiveRecord::Schema.define(version: 2019_05_27_030714) do
 
   create_table "taggings", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "tag_id"
