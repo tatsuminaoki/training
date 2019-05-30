3acb9d44154b820ff65ec49c080519187a9db27b
kenkaton <kentaro_kato@fablic.co.jp>
2019-05-30 16:26:14 +0900

    bin/rails g rambulance:exceptions_app

---
+++ task_manager/app/handlers/exceptions_app.rb
@@ -0,0 +1,16 @@
+class ExceptionsApp < Rambulance::ExceptionsApp
+  def bad_request
+  end
+
+  def forbidden
+  end
+
+  def internal_server_error
+  end
+
+  def not_found
+  end
+
+  def unprocessable_entity
+  end
+end
