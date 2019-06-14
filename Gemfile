90152fbccf6b0cff22a29d0b9003450db8829a68
Hasebe <ryo.hasebe@p58641.intra.rakuten.co.jp>
2019-06-14 15:13:09 +0900

    タイトルとステータスで検索できるようにした

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem "factory_bot_rails"
+  gem 'pry-rails'
+  gem 'pry-byebug'
 end
 
 group :development do
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,14 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :detail, presence: true, length: { maximum: 255 }
+  validates :status, presence: true
+
+  enum status: %i[todo doing done]
+
+  def self.search(params)
+    tasks = Task.all
+    tasks = tasks.where('title LIKE ?', "%#{sanitize_sql_like(params[:title])}%") if params[:title]
+    tasks = tasks.where(status: params[:status]) if params[:status]
+    tasks
+  end
 end
