36b95283001890650f163b7a1a2f80589af0d429
Hasebe <ryo.hasebe@p58641.intra.rakuten.co.jp>
2019-06-18 12:30:56 +0900

    ステータスのindexを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_054057) do
+ActiveRecord::Schema.define(version: 2019_06_18_030945) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_06_13_054057) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
-    t.index ["title", "status"], name: "index_tasks_on_title_and_status"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
