e673475e47ab0fba8edb83a095f891c9308496ba
masahiko kawai <masahiko_kawai@fablic.co.jp>
2019-06-19 15:30:16 +0900

    controllerのname spaceからmoduleを使うように修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,62 +1,64 @@
 # frozen_string_literal: true
 
-class Admin::UsersController < ApplicationController
-  before_action :user, only: %i[show edit update destroy]
+module Admin
+  class UsersController < ApplicationController
+    before_action :user, only: %i[show edit update destroy]
 
-  def index
-    @users = User.all.order(created_at: :desc)
-  end
-
-  def new
-    @user = User.new.tap(&:build_user_credential)
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to admin_users_path, success: t('messages.created', item: @user.model_name.human)
-    else
-      render :new
+    def index
+      @users = User.all.order(created_at: :desc)
     end
-  end
 
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to admin_users_path, success: t('messages.updated', item: @user.model_name.human)
-    else
-      render :edit
+    def new
+      @user = User.new.tap(&:build_user_credential)
     end
-  end
 
-  def destroy
-    @user.destroy!
-    redirect_to admin_users_path, success: t('messages.deleted', item: @user.model_name.human)
-  end
+    def create
+      @user = User.new(user_params)
 
-  private
+      if @user.save
+        redirect_to admin_users_path, success: t('messages.created', item: @user.model_name.human)
+      else
+        render :new
+      end
+    end
 
-  def user
-    @user = User.find(params[:id])
-  end
+    def show
+    end
 
-  def user_params
-    params.require(:user).permit(
-      :id,
-      :name,
-      :email,
-      :email_confirmation,
-      user_credential_attributes: %i[
-        id
-        password
-        password_confirmation
-      ],
-    )
+    def edit
+    end
+
+    def update
+      if @user.update(user_params)
+        redirect_to admin_users_path, success: t('messages.updated', item: @user.model_name.human)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @user.destroy!
+      redirect_to admin_users_path, success: t('messages.deleted', item: @user.model_name.human)
+    end
+
+    private
+
+    def user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(
+        :id,
+        :name,
+        :email,
+        :email_confirmation,
+        user_credential_attributes: %i[
+          id
+          password
+          password_confirmation
+        ],
+      )
+    end
   end
 end
