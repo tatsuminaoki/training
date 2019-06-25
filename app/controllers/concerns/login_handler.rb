4b6219d5e55613bfbfb2eaa16146ae51fbfa20f6
Hasebe <ryo.hasebe@p58641.intra.rakuten.co.jp>
2019-06-25 12:21:26 +0900

    cookieにemailも保存しておいて@current_userを引く時に使うようにした

--- app/controllers/concerns/login_handler.rb
+++ app/controllers/concerns/login_handler.rb
@@ -1,12 +1,13 @@
 module LoginHandler
   def current_user
     remember_token = User.encrypt(cookies[:user_remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
+    @current_user ||= User.find_by(remember_token: remember_token, mail: cookies[:mail])
   end
 
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:user_remember_token] = remember_token
+    cookies.permanent[:mail] = user.mail
     user.update!(remember_token: User.encrypt(remember_token))
     @current_user = user
   end
