c5af0b664530cdfd4b97f4f5d1e06c3d1d6ab26b
Higuchi <sadaya.higuchi@p60496.intra.rakuten.co.jp>
2019-09-10 11:26:05 +0900

    指摘の対応:新たに削除用マイグレーションは作らず変更する

--- myapp/db/migrate/20190910004643_remove_user_id_and_status_from_tasks.rb
+++
@@ -1,8 +0,0 @@
-class RemoveUserIdAndStatusFromTasks < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :tasks, :user_id, :integer
-
-    remove_column :tasks, :status, :boolean
-  end
-end
