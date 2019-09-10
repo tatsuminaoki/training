fbbe833071edcfd5969e2da4a2c31d6873c4b994
Higuchi <sadaya.higuchi@p60496.intra.rakuten.co.jp>
2019-09-10 17:03:39 +0900

    新規登録の追加

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -7,9 +7,19 @@ class TasksController < ApplicationController
     end
   
     def new
+        @task = Task.new
     end
   
     def create
+        @params = params.require(:task).permit(:title, :description)
+        @task = Task.new(@params)
+
+        if @task.save
+            # とりあえずリストに飛ばす
+            redirect_to action: 'index'
+        else
+            render :new
+        end
     end
   
     def edit
