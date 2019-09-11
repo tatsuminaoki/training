469d9035cf2a0d0d7cf7a99c0859ba905d7ff537
Higuchi <sadaya.higuchi@p60496.intra.rakuten.co.jp>
2019-09-11 16:31:47 +0900

    インデントをスペース2つに修正

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,33 +1,33 @@
 class TasksController < ApplicationController
-    def index
-        @tasks = Task.all
-    end
-  
-    def show
-    end
-  
-    def new
-        @task = Task.new
-    end
-  
-    def create
-        @params = params.require(:task).permit(:title, :description)
-        @task = Task.new(@params)
+  def index
+    @tasks = Task.all
+  end
 
-        if @task.save
-            # とりあえずリストに飛ばす
-            redirect_to action: 'index'
-        else
-            render :new
-        end
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @params = params.require(:task).permit(:title, :description)
+    @task = Task.new(@params)
+
+    if @task.save
+      # とりあえずリストに飛ばす
+      redirect_to action: 'index'
+    else
+      render :new
     end
-  
-    def edit
-    end
-  
-    def update
-    end
-  
-    def destroy
-    end    
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end    
 end
