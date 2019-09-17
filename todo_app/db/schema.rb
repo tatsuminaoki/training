318109e7728c440cc3981f0b027a2211ba934111
zoe <takeshi.yamazoe@rakuten.com>
2019-09-17 14:15:31 +0900

    [STEP9] add validation on name of task

--- todo_app/db/schema.rb
+++ todo_app/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_055424) do
+ActiveRecord::Schema.define(version: 2019_09_17_043935) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
