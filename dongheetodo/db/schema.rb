7a57244ea03466c563722fa7e341c3f0a6c29532
Kim Donghee <lastgleam@gmail.com>
2019-09-17 16:55:03 +0900

    Fix DB schema

--- dongheetodo/db/schema.rb
+++ dongheetodo/db/schema.rb
@@ -14,27 +14,27 @@ ActiveRecord::Schema.define(version: 2019_09_16_234341) do
 
   create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.bigint "task_id", null: false
-    t.string "name", null: false, comment: "ラベル名"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["task_id"], name: "fk_rails_516472e364"
   end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
-    t.string "user_id", null: false
-    t.string "name", null: false, comment: "タスク名"
+    t.bigint "user_id", null: false
+    t.string "name", null: false
     t.text "description", comment: "タスク内容"
-    t.integer "priority", null: false, comment: "優先順位"
-    t.integer "status", null: false, comment: "ステータス"
+    t.integer "priority", null: false, comment: "1:低 2:中 3:高 "
+    t.integer "status", null: false, comment: "1:未着手 2:着手 3:完了 "
     t.datetime "duedate", comment: "終了期限"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "fk_rails_4d2a9e4d7e"
   end
 
-  create_table "users", id: :string, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
-    t.string "password_digest", null: false, comment: "暗号化されたパスワード"
-    t.string "email", null: false, comment: "メールアドレス"
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "password_digest", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
