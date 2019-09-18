e42e98a2bc9110fa95b269bb0c47a782384180f7
zoe <takeshi.yamazoe@rakuten.com>
2019-09-18 09:49:55 +0900

    [STEP7] fix english sentences

--- todo_app/spec/requests/tasks_request_spec.rb
+++ todo_app/spec/requests/tasks_request_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :request do
   describe 'GET /tasks' do
-    it 'return 200' do
+    it 'returns 200' do
       get tasks_path
       expect(response).to have_http_status(200)
     end
@@ -12,14 +12,14 @@ RSpec.describe 'Tasks', type: :request do
     context 'when tasks exists' do
       let!(:task) { create(:task, name: '@my_task@') }
 
-      it 'display tasks' do
+      it 'displays tasks' do
         get tasks_path
         expect(response.body).to include('@my_task@')
       end
     end
 
     context 'when tasks not exists' do
-      it 'display blank message' do
+      it 'displays blank message' do
         get tasks_path
         expect(response.body).to include('タスクは登録されていません')
       end
@@ -27,12 +27,12 @@ RSpec.describe 'Tasks', type: :request do
   end
 
   describe 'GET /tasks/new' do
-    it 'return 200' do
+    it 'returns 200' do
       get new_task_path
       expect(response).to have_http_status(200)
     end
 
-    it 'render form' do
+    it 'renders form' do
       get new_task_path
       expect(response.body).to match(/<input.*?task\[name\].*?>/)
       expect(response.body).to include('<input type="submit"')
@@ -45,12 +45,12 @@ RSpec.describe 'Tasks', type: :request do
         allow_any_instance_of(Task).to receive(:save).and_return(false)
       end
 
-      it 'return 200' do
+      it 'returns 200' do
         post tasks_path, params: { task: attributes_for(:task) }
         expect(response).to have_http_status(200)
       end
 
-      it 're render form' do
+      it 'rerenders form' do
         post tasks_path, params: { task: attributes_for(:task) }
         expect(response.body).to match(/<input.*?task\[name\].*?>/)
         expect(response.body).to include('<input type="submit"')
@@ -64,13 +64,13 @@ RSpec.describe 'Tasks', type: :request do
         }.to change(Task, :count).by(1)
       end
 
-      it 'redirect to #show' do
+      it 'redirects to #show' do
         post tasks_path, params: { task: attributes_for(:task) }
         expect(response).to have_http_status(302)
         expect(response.header['Location']).to include(task_path(Task.last.id))
       end
 
-      it 'set flash message' do
+      it 'sets flash message' do
         post tasks_path, params: { task: attributes_for(:task) }
         expect(flash[:success]).to eq('タスクを作成しました。')
       end
@@ -81,19 +81,19 @@ RSpec.describe 'Tasks', type: :request do
     let!(:task) { create(:task, name: '@my_task@') }
 
     context 'when specify correct id' do
-      it 'return 200' do
+      it 'returns 200' do
         get task_path(id: task.id)
         expect(response).to have_http_status(200)
       end
 
-      it 'display task properties' do
+      it 'displays task properties' do
         get task_path(id: task.id)
         expect(response.body).to include('@my_task@')
       end
     end
 
     context 'when specify invalid id' do
-      it 'raise ActiveRecord::RecordNotFound' do
+      it 'raises ActiveRecord::RecordNotFound' do
         expect { get task_path(id: 0) }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
@@ -102,12 +102,12 @@ RSpec.describe 'Tasks', type: :request do
   describe 'GET /tasks/:id/edit' do
     let!(:task) { create(:task, name: '@my_task@') }
 
-    it 'return 200' do
+    it 'returns 200' do
       get edit_task_path(id: task.id)
       expect(response).to have_http_status(200)
     end
 
-    it 'render form' do
+    it 'renders form' do
       get edit_task_path(id: task.id)
       expect(response.body).to match(/<input.*?task\[name\].*?>/)
       expect(response.body).to include('<input type="submit"')
@@ -122,12 +122,12 @@ RSpec.describe 'Tasks', type: :request do
         allow_any_instance_of(Task).to receive(:update).and_return(false)
       end
 
-      it 'return 200' do
+      it 'returns 200' do
         put task_path(id: task.id), params: { task: attributes_for(:task) }
         expect(response).to have_http_status(200)
       end
 
-      it 're render form' do
+      it 'rerenders form' do
         put task_path(id: task.id), params: { task: attributes_for(:task) }
         expect(response.body).to match(/<input.*?task\[name\].*?>/)
         expect(response.body).to include('<input type="submit"')
@@ -141,13 +141,13 @@ RSpec.describe 'Tasks', type: :request do
         }.to change { Task.last.name }.from('@my_task@').to('@updated_my_task@')
       end
 
-      it 'redirect to #show' do
+      it 'redirects to #show' do
         put task_path(id: task.id), params: { task: attributes_for(:task) }
         expect(response).to have_http_status(302)
         expect(response.header['Location']).to include(task_path(Task.last.id))
       end
 
-      it 'set flash message' do
+      it 'sets flash message' do
         put task_path(id: task.id), params: { task: attributes_for(:task) }
         expect(flash[:success]).to eq('タスクを更新しました。')
       end
@@ -162,26 +162,26 @@ RSpec.describe 'Tasks', type: :request do
         allow_any_instance_of(Task).to receive(:destroy).and_return(false)
       end
 
-      it 'redirect to #show' do
+      it 'redirects to #show' do
         delete task_path(id: task.id)
         expect(response).to have_http_status(302)
         expect(response.header['Location']).to include(task_path(Task.last.id))
       end
 
-      it 'set flash message' do
+      it 'sets flash message' do
         delete task_path(id: task.id)
         expect(flash[:error]).to include('タスクの削除に削除しました。')
       end
     end
 
     context 'when succeeded to destroy' do
-      it 'redirect to #index' do
+      it 'redirects to #index' do
         delete task_path(id: task.id)
         expect(response).to have_http_status(302)
         expect(response.header['Location']).to include(tasks_path)
       end
 
-      it 'set flash message' do
+      it 'sets flash message' do
         delete task_path(id: task.id)
         expect(flash[:success]).to include('タスクを削除しました。')
       end
