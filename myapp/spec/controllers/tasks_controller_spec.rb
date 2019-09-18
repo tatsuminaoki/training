d51ee7cb11a4ea9f2d5ec065da68ed18714807f0
Higuchi <sadaya.higuchi@p60496.intra.rakuten.co.jp>
2019-09-18 18:17:43 +0900

    指摘の対応：rspecの修正

--- myapp/spec/controllers/tasks_controller_spec.rb
+++ myapp/spec/controllers/tasks_controller_spec.rb
@@ -1,17 +1,12 @@
 describe TasksController, type: :request do
   describe 'GET #index' do
     before do
-      FactoryBot.create :task1
-      FactoryBot.create :task2
+      create_list(:task, 2)
     end
 
-    it 'リクエストが成功すること' do
+    it 'リクエストが成功すること、タイトルが表示されていること' do
       get tasks_url
-      expect(response.status).to eq 200
-    end
-
-    it 'タイトルが表示されていること' do
-      get tasks_url
+      expect(response).to have_http_status :ok
       expect(response.body).to include "Task1"
       expect(response.body).to include "Task2"
     end
@@ -20,15 +15,16 @@ describe TasksController, type: :request do
   describe 'GET #new' do
     it 'リクエストが成功すること' do
       get new_task_url
-      expect(response.status).to eq 200
+      expect(response).to have_http_status :ok
     end
   end
 
   describe 'POST #create' do
     context '正常なパラメータの場合' do
-      it 'リクエストが成功すること' do
+      it 'TOPへのリダイレクトが成功すること' do
         post tasks_url, params: { task: FactoryBot.attributes_for(:task) }
-        expect(response.status).to eq 302
+        expect(response).to have_http_status :found
+        expect(response).to redirect_to tasks_url
       end
 
       it 'タスクが登録されること' do
@@ -36,17 +32,13 @@ describe TasksController, type: :request do
           post tasks_url, params: { task: FactoryBot.attributes_for(:task) }
         end.to change(Task, :count).by(1)
       end
-
-      it 'TOPにリダイレクトすること' do
-        post tasks_url, params: { task: FactoryBot.attributes_for(:task) }
-        expect(response).to redirect_to '/'
-      end
     end
 
-    context 'パラメータが不正な場合' do
-      it 'リクエストが成功すること' do
+    context 'パラメータが不正(titleが空)な場合' do
+      it 'ROOTへのリダイレクトが成功すること' do
         post tasks_url, params: { task: FactoryBot.attributes_for(:task, title: nil) }
-        expect(response.status).to eq 200
+        expect(response).to have_http_status :found
+        expect(response).to redirect_to root_url
       end
 
       it 'ユーザーが登録されないこと' do
@@ -54,90 +46,65 @@ describe TasksController, type: :request do
           post tasks_url, params: { task: FactoryBot.attributes_for(:task, title: nil) }
         end.to_not change(Task, :count)
       end
-
-      it 'エラーが表示されること' do
-        post tasks_url, params: { task: FactoryBot.attributes_for(:task, title: nil) }
-        expect(response.body).to include '登録に失敗しました'
-      end
     end
   end
 
   describe 'GET #edit' do
-    let(:task1) { FactoryBot.create :task1 }
+    let(:task) { FactoryBot.create :task }
   
-    it 'リクエストが成功すること' do
-      get edit_task_url task1
-      expect(response.status).to eq 200
-    end
- 
-    it 'タイトルが表示されていること' do
-      get edit_task_url task1
-      expect(response.body).to include 'Task1'
-    end
-
-    it '説明が表示されていること' do
-      get edit_task_url task1
-      expect(response.body).to include 'Description1'
+    it 'リクエストが成功し、タイトルと説明が表示されていること' do
+      get edit_task_url task
+      expect(response).to have_http_status :ok
+      expect(response.body).to include 'Task'
+      expect(response.body).to include 'Description'
     end
   end
 
   describe 'PUT #update' do
-    let(:task1) { FactoryBot.create :task1 }
+    let!(:task) { FactoryBot.create :task }
   
     context '正常なパラメータの場合' do
-      it 'リクエストが成功すること' do
-        put task_url task1, params: { task: FactoryBot.attributes_for(:task2) }
-        expect(response.status).to eq 302
+      it 'TOPへのリダイレクトが成功すること' do
+        put task_url task, params: { task: FactoryBot.attributes_for(:task, title: 'Update Task') }
+        expect(response).to have_http_status :found
+        expect(response).to redirect_to tasks_url
       end
 
       it 'タイトルが更新されること' do
         expect do
-          put task_url task1, params: { task: FactoryBot.attributes_for(:task2) }
-        end.to change { Task.find(task1.id).title }.from('Task1').to('Task2')
-      end
-
-      it 'リダイレクトすること' do
-        put task_url task1, params: { task: FactoryBot.attributes_for(:task2) }
-        expect(response).to redirect_to '/'
+          put task_url task, params: { task: FactoryBot.attributes_for(:task, title: 'Update Task') }
+        end.to change { Task.find(task.id).title }.to('Update Task')
       end
     end
 
     context 'パラメータが不正な場合' do
-      it 'リクエストが成功すること' do
-        put task_url task1, params: { task: FactoryBot.attributes_for(:task3) }
-        expect(response.status).to eq 302
+      it 'ROOTへのリダイレクトが成功すること' do
+        put task_url task, params: { task: FactoryBot.attributes_for(:blank_task) }
+        expect(response).to have_http_status :found
+        expect(response).to redirect_to root_url
       end
 
       it 'タイトルが更新しないこと' do
         expect do
-          put task_url task1, params: { task: FactoryBot.attributes_for(:task3) }
-        end.to_not change(Task.find(task1.id), :title)
-      end
-
-      it 'TOPにリダイレクトすること' do
-        put task_url task1, params: { task: FactoryBot.attributes_for(:task3) }
-        expect(response).to redirect_to '/'
+          put task_url task, params: { task: FactoryBot.attributes_for(:blank_task) }
+        end.to_not change(Task.find(task.id), :title)
       end
     end
   end
 
   describe 'DELETE #destroy' do
-    let!(:task1) { FactoryBot.create :task1 }
+    let!(:task) { FactoryBot.create :task }
 
-    it 'リクエストが成功すること' do
-      delete task_url task1
-      expect(response.status).to eq 302
+    it 'TOPへのリダイレクトが成功すること' do
+      delete task_url task
+      expect(response).to have_http_status :found
+      expect(response).to redirect_to tasks_url
     end
 
     it 'タスクが削除されること' do
       expect do
-        delete task_url task1
+        delete task_url task
       end.to change(Task, :count).by(-1)
-    end
-
-    it 'TOPにリダイレクトすること' do
-      delete task_url task1
-      expect(response).to redirect_to '/'
     end
   end
 end
