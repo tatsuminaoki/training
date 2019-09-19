2e83e16608ac73a4152355d80b0d634abc422a5e
zoe <takeshi.yamazoe@rakuten.com>
2019-09-19 10:21:08 +0900

    [STEP13] add redis-rails gem

---
+++ todo_app/config/initializers/session_store.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.config.session_store(
+  :redis_store,
+  servers: ['redis://localhost:6379/0/session'],
+  expire_after: 90.minutes,
+  key: "_#{Rails.application.class.parent_name.downcase}_session",
+  threadsafe: true,
+  signed: true,
+)
