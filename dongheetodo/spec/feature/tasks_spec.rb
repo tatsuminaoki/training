6d24e9ae6efa0bda6e3a0f695a748b882f4eab25
Donghee KIM <lastgleam@gmail.com>
2019-09-24 12:15:21 +0900

    Fix show and edit task test

--- dongheetodo/spec/feature/tasks_spec.rb
+++ dongheetodo/spec/feature/tasks_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
-RSpec.feature "[タスクCRUD]", js: true do
+RSpec.feature "Tasks", type: :feature, js: true do
   background do
     # TODO: ログイン機能追加後、user_idを動的に当てるように修正
-    @user = User.create!(id: 1, email: "donghee_kim@fablic.co.jp", password_digest: "hogehoge")
-    @task = Task.create!(user_id: @user.id, name: "hoge", description: "hogehoge",
+    user = User.create!(id: 1, email: "donghee_kim@fablic.co.jp", password_digest: "hogehoge")
+    Task.create!(user_id: user.id, name: "hoge", description: "hogehoge",
                          priority: Task.priorities[:中], status: Task.statuses[:完了])
   end
+
   scenario "タスク一覧を表示する" do
     visit tasks_path
     expect(page).to have_content "タスク一覧"
   end
+
   scenario "タスクを作成する" do
     visit new_task_path
     fill_in "task_name", with: "ダミーデータ"
@@ -20,12 +22,16 @@ RSpec.feature "[タスクCRUD]", js: true do
     click_button "Create Task"
     expect(page).to have_content "正常に作成しました"
   end
+
   scenario "タスク詳細を表示する" do
-    visit task_path(@task.id)
+    visit root_path
+    all("tbody tr ")[0].click_link "詳細"
     expect(page).to have_content "タスク詳細"
   end
+
   scenario "タスクを更新する" do
-    visit edit_task_path(@task.id)
+    visit root_path
+    all("tbody tr ")[0].click_link "修正"
     fill_in "task_name", with: "更新するよー"
     fill_in "task_description", with: "テストのため更新します"
     select "未着手", from: "task_status"
@@ -33,6 +39,7 @@ RSpec.feature "[タスクCRUD]", js: true do
     click_button "Update Task"
     expect(page).to have_content "正常に更新しました"
   end
+
   scenario "タスクを削除する" do
     visit tasks_path
     all("tbody tr ")[0].click_link "削除"
