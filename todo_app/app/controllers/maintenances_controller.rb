602e97c4fa5c03c2bc6d9b80f7ba0ffea250762a
zoe <takeshi.yamazoe@rakuten.com>
2019-09-25 16:26:26 +0900

    [STEP14] fix if/else syntax

--- todo_app/app/controllers/maintenances_controller.rb
+++ todo_app/app/controllers/maintenances_controller.rb
@@ -2,10 +2,9 @@
 
 class MaintenancesController < ApplicationController
   def show
-    if !File.exist?(Rails.root.join('tmp', 'maintenance.txt'))
-      redirect_to root_path, status: 302
-    else
-      render status: 503, layout: false
+    unless File.exist?(Rails.root.join('tmp', 'maintenance.txt'))
+      redirect_to(root_path, status: 302) and return
     end
+    render status: 503, layout: false
   end
 end
