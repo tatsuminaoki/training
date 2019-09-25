3f93ad3bc5042a64558fa70df85c018eb65b5050
Donghee KIM <lastgleam@gmail.com>
2019-09-25 11:44:13 +0900

    Update time zone to Asia/Tokyo

--- dongheetodo/config/application.rb
+++ dongheetodo/config/application.rb
@@ -17,5 +17,16 @@ module Dongheetodo
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
+    #
+    # タイムゾーンを日本標準時に設定する
+    #
+    config.time_zone = "Asia/Tokyo"
+
+    #
+    # データベースTimeZoneは必ず:utcか:localを指定する
+    #
+    # :utc   = UTCタイムゾーンにする
+    # :local = OSのタイムゾーンに従う
+    config.active_record.default_timezone = :local
   end
 end
