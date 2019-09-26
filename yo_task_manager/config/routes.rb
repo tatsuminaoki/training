5f0793684448070136cc5e8c632a3539fb8ac9f3
Yo_IT <10822260+yo-it-engineer@users.noreply.github.com>
2019-09-26 10:07:20 +0900

    rails g controller task index create show edit destroy

--- yo_task_manager/config/routes.rb
+++ yo_task_manager/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'task/index'
+  get 'task/create'
+  get 'task/show'
+  get 'task/edit'
+  get 'task/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
