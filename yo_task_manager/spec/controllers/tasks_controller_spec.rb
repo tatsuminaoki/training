b22b4b1ebd540649415cd5a051476c1836591794
Yo_IT <10822260+yo-it-engineer@users.noreply.github.com>
2019-09-30 13:27:23 +0900

    add some basic rspec for tasks_controller

--- yo_task_manager/spec/controllers/tasks_controller_spec.rb
+++ yo_task_manager/spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  
+  context '#index' do
+    it 'assigns @task' do
+      task = Task.create
+      get :index
+      expect(assigns(:tasks)).to eq([task])
+    end
+    it 'renders index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+
+  context '#new' do
+
+  end
+
+  context '#create' do
+
+  end
+
+  context '#show' do
+
+  end
+
+  context '#edit' do
+
+  end
+
+  context '#update' do
+
+  end
+
+  context '#destroy' do
+
+  end
 end
