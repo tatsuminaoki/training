905f142caabd11cb4597b65cf86435bbc4b1acdb
Higuchi <sadaya.higuchi@p60496.intra.rakuten.co.jp>
2019-09-30 15:36:58 +0900

    add spec test

--- myapp/spec/factories/tasks.rb
+++ myapp/spec/factories/tasks.rb
@@ -1,13 +1,19 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :task, class: Task do
     sequence(:title) { |n| "Task#{n}" }
     sequence(:description) { |n| "Description#{n}" }
     sequence(:status) { Task.statuses.keys[0] }
+
+    user
   end
 
   factory :blank_task, class: Task do
     title       { '' }
     description { '' }
     status      { Task.statuses.keys[0] }
+
+    user
   end
 end
--- myapp/spec/models/user_spec.rb
+++ myapp/spec/models/user_spec.rb
@@ -1,5 +1,85 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ログインIDのバリデーション' do
+    before do
+      user = FactoryBot.attributes_for(:user)
+      @user = User.new(user)
+    end
+
+    context '100文字な場合' do
+      it '登録できる' do
+        @user.login_id = 'a' * 100
+        expect(@user).to be_valid
+      end
+    end
+
+    context '101文字な場合' do
+      it 'エラーになる' do
+        @user.login_id = 'a' * 101
+        expect(@user).not_to be_valid
+      end
+    end
+
+    context '半角英数字のみな場合' do
+      it '登録できる' do
+        @user.login_id = 'abcABC0123'
+        expect(@user).to be_valid
+      end
+    end
+
+    context '半角英数字以外な場合' do
+      it 'エラーになる' do
+        @user.login_id = 'kigou$ha^dame,napattern'
+        expect(@user).not_to be_valid
+        @user.login_id = '日本語もだめなパターン'
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe 'パスワードのバリデーション' do
+    before do
+      user = FactoryBot.attributes_for(:user)
+      @user = User.new(user)
+    end
+
+    context '100文字な場合' do
+      it '登録できる' do
+        @user.password_digest = 'abcA1' * 20
+        expect(@user).to be_valid
+      end
+    end
+
+    context '101文字な場合' do
+      it 'エラーになる' do
+        @user.password_digest = 'a' * 101
+        expect(@user).not_to be_valid
+      end
+    end
+
+    context '半角英小文字大文字数字をそれぞれ1種類以上含む場合' do
+      it '登録できる' do
+        @user.password_digest = 'abcABC0123'
+        expect(@user).to be_valid
+      end
+    end
+
+    context '半角英小文字大文字数字をそれぞれ1種類以上含んでいない場合' do
+      it 'エラーになる' do
+        @user.password_digest = 'Kigou-Ga-Fukumareteiru123'
+        expect(@user).not_to be_valid
+        @user.password_digest = '日本語もだめなパターン'
+        expect(@user).not_to be_valid
+        @user.password_digest = 'SuujiGaFukumareteinai'
+        expect(@user).not_to be_valid
+        @user.password_digest = 'oomojigafukumareteinai123'
+        expect(@user).not_to be_valid
+        @user.password_digest = 'KOMOJIGAFUKUMARETEINAI123'
+        expect(@user).not_to be_valid
+      end
+    end
+  end
 end
