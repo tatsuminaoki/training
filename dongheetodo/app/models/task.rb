2f522482c7c19f0b5c00aa44e714844d7365692f
Donghee KIM <lastgleam@gmail.com>
2019-10-01 09:51:46 +0900

    Add validation to Task model

--- dongheetodo/app/models/task.rb
+++ dongheetodo/app/models/task.rb
@@ -4,4 +4,9 @@ class Task < ApplicationRecord
 
   enum status: {todo: 1, doing: 2, done: 3}
   enum priority: {low: 1, mid: 2, high: 3}
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :description, length: { maximum: 255 }
+  validates :status, presence: true, inclusion: { in: Task.statuses.keys }
+  validates :priority, presence: true, inclusion: { in: Task.priorities.keys }
 end
