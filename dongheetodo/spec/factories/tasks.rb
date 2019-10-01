bb5a7b1cff6391d4b3450d2526d02f12c2969cf9
Donghee KIM <lastgleam@gmail.com>
2019-10-01 17:24:28 +0900

    Update Task Factory to generate attributes randomly

--- dongheetodo/spec/factories/tasks.rb
+++ dongheetodo/spec/factories/tasks.rb
@@ -1,9 +1,11 @@
 FactoryBot.define do
   factory :task do
     sequence(:name) { |n| "task#{n}" }
-    description { 'task description' }
-    status { Task.statuses[:done] }
-    priority { Task.priorities[:high] }
+    description { Faker::Lorem.characters(number: 255) }
+    status { Task.statuses.to_a.sample[0] }
+    priority { Task.priorities.to_a.sample[0] }
+    # 現在時刻から１ヶ月後の日時をランダムで付与
+    duedate { rand(Time.now..(Time.now + 60 * 60 * 24 * 30)) }
     user
   end
 end
