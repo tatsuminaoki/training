338ef6a3b5fd752681ff58971acd87b44830ee02
Donghee KIM <lastgleam@gmail.com>
2019-10-02 11:47:02 +0900

    Refactor task factory

--- dongheetodo/spec/factories/tasks.rb
+++ dongheetodo/spec/factories/tasks.rb
@@ -2,10 +2,11 @@ FactoryBot.define do
   factory :task do
     sequence(:name) { |n| "task#{n}" }
     description { Faker::Lorem.characters(number: 255) }
-    status { Task.statuses.to_a.sample[0] }
-    priority { Task.priorities.to_a.sample[0] }
+    status { Task.statuses.keys.sample }
+    priority { Task.priorities.keys.sample }
     # 現在時刻から１ヶ月後の日時をランダムで付与
-    duedate { rand(Time.now..(Time.now + 60 * 60 * 24 * 30)) }
+    duedate { rand(Time.now..Time.now.since(30.days)) }
+    created_at { rand(Time.now..Time.now.since(30.days)) }
     user
   end
 end
