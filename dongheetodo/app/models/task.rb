52521900106e991a6595f375d8afc3065217e4d9
Donghee KIM <lastgleam@gmail.com>
2019-10-03 10:23:26 +0900

    change default order to asc

--- dongheetodo/app/models/task.rb
+++ dongheetodo/app/models/task.rb
@@ -5,7 +5,7 @@ class Task < ApplicationRecord
   enum status: {todo: 1, doing: 2, done: 3}
   enum priority: {low: 1, mid: 2, high: 3}
   enum sort_by: [:id, :duedate, :created_at]
-  enum order: [:desc, :asc]
+  enum order: [:asc, :desc]
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 255 }
