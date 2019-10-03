06bcc2d3d8a7c7d1055b67d30fdaf7ac8532b233
Donghee KIM <lastgleam@gmail.com>
2019-10-03 14:41:24 +0900

    Add migration to create tasks name index

--- dongheetodo/db/schema.rb
+++ dongheetodo/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_234341) do
+ActiveRecord::Schema.define(version: 2019_10_03_043554) do
 
   create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.bigint "task_id", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_234341) do
     t.datetime "duedate", comment: "終了期限"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tasks_on_name"
     t.index ["user_id"], name: "fk_rails_4d2a9e4d7e"
   end
 
