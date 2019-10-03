a4c8dc92efb522e2e2ec4a339b1120b04302fef4
Higuchi <sadaya.higuchi@p60496.intra.rakuten.co.jp>
2019-10-03 16:54:32 +0900

    modify user validation

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -5,12 +5,14 @@ class User < ApplicationRecord
 
   validates :login_id,
             presence: true,
+            uniqueness: { case_sensitive: true },
             length: { in: 1..100 },
             format: { with: /\A[a-z\d]{1,100}+\z/i, message: '100文字以内の英数字のみが使えます' }
-  validates :password_digest,
+
+  has_secure_password validations: false
+  validates :password,
             presence: true,
             length: { in: 8..100 },
+            allow_nil: true,
             format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,100}+\z/, message: '8〜100文字の半角英小文字大文字数字をそれぞれ1種類以上含む必要があります' }
-
-  has_secure_password
 end
