07a419ad66682ed366ab41aa43a44edfcd221512
Yo_IT <10822260+yo-it-engineer@users.noreply.github.com>
2019-10-04 15:01:22 +0900

    add custom error pages

--- yo_task_manager/config/routes.rb
+++ yo_task_manager/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get '/:locale' => 'tasks#index'
   root to: 'tasks#index'
 
   scope "/:locale", locale: /ja|en/ do
     resources :tasks
   end
+
+  get '/404', to: 'errors#not_found'
+  get '/422', to: 'errors#unacceptable'
+  get '/500', to: 'errors#internal_error'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
