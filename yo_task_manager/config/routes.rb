8c295ee3d323a103550739b14375395a76353687
Yo_IT <10822260+yo-it-engineer@users.noreply.github.com>
2019-10-04 10:34:50 +0900

    I18n setup and initializer. update routes to support I18n

--- yo_task_manager/config/routes.rb
+++ yo_task_manager/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get '/:locale' => 'tasks#index'
   root to: 'tasks#index'
-  resources :tasks
+
+  scope "/:locale", locale: /ja|en/ do
+    resources :tasks
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
