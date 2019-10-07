2290bf722f2545fec2b56eb8ed661001ee8152de
Higuchi <sadaya.higuchi@p60496.intra.rakuten.co.jp>
2019-10-07 09:54:50 +0900

    modify session to devise gem

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:login_id]
+
   has_many :tasks
 
   validates :login_id,
@@ -9,18 +13,24 @@ class User < ApplicationRecord
             length: { in: 1..100 },
             format: { with: /\A[a-z\d]{1,100}+\z/i, message: '100文字以内の英数字のみが使えます' }
 
-  has_secure_password validations: false
+=begin
   validates :password,
             presence: true,
             length: { in: 8..100 },
             allow_nil: true,
             format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,100}+\z/, message: '8〜100文字の半角英小文字大文字数字をそれぞれ1種類以上含む必要があります' }
+=end
 
-  def self.new_remember_token
-    SecureRandom.urlsafe_base64
+  # No use email
+  def email_required?
+    false
+  end
+ 
+  def email_changed?
+    false
   end
 
-  def self.encrypt(token)
-    Digest::SHA256.hexdigest(token.to_s)
+  def will_save_change_to_email?
+    false
   end
 end
