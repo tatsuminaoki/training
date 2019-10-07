fd745d8335faa90f040469cf59496dcb576288bd
Donghee KIM <lastgleam@gmail.com>
2019-10-07 15:45:10 +0900

    Add user signup feature

--- dongheetodo/config/routes.rb
+++ dongheetodo/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks
+  resources :users
+  
   root "tasks#index"
   # どこにも当てはまらなかったら404エラーページを返す
   get "*path", to: "application#render_404"
