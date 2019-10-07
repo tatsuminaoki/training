2a3dfd40920547a815977bbdff11c2de974801dc
Higuchi <sadaya.higuchi@p60496.intra.rakuten.co.jp>
2019-10-07 11:04:49 +0900

    add rails_admin

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :rememberable, :validatable,
          :authentication_keys => [:login_id]
 
   has_many :tasks
@@ -12,14 +12,6 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: true },
             length: { in: 1..100 },
             format: { with: /\A[a-z\d]{1,100}+\z/i, message: '100文字以内の英数字のみが使えます' }
-
-=begin
-  validates :password,
-            presence: true,
-            length: { in: 8..100 },
-            allow_nil: true,
-            format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,100}+\z/, message: '8〜100文字の半角英小文字大文字数字をそれぞれ1種類以上含む必要があります' }
-=end
 
   # No use email
   def email_required?
