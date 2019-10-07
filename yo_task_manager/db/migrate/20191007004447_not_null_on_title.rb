e0aa7a6c16d0f4bc1b75c2370154993e81293d8c
Yo_IT <10822260+yo-it-engineer@users.noreply.github.com>
2019-10-07 14:32:57 +0900

    fix rubocop warning

--- yo_task_manager/db/migrate/20191007004447_not_null_on_title.rb
+++ yo_task_manager/db/migrate/20191007004447_not_null_on_title.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class NotNullOnTitle < ActiveRecord::Migration[6.0]
   def change
     change_column_null :tasks, :title, false
--- yo_task_manager/spec/models/task_spec.rb
+++ yo_task_manager/spec/models/task_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   context 'title cannot be nil' do
     it 'should return error' do
       task = Task.new(title: nil, body: 'test')
-      expect{task.save!}.to raise_error(ActiveRecord::RecordInvalid)
+      expect { task.save! }.to raise_error(ActiveRecord::RecordInvalid)
       expect(task.valid?).to eq false
     end
   end
@@ -12,7 +14,7 @@ RSpec.describe Task, type: :model do
   context 'title cannot allow empty string' do
     it 'should return error' do
       task = Task.new(title: '', body: 'test')
-      expect{task.save!}.to raise_error(ActiveRecord::RecordInvalid)
+      expect { task.save! }.to raise_error(ActiveRecord::RecordInvalid)
       expect(task.valid?).to eq false
     end
   end
