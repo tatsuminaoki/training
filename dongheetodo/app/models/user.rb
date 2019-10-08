6fda4987d328b5fd4ac4f465bec765fac754d3c2
Donghee KIM <lastgleam@gmail.com>
2019-10-08 12:30:25 +0900

    Fix user model validation

--- dongheetodo/app/models/user.rb
+++ dongheetodo/app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email, uniqueness: true
-  validates :password_digest, length: { minimum: 8 }
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true, length: { minimum: 8 }
 end
