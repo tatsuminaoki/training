3408fb7b0c6923232029b1d3f1ec38ac32c8e70e
Donghee KIM <lastgleam@gmail.com>
2019-10-08 15:40:30 +0900

    Add user email validation

--- dongheetodo/app/models/user.rb
+++ dongheetodo/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: true }
   validates :password_digest, presence: true, length: { minimum: 8 }
 end
