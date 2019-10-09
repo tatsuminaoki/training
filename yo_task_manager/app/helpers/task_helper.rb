1bbbe6b6f161637790b8c359c556903f0e4e3d10
Yo_IT <10822260+yo-it-engineer@users.noreply.github.com>
2019-10-09 15:42:43 +0900

    refactor aasm_state select field to task_helper

--- yo_task_manager/app/helpers/task_helper.rb
+++ yo_task_manager/app/helpers/task_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module TaskHelper
+  def aasm_states_option
+    # generates [ ['', ''], ['未着手', :not_yet], ['着手中', :on_going], ... ]
+    [['', '']].concat(Task.aasm.states.map(&:name).collect {|t| [Task.human_attribute_name("aasm_state.#{t.to_s}"), t]})
+  end
 end
