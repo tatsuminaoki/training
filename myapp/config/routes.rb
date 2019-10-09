6a6d1228c0851f2099a87f316fff060f6ac3a35a
yukari6 <yukari_ogura@fablic.co.jp>
2019-10-09 10:29:28 +0900

    Add named route and link.

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'task#index'
   get 'task/index'
-  get 'task/show'
   get 'task/new'
+  get 'task/:id', to: 'task#show', as: 'task'
   post 'task/create'
-  get 'task/edit'
+  get 'task/:id/edit', to: 'task#edit', as: 'task_edit'
   get 'task/update'
   post 'task/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
