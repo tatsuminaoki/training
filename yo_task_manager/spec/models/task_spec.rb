033689a6c28f2d01614dc1cdf7547b00f17d3cfa
Yo_IT <10822260+yo-it-engineer@users.noreply.github.com>
2019-10-09 14:46:43 +0900

    add model spec for tasks searching

--- yo_task_manager/spec/models/task_spec.rb
+++ yo_task_manager/spec/models/task_spec.rb
@@ -18,4 +18,30 @@ RSpec.describe Task, type: :model do
       expect(task.valid?).to eq false
     end
   end
+
+  context 'task searching' do
+    let!(:task1) { Task.create(title: 'task1', aasm_state: 'not_yet') }
+    let!(:task2) { Task.create(title: 'task2', aasm_state: 'on_going') }
+    let!(:task3) { Task.create(title: 'task3', aasm_state: 'done') }
+    let!(:task4) { Task.create(title: 'task4', aasm_state: 'on_going') }
+    subject { Task.ransack(params).result(distinct: true) }
+    context 'search with non exist title' do
+      let(:params) {{ title_cont: 'not exist', aasm_state_eq: '' }}
+      it 'should return 0 result' do
+        expect(subject.count).to eq 0
+      end
+    end
+    context 'search with existed title' do
+      let(:params) {{ title_cont: 'task', aasm_state_eq: '' }}
+      it 'should return 4 result' do
+        expect(subject.count).to eq 4
+      end
+    end
+    context 'search with aasm_state' do
+      let(:params) {{ title_cont: '', aasm_state_eq: 'on_going' }}
+      it 'should return 2 result' do
+        expect(subject.count).to eq 2
+      end
+    end
+  end
 end
