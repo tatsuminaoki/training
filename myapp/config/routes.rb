4c41ccb16e149bb220d3020b8b18eaeaa72c5dde
yukari6 <yukari_ogura@fablic.co.jp>
2019-10-09 14:01:40 +0900

    Refactor for route.

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
+  resources :tasks
   root to: 'tasks#index'
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/:id', to: 'tasks#show', as: 'tasks'
-  post 'tasks/create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'tasks_edit'
-  get 'tasks/update'
-  post 'tasks/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
