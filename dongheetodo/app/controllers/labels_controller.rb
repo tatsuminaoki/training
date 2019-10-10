87ff09de4e1d854febd7098373902ca7badca756
Donghee KIM <lastgleam@gmail.com>
2019-10-10 11:44:00 +0900

    Add features for labels

---
+++ dongheetodo/app/controllers/labels_controller.rb
@@ -0,0 +1,29 @@
+class LabelsController < ApplicationController
+  def index
+    @label = Label.new
+    select_labels
+  end
+
+  def create
+    @label = Label.new(label_params)
+    if @label.save
+      flash[:success] = t("message.success.complete_create")
+    end
+    select_labels
+    render "index"
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def label_params
+    params.require(:label).permit(:name, :color)
+  end
+
+  def select_labels
+    @labels = Label.all.includes(:task_labels).includes(:tasks).page(params[:page]).per(20)
+  end
+end
