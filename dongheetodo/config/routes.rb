4faa6172454f86c514f38b2358dfad0da8fdd6db
Donghee KIM <lastgleam@gmail.com>
2019-10-10 15:00:10 +0900

    Fix routes

--- dongheetodo/config/routes.rb
+++ dongheetodo/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks
-  resources :labels, only: %i[index create destroy]
+  resources :labels, only: %i[index create]
 
   root "tasks#index"
 
