e221b452b1242642eff07e675ed8473fe205d639
Donghee KIM <lastgleam@gmail.com>
2019-10-10 11:34:44 +0900

    Update models

--- dongheetodo/app/models/task.rb
+++ dongheetodo/app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
-  has_many :labels
+  has_many :task_labels
+  has_many :labels, through: :task_labels
 
   enum status: {todo: 1, doing: 2, done: 3}
   enum priority: {low: 1, mid: 2, high: 3}
