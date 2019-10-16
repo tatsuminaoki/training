9b5594afe3ebbbbbe26e88f4ef1bf92af7f44a21
yukari6 <yukari_ogura@fablic.co.jp>
2019-10-16 14:02:19 +0900

    Fix flash message template.

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -16,7 +16,7 @@ class TasksController < ApplicationController
     # @TODO step16でユーザ登録機能を実装したら任意のユーザで登録するよう修正
     @task[:user_id] = 1
     if @task.save
-      flash[:success] = t 'flash.create.success', id: @task.id
+      flash[:success] = t 'flash.create.success'
       redirect_to task_path(@task.id)
     else
       flash[:fail] = t 'flash.create.fail'
@@ -31,10 +31,10 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     if @task.update(task_params)
-      flash[:success] = t 'flash.update.success', id: @task.id
+      flash[:success] = t 'flash.update.success'
       redirect_to task_path(@task.id)
     else
-      flash[:fail] = t 'flash.update.fail', id: @task.id
+      flash[:fail] = t 'flash.update.fail'
       redirect_to edit_task_path(@task.id)
     end
   end
@@ -42,9 +42,9 @@ class TasksController < ApplicationController
   def destroy
     @task = Task.find(params[:id])
     if @task.destroy
-      flash[:success] = t 'flash.remove.success', id: @task.id
+      flash[:success] = t 'flash.remove.success'
     else
-      flash[:success] = t 'flash.remove.fail', id: @task.id
+      flash[:success] = t 'flash.remove.fail'
     end
     redirect_to tasks_path
   end
