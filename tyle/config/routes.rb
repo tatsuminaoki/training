4d627afe4ad8a824396346e4a027023e3c5d76ef
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-10-18 14:47:20 +0900

    enable you to see tasks in localhost:3000/

--- tyle/config/routes.rb
+++ tyle/config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   # get 'tasks/update'
   # get 'tasks/destroy'
   resources :tasks
+  get '', to: 'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
