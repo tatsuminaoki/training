a9251d18cc7c7269861cea8cc1c5c4f03fbef17d
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-10-23 13:55:56 +0900

    update routing

--- tyle/config/routes.rb
+++ tyle/config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  # get 'tasks/index'
-  # get 'tasks/new'
-  # get 'tasks/create'
-  # get 'tasks/show'
-  # get 'tasks/edit'
-  # get 'tasks/update'
-  # get 'tasks/destroy'
   resources :tasks
-  get '', to: 'tasks#index'
+  root to: 'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
