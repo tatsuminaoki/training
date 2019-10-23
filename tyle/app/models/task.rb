5dd4ea178f92d5d5fbd7397e4ddbbc907daa6268
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-10-23 13:56:27 +0900

    use symbol instead of string

--- tyle/app/models/task.rb
+++ tyle/app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user
-  enum priority: { low: 0, medium: 1, high: 2 }
-  enum status: { 'waiting': 0, 'in progress': 1, done: 2 }
+  enum priority: %i[low medium high]
+  enum status: %i[waiting in_progress done]
 end
