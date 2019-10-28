1406fd228867fe32e9db6bcfe3a29375252ac8ed
yukari6 <yukari_ogura@fablic.co.jp>
2019-10-28 11:26:46 +0900

    Reflect review comment.

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -19,7 +19,7 @@ class TasksController < ApplicationController
       flash[:success] = t('flash.create.success')
       redirect_to task_path(@task.id)
     else
-      flash[:fail] = t('flash.create.fail')
+      flash.now[:fail] = t('flash.create.fail')
       render :new
     end
   end
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
       flash[:success] = t('flash.update.success')
       redirect_to task_path(@task.id)
     else
-      flash[:fail] = t('flash.update.fail')
+      flash.now[:fail] = t('flash.update.fail')
       render :edit
     end
   end
