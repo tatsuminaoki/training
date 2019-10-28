40a2aba99a45828c6a0ce0e2a9df86c40c53074b
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-10-28 10:23:15 +0900

    update a rspec test of tasks

--- tyle/spec/system/tasks_spec.rb
+++ tyle/spec/system/tasks_spec.rb
@@ -3,14 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  # all tests need a user; therefore let! is used. let is not used.
-  let!(:user) { User.create(id: 1, name: 'user1', login_id: 'id1', password_digest: 'password1') }
+  let(:user) { User.create(id: 1, name: 'user1', login_id: 'id1', password_digest: 'password1') }
+  let!(:task) { Task.create(name: 'task1', description: 'this is a task1', user_id: user.id, priority: 0, status: 0) }
+  let!(:task2) { Task.create(name: 'task2', description: 'this is a task2', user_id: user.id, priority: 1, status: 1) }
+  let!(:task3) { Task.create(name: 'task3', description: 'this is a task3', user_id: user.id, priority: 2, status: 2) }
 
-  it 'testing of tasks/' do
-    @task = Task.create!(name: 'task1', description: 'this is a task1', user_id: user.id, priority: 0, status: 0)
-    @task = Task.create!(name: 'task2', description: 'this is a task2', user_id: user.id, priority: 1, status: 1)
-    @task = Task.create!(name: 'task3', description: 'this is a task3', user_id: user.id, priority: 2, status: 2)
-
+  it 'tests /tasks/' do
     visit tasks_path
     expect(page).to have_content 'task1'
     expect(page).to have_content 'low'
@@ -23,7 +21,7 @@ RSpec.describe 'Tasks', type: :system do
     expect(page).to have_content 'done'
   end
 
-  it 'testing of tasks/new' do
+  it 'tests /tasks/new' do
     visit new_task_path
     fill_in 'task_name', with: 'task1'
     fill_in 'task_description', with: 'this is a task1'
@@ -38,24 +36,20 @@ RSpec.describe 'Tasks', type: :system do
     expect(page).to have_content 'in_progress'
   end
 
-  it 'testing of tasks/show' do
-    @task = Task.create!(name: 'task1', description: 'this is a task1', user_id: user.id, priority: 1, status: 1)
-
-    visit task_path(@task)
+  it 'tests /tasks/show' do
+    visit task_path(task)
     expect(page).to have_content 'task1'
     expect(page).to have_content 'this is a task1'
-    expect(page).to have_content 'medium'
-    expect(page).to have_content 'in_progress'
+    expect(page).to have_content 'low'
+    expect(page).to have_content 'waiting'
   end
 
-  it 'testing of tasks/edit' do
-    @task = Task.create!(name: 'task1', description: 'this is a task1', user_id: user.id, priority: 1, status: 1)
-
-    visit edit_task_path(@task)
+  it 'tests /tasks/edit' do
+    visit edit_task_path(task)
     expect(page).to have_field 'task_name', with: 'task1'
     expect(page).to have_field 'task_description', with: 'this is a task1'
-    expect(page).to have_field 'task_priority', with: 'medium'
-    expect(page).to have_field 'task_status', with: 'in_progress'
+    expect(page).to have_field 'task_priority', with: 'low'
+    expect(page).to have_field 'task_status', with: 'waiting'
     fill_in 'task_name', with: 'task2'
     fill_in 'task_description', with: 'this is a task2'
     select 'high', from: 'task_priority'
