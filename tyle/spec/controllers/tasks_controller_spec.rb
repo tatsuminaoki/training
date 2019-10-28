43797b2de91161da5d7295a5f602c5af44b4523a
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-10-28 15:32:12 +0900

    add describe in the task controller spec test

--- tyle/spec/controllers/tasks_controller_spec.rb
+++ tyle/spec/controllers/tasks_controller_spec.rb
@@ -6,38 +6,52 @@ RSpec.describe TasksController, type: :controller do
   let(:user) { User.create(id: 1, name: 'user1', login_id: 'id1', password_digest: 'password1') }
   let(:task) { Task.create(name: 'task1', description: 'this is a task1', user_id: user.id, priority: 0, status: 0) }
 
-  it 'tests get index' do
-    get :index
-    expect(response).to be_successful
+  describe 'GET #index' do
+    it 'returns http success' do
+      get :index
+      expect(response).to be_successful
+    end
   end
 
-  it 'tests get new' do
-    get :new
-    expect(response).to be_successful
+  describe 'GET #new' do
+    it 'returns http success' do
+      get :new
+      expect(response).to be_successful
+    end
   end
 
-  it 'tests post create' do
-    post :create, params: { task: { name: 'task1', description: 'this is a task1', user_id: user.id, priority: 'low', status: 'waiting' } }
-    expect(response).to redirect_to(task_path(Task.last))
+  describe 'POST #create' do
+    it 'returns redirect to the created task page' do
+      post :create, params: { task: { name: 'task1', description: 'this is a task1', user_id: user.id, priority: 'low', status: 'waiting' } }
+      expect(response).to redirect_to(task_path(Task.last))
+    end
   end
 
-  it 'tests get show' do
-    get :show, params: { id: task.id }
-    expect(response).to be_successful
+  describe 'GET #show' do
+    it 'returns http success' do
+      get :show, params: { id: task.id }
+      expect(response).to be_successful
+    end
   end
 
-  it 'tests get edit' do
-    get :edit, params: { id: task.id }
-    expect(response).to be_successful
+  describe 'GET #edit' do
+    it 'returns http success' do
+      get :edit, params: { id: task.id }
+      expect(response).to be_successful
+    end
   end
 
-  it 'tests get update' do
-    post :update, params: { id: task.id, task: { name: 'task2', description: 'this is a task2', user_id: user.id, priority: 'medium', status: 'in_progress' } }
-    expect(response).to redirect_to(task_path(task))
+  describe 'PATCH #update' do
+    it 'returns redirect to the updated task page' do
+      patch :update, params: { id: task.id, task: { name: 'task2', description: 'this is a task2', user_id: user.id, priority: 'medium', status: 'in_progress' } }
+      expect(response).to redirect_to(task_path(task))
+    end
   end
 
-  it 'tests get destroy' do
-    get :destroy, params: { id: task.id }
-    expect(response).to redirect_to(tasks_path)
+  describe 'DELETE #destroy' do
+    it 'returns redirect to the index page' do
+      delete :destroy, params: { id: task.id }
+      expect(response).to redirect_to(tasks_path)
+    end
   end
 end
