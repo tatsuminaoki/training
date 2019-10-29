e50c7159e02fe2d910484548e2c88e521c9f2a6f
yukari6 <yukari_ogura@fablic.co.jp>
2019-10-29 17:39:33 +0900

    Make user seed.

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+User.create!(
+  [
+    {
+      name: '楽天花子',
+      email: 'hanako.rakuten@rakuten.com',
+      encrypted_password: '',
+      role: 0
+    },
+    {
+      name: '楽天太郎',
+      email: 'taro.rakuten@rakuten.com',
+      encrypted_password: '',
+      role: 0
+    },
+    {
+      name: '管理者太郎',
+      email: 'taro.rakuten@rakuten.com',
+      encrypted_password: '',
+      role: 1
+    }
+  ]
+)
