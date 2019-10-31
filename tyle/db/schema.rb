c2d2f6a0a30a416626455fef9ccfa5104523d8e1
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-10-31 16:56:29 +0900

    add index in task.name

--- tyle/db/schema.rb
+++ tyle/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_022755) do
+ActiveRecord::Schema.define(version: 2019_10_31_074449) do
 
   create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_022755) do
     t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tasks_on_name"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
