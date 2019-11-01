6f7de7faf18b182262525ffeba580684ebb5e3c6
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-01 15:50:08 +0900

    refactor rspec tests

--- tyle/spec/models/task_spec.rb
+++ tyle/spec/models/task_spec.rb
@@ -8,40 +8,40 @@ RSpec.describe Task, type: :model do
   describe '#create' do
     let(:task) { create(:task, { user_id: user.id }) }
 
-    it 'creates a task' do
+    it 'successfully creates a task' do
       expect(task).to be_valid
       expect(task.errors.count).to eq(0)
     end
 
-    it 'creates a task without a name' do
+    it 'fails to create a task without a name' do
       expect { create(:task, { name: nil, user_id: user.id }) }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
-    it 'creates a task without a priority' do
+    it 'fails to create a task without a priority' do
       expect { create(:task, { priority: nil, user_id: user.id }) }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
-    it 'creates a task without a status' do
+    it 'fails to create a task without a status' do
       expect { create(:task, { status: nil, user_id: user.id }) }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
-    it 'creates a task without a status' do
+    it 'fails to create a task without a status' do
       expect { create(:task, { due: nil, user_id: user.id }) }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
-    it 'a task with priority number over' do
+    it 'fails to create a task with priority number over' do
       expect { create(:task, { priority: 4, user_id: user.id }) }.to raise_error(ArgumentError)
     end
 
-    it 'a task with priority number under' do
+    it 'fails to create a task with priority number under' do
       expect { create(:task, { priority: -1, user_id: user.id }) }.to raise_error(ArgumentError)
     end
 
-    it 'a task with status number over' do
+    it 'fails to create a task with status number over' do
       expect { create(:task, { status: 4, user_id: user.id }) }.to raise_error(ArgumentError)
     end
 
-    it 'a task with status number under' do
+    it 'fails to create a task with status number under' do
       expect { create(:task, { status: -1, user_id: user.id }) }.to raise_error(ArgumentError)
     end
   end
--- tyle/spec/system/tasks_spec.rb
+++ tyle/spec/system/tasks_spec.rb
@@ -12,99 +12,115 @@ RSpec.describe 'Tasks', type: :system do
       create(:task3, { user_id: user.id })
     end
 
-    it 'tests /tasks/' do
-      visit tasks_path
-      expect(page).to have_content 'task1'
-      expect(page).to have_content 'low'
-      expect(page).to have_content 'waiting'
-      expect(page).to have_content '2020/12/31'
-      expect(page).to have_content 'task2'
-      expect(page).to have_content 'medium'
-      expect(page).to have_content 'in_progress'
-      expect(page).to have_content '2021/01/01'
-      expect(page).to have_content 'task3'
-      expect(page).to have_content 'high'
-      expect(page).to have_content 'done'
-      expect(page).to have_content '2021/01/02'
+    context 'visit tasks_path' do
+      subject { visit tasks_path }
+
+      it 'lists the appropriate tasks' do
+        subject
+        expect(page).to have_content 'task1'
+        expect(page).to have_content 'low'
+        expect(page).to have_content 'waiting'
+        expect(page).to have_content '2020/12/31'
+        expect(page).to have_content 'task2'
+        expect(page).to have_content 'medium'
+        expect(page).to have_content 'in_progress'
+        expect(page).to have_content '2021/01/01'
+        expect(page).to have_content 'task3'
+        expect(page).to have_content 'high'
+        expect(page).to have_content 'done'
+        expect(page).to have_content '2021/01/02'
+      end
     end
 
-    it 'tests /tasks/new' do
-      visit new_task_path
-      fill_in 'task_name', with: 'task1'
-      fill_in 'task_description', with: 'this is a task1'
-      fill_in 'task_due', with: '20201231'
-      select 'medium', from: 'task_priority'
-      select 'in_progress', from: 'task_status'
-      click_button '登録する'
+    context 'visit new_task_path' do
+      subject { visit new_task_path }
 
-      expect(page).to have_content 'タスクが追加されました！'
-      expect(page).to have_content 'task1'
-      expect(page).to have_content 'this is a task1'
-      expect(page).to have_content 'medium'
-      expect(page).to have_content 'in_progress'
-      expect(page).to have_content '2020/12/31'
+      it 'enables you to create a new task' do
+        subject
+        visit new_task_path
+        fill_in 'task_name', with: 'task1'
+        fill_in 'task_description', with: 'this is a task1'
+        fill_in 'task_due', with: '20201231'
+        select 'medium', from: 'task_priority'
+        select 'in_progress', from: 'task_status'
+        click_button '登録する'
+
+        expect(page).to have_content 'タスクが追加されました！'
+        expect(page).to have_content 'task1'
+        expect(page).to have_content 'this is a task1'
+        expect(page).to have_content 'medium'
+        expect(page).to have_content 'in_progress'
+        expect(page).to have_content '2020/12/31'
+      end
     end
 
-    it 'tests /tasks/show' do
-      visit task_path(task)
-      expect(page).to have_content 'task1'
-      expect(page).to have_content 'this is a task1'
-      expect(page).to have_content 'low'
-      expect(page).to have_content 'waiting'
-      expect(page).to have_content '2020/12/31'
+    context 'visit task_path(task)' do
+      subject { visit task_path(task) }
+
+      it 'shows the appropriate task' do
+        subject
+        expect(page).to have_content 'task1'
+        expect(page).to have_content 'this is a task1'
+        expect(page).to have_content 'low'
+        expect(page).to have_content 'waiting'
+        expect(page).to have_content '2020/12/31'
+      end
+
+      it 'enables you to delete the task with the delete button' do
+        subject
+        expect(page).to have_content 'task1'
+        expect(page).to have_content 'low'
+        expect(page).to have_content 'waiting'
+        expect(page).to have_content '2020/12/31'
+
+        # click DELETE and Cancel
+        click_on '削除'
+        expect(page.driver.browser.switch_to.alert.text).to eq '本当にタスクを削除してもいいですか？'
+        page.driver.browser.switch_to.alert.dismiss
+
+        expect(page).to have_content 'task1'
+        expect(page).to have_content 'this is a task1'
+        expect(page).to have_content 'low'
+        expect(page).to have_content 'waiting'
+        expect(page).to have_content '2020/12/31'
+
+        # click DELETE and OK
+        click_on '削除'
+        expect(page.driver.browser.switch_to.alert.text).to eq '本当にタスクを削除してもいいですか？'
+        page.driver.browser.switch_to.alert.accept
+
+        expect(page).to have_no_content 'task1'
+        expect(page).to have_no_content 'this is a task1'
+        expect(page).to have_no_content 'low'
+        expect(page).to have_no_content 'waiting'
+        expect(page).to have_no_content '2020/12/31'
+      end
     end
 
-    it 'tests /tasks/edit' do
-      visit edit_task_path(task)
-      expect(page).to have_field 'task_name', with: 'task1'
-      expect(page).to have_field 'task_description', with: 'this is a task1'
-      expect(page).to have_field 'task_priority', with: 'low'
-      expect(page).to have_field 'task_status', with: 'waiting'
-      expect(page).to have_field 'task_due', with: '2020-12-31 00:00:00 +0900'
-      fill_in 'task_name', with: 'task2'
-      fill_in 'task_description', with: 'this is a task2'
-      fill_in 'task_due', with: '20210101'
-      select 'high', from: 'task_priority'
-      select 'done', from: 'task_status'
-      click_button '更新する'
+    context 'visit edit_task_path(task)' do
+      subject { visit edit_task_path(task) }
 
-      expect(page).to have_content 'タスクが更新されました！'
-      expect(page).to have_content 'task2'
-      expect(page).to have_content 'this is a task2'
-      expect(page).to have_content 'high'
-      expect(page).to have_content 'done'
-      expect(page).to have_content '2021/01/01'
-    end
+      it 'tests /tasks/edit' do
+        subject
+        expect(page).to have_field 'task_name', with: 'task1'
+        expect(page).to have_field 'task_description', with: 'this is a task1'
+        expect(page).to have_field 'task_priority', with: 'low'
+        expect(page).to have_field 'task_status', with: 'waiting'
+        expect(page).to have_field 'task_due', with: '2020-12-31 00:00:00 +0900'
+        fill_in 'task_name', with: 'task2'
+        fill_in 'task_description', with: 'this is a task2'
+        fill_in 'task_due', with: '20210101'
+        select 'high', from: 'task_priority'
+        select 'done', from: 'task_status'
+        click_button '更新する'
 
-    it 'tests a delete button at /tasks/show' do
-      visit tasks_path
-      expect(page).to have_content 'task1'
-      expect(page).to have_content 'low'
-      expect(page).to have_content 'waiting'
-      expect(page).to have_content '2020/12/31'
-      visit task_path(task)
-
-      # click DELETE and Cancel
-      click_on '削除'
-      expect(page.driver.browser.switch_to.alert.text).to eq '本当にタスクを削除してもいいですか？'
-      page.driver.browser.switch_to.alert.dismiss
-
-      expect(page).to have_content 'task1'
-      expect(page).to have_content 'this is a task1'
-      expect(page).to have_content 'low'
-      expect(page).to have_content 'waiting'
-      expect(page).to have_content '2020/12/31'
-
-      # click DELETE and OK
-      click_on '削除'
-      expect(page.driver.browser.switch_to.alert.text).to eq '本当にタスクを削除してもいいですか？'
-      page.driver.browser.switch_to.alert.accept
-
-      expect(page).to have_no_content 'task1'
-      expect(page).to have_no_content 'this is a task1'
-      expect(page).to have_no_content 'low'
-      expect(page).to have_no_content 'waiting'
-      expect(page).to have_no_content '2020/12/31'
+        expect(page).to have_content 'タスクが更新されました！'
+        expect(page).to have_content 'task2'
+        expect(page).to have_content 'this is a task2'
+        expect(page).to have_content 'high'
+        expect(page).to have_content 'done'
+        expect(page).to have_content '2021/01/01'
+      end
     end
   end
 
