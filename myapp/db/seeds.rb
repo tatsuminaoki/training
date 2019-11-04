836d47cbdb47eef3607f227be5b0b00dc5e35da5
yukari6 <yukari_ogura@fablic.co.jp>
2019-11-05 08:21:37 +0900

    Fix code following coding rule.

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -5,19 +5,19 @@ User.create!(
       name: '楽天花子',
       email: 'hanako.rakuten@rakuten.com',
       encrypted_password: '',
-      role: 0
+      role: 0,
     },
     {
       name: '楽天太郎',
       email: 'taro.rakuten@rakuten.com',
       encrypted_password: '',
-      role: 0
+      role: 0,
     },
     {
       name: '管理者太郎',
       email: 'taro.admin@rakuten.com',
       encrypted_password: '',
-      role: 1
+      role: 1,
     }
   ]
 )
