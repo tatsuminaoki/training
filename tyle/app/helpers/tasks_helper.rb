0abe54c540082dee27016670233d3cfb3f244287
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-05 09:54:23 +0900

    Refactor switch_asc_and_desc with rails_helper

--- tyle/app/helpers/tasks_helper.rb
+++ tyle/app/helpers/tasks_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module TasksHelper
+  def switch_asc_and_desc(column)
+    if params[:sort] == column && params[:direction] == 'desc'
+      link_to t("activerecord.attributes.task.#{column}"), sort: column, direction: 'asc'
+    else
+      link_to t("activerecord.attributes.task.#{column}"), sort: column, direction: 'desc'
+    end
+  end
 end
