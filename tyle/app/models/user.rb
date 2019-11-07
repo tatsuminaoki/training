35b6f7dc62cbae4127cc6f0ccdf365149b3d2b62
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-07 13:09:13 +0900

    Update user model

--- tyle/app/models/user.rb
+++ tyle/app/models/user.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password validations: true
 
   has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
-  validates :login_id, presence: true
+  validates :login_id, presence: true, uniqueness: true
   validates :password, length: { minimum: 8 }, presence: true, on: :create
   validates :password, length: { minimum: 8 }, presence: true, on: :update
 end
--- tyle/db/schema.rb
+++ tyle/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_074449) do
+ActiveRecord::Schema.define(version: 2019_11_07_040709) do
 
   create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_10_31_074449) do
     t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_token"
   end
 
   add_foreign_key "task_labels", "labels"
