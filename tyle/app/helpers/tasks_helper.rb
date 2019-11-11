ae8a88a3a1025b132ff26388ce8692303f8dfe8b
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-11 18:15:27 +0900

    Refactor the index page view

--- tyle/app/helpers/tasks_helper.rb
+++ tyle/app/helpers/tasks_helper.rb
@@ -2,15 +2,18 @@
 
 module TasksHelper
   def switch_asc_and_desc(column)
-    if params[:sort] == column && params[:direction] == 'desc'
-      link_to t("activerecord.attributes.task.#{column}"), sort: column, direction: 'asc'
-    else
+    if params[:sort] == column && params[:direction] == 'asc'
       link_to t("activerecord.attributes.task.#{column}"), sort: column, direction: 'desc'
+    else
+      link_to t("activerecord.attributes.task.#{column}"), sort: column, direction: 'asc'
     end
   end
 
   def show_asc_or_desc(column)
-    if params[:sort] == column && params[:direction] == 'desc' then
+    default = (column == 'created_at' && params[:sort] == nil && params[:direction] == nil)
+    if default then
+      '▼'
+    elsif params[:sort] == column && params[:direction] == 'desc' then
       '▼'
     elsif params[:sort] == column && params[:direction] == 'asc' then
       '▲'
