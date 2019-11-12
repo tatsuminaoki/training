60bf916f486a9a602f74afeb6c0077224ee8ad82
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-13 07:34:08 +0900

    Support Rails 6.0 update

--- tyle/app/models/user.rb
+++ tyle/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
-  validates :login_id, presence: true, uniqueness: true
+  validates :login_id, presence: true, uniqueness: { case_sensitive: true }
   validates :password, length: { minimum: 8 }, presence: true, on: :create
 
   def self.new_remember_token
