660e8ed9d2c862078cc6d9f6d70f1e8abbe3f44d
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-13 15:09:30 +0900

    Add admin pages

---
+++ tyle/app/controllers/admin/users_controller.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+class Admin::UsersController < ApplicationController
+  before_action :user, only: %i[show edit update destroy]
+
+  def index
+    @users = User.eager_load(:tasks).order(created_at: :desc)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path(@user), notice: t('message.user.created')
+    else
+      render :new
+    end
+  end
+
+  def show
+    @tasks = Task.where(user_id: @user.id)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: t('message.user.updated')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if current_user == @user
+      redirect_to admin_users_path, notice: t('message.user.destroyed')
+    else
+      if @user.destroy
+        redirect_to admin_users_path, notice: t('message.user.destroyed')
+      else
+        redirect_to admin_users_path
+      end
+    end
+  end
+
+  private
+
+  def user
+    @user = User.find(params[:id])
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :login_id, :password, :password_confirmation, :role)
+  end
+end
--- tyle/config/routes.rb
+++ tyle/config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :users
+  resources :users, only: %i[new create]
   resources :tasks
+  namespace :admin do
+    resources :users
+  end
   root to: 'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
