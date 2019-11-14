f07f7eb718fc68a233478cccce685e0675c86daf
Tadashi Toyokura <tadashi.toyokura@rakuten.com>
2019-11-14 12:53:44 +0900

    add sort link

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -47,7 +47,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description, :status)
+    params.require(:task).permit(:name, :description, :status, :deadline)
   end
 
   def find_task
--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -13,8 +13,15 @@ class Task < ApplicationRecord
     Task.statuses.map { |k, v| [Task.human_attribute_name("status.#{k}"), v] }.to_h
   end
 
+  def readble_deadline
+    self.deadline.present? ? self.deadline : I18n.t(:no_deadline)
+  end
+
   def self.find_with_conditions(params)
-    tasks = Task.order(created_at: :desc).page params[:page]
+    sort_column = params[:sort_column].present? ? params[:sort_column] : 'created_at'
+    order = params[:order].present? ? params[:order] : 'desc'
+    tasks = Task.order(sort_column.to_sym => order.to_sym)
+    tasks = tasks.page params[:page]
     tasks = tasks.where('name like ?', "%#{params[:name]}%") if params[:name].present?
     tasks = tasks.where(status: params[:status].to_i) if params[:status].present?
     tasks
