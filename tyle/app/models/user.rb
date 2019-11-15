74515d3bc6b11d86f94947124d6816ad88e45624
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-15 11:51:23 +0900

    Add validation before update and destroy

--- tyle/app/models/user.rb
+++ tyle/app/models/user.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_update :update_the_last_admin_validator
+  before_destroy :destroy_the_last_admin_validator
+
   has_secure_password validations: true
 
   has_many :tasks, dependent: :destroy
@@ -18,5 +21,15 @@ class User < ApplicationRecord
 
   def self.encrypt(token)
     Digest::SHA256.hexdigest(token.to_s)
+  end
+
+  def update_the_last_admin_validator
+    return if self.role == 'general' && User.where(role: 1).count == 1 && User.find_by(role: 1).id == self.id
+    throw :abort
+  end
+
+  def destroy_the_last_admin_validator
+    return if User.where(role: 1).count == 1 && User.find_by(role: 1).id == self.id
+    throw :abort
   end
 end
