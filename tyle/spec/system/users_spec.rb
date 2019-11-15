53ea134df17038d9a2b1a3af76fb917ae67aab91
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-15 11:51:34 +0900

    rubocop

--- tyle/spec/system/users_spec.rb
+++ tyle/spec/system/users_spec.rb
@@ -5,21 +5,21 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :system do
   describe 'register and login' do
     it 'register and login' do
-    # register
-    visit new_user_path
-    fill_in 'user_name', with: 'user1'
-    fill_in 'user_login_id', with: 'id1'
-    fill_in 'user_password', with: 'password1'
-    fill_in 'user_password_confirmation', with: 'password1'
-    click_on '登録する'
+      # register
+      visit new_user_path
+      fill_in 'user_name', with: 'user1'
+      fill_in 'user_login_id', with: 'id1'
+      fill_in 'user_password', with: 'password1'
+      fill_in 'user_password_confirmation', with: 'password1'
+      click_on '登録する'
 
-    # login
-    fill_in 'session_login_id', with: 'id1'
-    fill_in 'session_password', with: 'password1'
-    click_on 'ログイン'
-    sleep 1
+      # login
+      fill_in 'session_login_id', with: 'id1'
+      fill_in 'session_password', with: 'password1'
+      click_on 'ログイン'
+      sleep 1
 
-    expect(page).to have_content 'TYLE'
+      expect(page).to have_content 'TYLE'
     end
   end
 end
