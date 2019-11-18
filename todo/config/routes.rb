35ecb10374dad8f716eed10a2dbe290e36084762
tana2034 <nanako.takahashi@rakuten.com>
2019-11-18 14:06:48 +0900

    installed webpacker

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks/new'
+  post 'tasks/create'
+  get 'tasks/edit'
+  get 'tasks/show'
+  get 'tasks/index'
 end
