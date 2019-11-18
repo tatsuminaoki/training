bd7b03dd65f1e2b105edc77968abe4b21a336406
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-18 10:03:35 +0900

    Add the validation of user.login_id

--- tyle/app/models/user.rb
+++ tyle/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
-  validates :login_id, presence: true
+  validates :login_id, presence: true, uniqueness: { case_sensitive: true }
   validates :password, length: { minimum: 8 }, presence: true, on: :create
   validates :password, length: { minimum: 8 }, presence: true, on: :update
 end
