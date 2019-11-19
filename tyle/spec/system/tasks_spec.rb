c22f9f6a4299333c29d165b7240d48e57707e7fc
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-19 14:13:52 +0900

    Add rspec of search by label

--- tyle/spec/system/tasks_spec.rb
+++ tyle/spec/system/tasks_spec.rb
@@ -181,10 +181,16 @@ RSpec.describe 'Tasks', type: :system do
   end
 
   describe 'search' do
+    let(:task) { create(:task, { user_id: user.id, created_at: 2.days }) }
+    let(:task2) { create(:task2, { user_id: user.id, created_at: 1.day }) }
+    let(:label) { create(:label) }
+    let(:label2) { create(:label2) }
+
     before do
-      create(:task, { user_id: user.id, created_at: 2.days })
-      create(:task2, { user_id: user.id, created_at: 1.day })
       create(:task3, { user_id: user.id, created_at: Time.zone.now })
+
+      task.task_labels.create!(label_id: label.id)
+      task2.task_labels.create!(label_id: label2.id)
     end
 
     context 'visit tasks_path' do
@@ -244,11 +250,30 @@ RSpec.describe 'Tasks', type: :system do
         expect(page).to have_content '2021/01/02'
       end
 
+      it 'tasks searched by label `label1`' do
+        subject
+        select 'label1', from: 'label_ids'
+        click_on '検索'
+        expect(page).to have_content 'task1'
+        expect(page).to have_content 'low'
+        expect(page).to have_content 'waiting'
+        expect(page).to have_content '2020/12/31'
+        expect(page).to have_no_content 'task2'
+        expect(page).to have_no_content 'medium'
+        expect(page).to have_no_content 'in_progress'
+        expect(page).to have_no_content '2021/01/01'
+        expect(page).to have_no_content 'task3'
+        expect(page).to have_no_content 'high'
+        expect(page).to have_no_content 'done'
+        expect(page).to have_no_content '2021/01/02'
+      end
+
       it 'tasks searched by name `task`, priority `Medium`, and status `In_progress`' do
         subject
         fill_in 'search_by_name', with: 'task'
         select 'Medium', from: 'search_by_priority'
         select 'In_progress', from: 'search_by_status'
+        select 'label2', from: 'label_ids'
         click_on '検索'
         expect(page).to have_no_content 'task1'
         expect(page).to have_no_content 'low'
