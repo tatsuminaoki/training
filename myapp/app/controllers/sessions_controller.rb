52f65a7d2f9d78e5dd0425db723922e5f124c2e9
Tadashi Toyokura <tadashi.toyokura@rakuten.com>
2019-11-19 16:46:52 +0900

    fix rubocop

--- myapp/app/controllers/sessions_controller.rb
+++ myapp/app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  before_action :find_user
   skip_before_action :require_login
 
   def new
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user&.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user && @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to root_url, notice: t(:logged_in)
     else
       flash.now[:message] = t :login_failed_msg
@@ -21,5 +21,11 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:message] = t :logged_out
     redirect_to login_url
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(email: params[:email])
   end
 end
