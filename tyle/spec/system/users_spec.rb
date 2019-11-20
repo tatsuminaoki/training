cf2c7b6d8943049ac6c947ae5a112517343a1e8c
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-20 16:53:18 +0900

    Update rspec of system/users and system/sessions

--- tyle/spec/system/users_spec.rb
+++ tyle/spec/system/users_spec.rb
@@ -3,23 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-  describe 'register and login' do
-    it 'register and login' do
-      # register
+  describe 'GET #new' do
+    before do
       visit new_user_path
-      fill_in 'user_name', with: 'user1'
-      fill_in 'user_login_id', with: 'id1'
-      fill_in 'user_password', with: 'password1'
-      fill_in 'user_password_confirmation', with: 'password1'
-      click_on '登録する'
+    end
 
-      # login
-      fill_in 'session_login_id', with: 'id1'
-      fill_in 'session_password', with: 'password1'
-      click_on 'ログイン'
-      sleep 1
+    context 'when you correctly fill out the form' do
+      it 'enables you to successfully create a new user' do
+        fill_in 'user_name', with: 'user1'
+        fill_in 'user_login_id', with: 'id1'
+        fill_in 'user_password', with: 'password1'
+        fill_in 'user_password_confirmation', with: 'password1'
+        click_button '登録する'
 
-      expect(page).to have_content 'TYLE'
+        expect(page).to have_content 'ユーザーが追加されました！'
+        expect(page).to have_content 'ログインIDとパスワードを入力してください'
+
+        # You will log in to verify that you have successfully create the user.
+        fill_in 'session_login_id', with: 'id1'
+        fill_in 'session_password', with: 'password1'
+        click_button 'ログイン'
+
+        expect(page).to have_content 'ログアウト'
+      end
     end
   end
 end
