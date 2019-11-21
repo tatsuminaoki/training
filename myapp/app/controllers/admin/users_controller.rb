5bada8ab556f2c14a34e6a5ed4a4012c3f6fba86
Tadashi Toyokura <tadashi.toyokura@rakuten.com>
2019-11-21 17:47:29 +0900

    add user management.

---
+++ myapp/app/controllers/admin/users_controller.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+class Admin::UsersController < ApplicationController
+  layout "admin/user"
+
+  before_action :find_user, only: %i[edit update show destroy]
+
+  def index
+    @users = User.all.page params[:page]
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:message] = t :new_user_created
+      redirect_to admin_user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @tasks = @user.tasks.find_with_params(params)
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:message] = t :user_updated
+      redirect_to admin_user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:message] = t :user_deleted
+      redirect_to admin_users_path
+    else
+      redirect_to admin_user_path(@user)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :account, :password)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'signup', to: 'users#new', as: 'signup'
+
+  namespace :admin do
+    resources :users
+  end
 end
