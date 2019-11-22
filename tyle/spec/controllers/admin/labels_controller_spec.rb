05c373bb79fc1f94c0bbd684e87037fc0742252c
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-11-22 11:18:34 +0900

    Add label controller rspec

--- tyle/spec/controllers/admin/labels_controller_spec.rb
+++ tyle/spec/controllers/admin/labels_controller_spec.rb
@@ -1,54 +1,111 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Admin::LabelsController, type: :controller do
+  render_views
 
-  describe "GET #index" do
-    it "returns http success" do
+  let(:user) { create(:user) }
+  let!(:label) { create(:label) }
+
+  # login
+  before do
+    remember_token = User.encrypt(cookies[:user_remember_token])
+    cookies.permanent[:user_remember_token] = remember_token
+    user.update!(remember_token: User.encrypt(remember_token))
+  end
+
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
+      expect(response).to render_template('admin/labels/index')
+      expect(response.body).to have_content('ラベルリスト')
+      expect(response.body).to have_content('label1')
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
+  describe 'GET #new' do
+    it 'returns http success' do
       get :new
       expect(response).to have_http_status(:success)
+      expect(response).to render_template('admin/labels/new')
+      expect(response.body).to have_content('追加するラベルの詳細を入力してください')
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
+  describe 'POST #create' do
+    context 'when user sends the correct parameters' do
+      it 'returns the redirect to the label page' do
+        post :create, params: { label: { name: 'label2' } }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(admin_label_path(Label.last))
+
+        get :show, params: { id: Label.last.id }
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('admin/labels/show')
+        expect(response.body).to have_content('ラベルの詳細')
+        expect(response.body).to have_content('label2')
+      end
+    end
+
+    context 'when user sends parameters with empty name' do
+      it 'returns http success' do
+        post :create, params: { label: { name: '' } }
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('admin/labels/new')
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get :show, params: { id: label.id }
       expect(response).to have_http_status(:success)
+      expect(response).to render_template('admin/labels/show')
+      expect(response.body).to have_content('ラベルの詳細')
+      expect(response.body).to have_content('label1')
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
+  describe 'GET #edit' do
+    it 'returns http success' do
+      get :edit, params: { id: label.id }
       expect(response).to have_http_status(:success)
+      expect(response).to render_template('admin/labels/edit')
+      expect(response.body).to have_content('編集するラベルの詳細を入力してください')
     end
   end
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
+  describe 'POST #update' do
+    context 'when user sends the correct parameters' do
+      it 'returns the redirect to the label page' do
+        post :update, params: { id: label.id, label: { name: 'label2' } }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(admin_label_path(label))
+
+        get :show, params: { id: label.id }
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('admin/labels/show')
+        expect(response.body).to have_content('ラベルの詳細')
+        expect(response.body).to have_content('label2')
+      end
+    end
+
+    context 'when user sends parameters with empty name' do
+      it 'returns http success' do
+        post :update, params: { id: label.id, label: { name: '' } }
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('admin/labels/edit')
+      end
     end
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
+  describe 'DELETE #destroy' do
+    it 'returns the redirect to the index page' do
+      delete :destroy, params: { id: label.id }
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(admin_labels_path)
     end
   end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
