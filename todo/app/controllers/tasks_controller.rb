2e327be2cd990a981dcc26acc51d7e37a61e72d3
tana2034 <33181485+tana2034@users.noreply.github.com>
2019-11-22 15:48:17 +0900

    Update todo/app/controllers/tasks_controller.rb
    step10 orderの条件をhashに変更
    
    Co-Authored-By: Hiroki Kishi <hiroki_kishi@fablic.co.jp>

--- todo/app/controllers/tasks_controller.rb
+++ todo/app/controllers/tasks_controller.rb
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
 
   # GET /tasks/
   def index
-    @tasks = Task.all.order('created_at DESC')
+    @tasks = Task.all.order(created_at: :desc)
   end
 
   # DELETE /task/:id
