d27d1fb4cd927251ad60a8a71723389c16680383
Tadashi Toyokura <tadashi.toyokura@rakuten.com>
2019-11-22 12:25:38 +0900

    add role to user

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -2,8 +2,29 @@
 
 class User < ApplicationRecord
   has_secure_password
-  has_many :tasks, dependent: :destroy
+  has_many :tasks, dependent: :delete_all
 
   validates :account, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4 }
   validates :password, length: { minimum: 4 }
+
+  enum role: %i[user admin]
+
+  before_destroy :ensure_atleast_one_admin
+
+  def readable_role
+    User.human_attribute_name("role.#{self.role}")
+  end
+
+  def self.readable_roles
+    User.roles.map { |k, _| [User.human_attribute_name("role.#{k}"), k] }.to_h
+  end
+
+  private
+
+  def ensure_atleast_one_admin
+    if role == 'admin' && User.where(role: 'admin').count == 1
+      errors[:base] << I18n.t(:need_atleast_one_admin)
+      throw :abort
+    end
+  end
 end
