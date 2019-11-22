30b0cf75adf1b224a6eaa4fd236cac27980e1028
tana2034 <nanako.takahashi@rakuten.com>
2019-11-22 10:09:46 +0900

    step9 rubocop auto correct

--- todo/config/application.rb
+++ todo/config/application.rb
@@ -18,7 +18,7 @@ module Todo
     config.active_record.default_timezone = :local
 
     # I18n
-    I18n.available_locales = %i(ja en)
+    I18n.available_locales = %i[ja en]
     I18n.enforce_available_locales = true
     I18n.default_locale = :ja
 
