6d467c965016dbf8eb28f4a29815e12fed0264e5
tana2034 <nanako.takahashi@rakuten.com>
2019-11-25 11:25:19 +0900

    step11 tasksのバリデーション追加

--- todo/app/models/task.rb
+++ todo/app/models/task.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  validates :title, :status, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :status, inclusion: { in: 0..2 }
 end
---
+++ todo/spec/models/task_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  let(:title) { 'title' }
+  let(:status) { 0 }
+  let(:description) { 'a' * 250 }
+  subject { Task.new(title: title, description: description, status: status) }
+
+  describe 'when valid' do
+    context 'with valid attributes' do
+      it { expect(subject).to be_valid }
+    end
+
+    context 'with a max-length title' do
+      let(:title) { 'a' * 250 }
+      it { expect(subject).to be_valid }
+    end
+
+    context 'with a valid status' do
+      let(:status) { 2 }
+      it { expect(subject).to be_valid }
+    end
+  end
+
+  describe 'when not valid' do
+    context 'without a title' do
+      let(:title) { nil }
+      it { expect(subject).to_not be_valid }
+    end
+
+    context 'without a status' do
+      let(:status) { nil }
+      it { expect(subject).to_not be_valid }
+
+      let(:status) { 4 }
+      it { expect(subject).to_not be_valid }
+    end
+
+    context 'with a title over max length' do
+      let(:title) { 'a' * 251 }
+      it { expect(subject).to_not be_valid }
+    end
+  end
+end
