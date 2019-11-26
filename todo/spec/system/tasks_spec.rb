fcfb5822bb39e0c17b3cf7d582d0ba96c6b5e283
tana2034 <nanako.takahashi@rakuten.com>
2019-11-26 17:59:07 +0900

    step11 新規・編集画面でバリデーションエラー表示時のテスト追加

--- todo/spec/system/tasks_spec.rb
+++ todo/spec/system/tasks_spec.rb
@@ -27,36 +27,74 @@ RSpec.describe 'Tasks', type: :system do
     expect(page).to have_content '未着手'
   end
 
-  it 'create new task' do
-    visit tasks_new_path
+  describe 'when create new task' do
+    context 'with valid attributes' do
+      it 'fill all items' do
+        visit tasks_new_path
+        fill_in 'task_title', with: '新規タスク'
+        fill_in 'task_description', with: '新規タスク詳細'
+        select '着手', from: 'task_status'
 
-    fill_in 'task_title', with: '新規タスク'
-    fill_in 'task_description', with: '新規タスク詳細'
-    select '着手', from: 'task_status'
+        click_button '追加'
 
-    click_button '追加'
+        expect(page).to have_content 'Success!'
+        expect(page).to have_content '新規タスク'
+      end
+    end
 
-    expect(page).to have_content 'Success!'
-    expect(page).to have_content '新規タスク'
+    context 'with invalid attributes' do
+      it 'no title' do
+        visit tasks_new_path
+        click_button '追加'
+        expect(page).to have_content 'タスク名 を入力してください'
+      end
+
+      it 'too long title' do
+        visit tasks_new_path
+        fill_in 'task_title', with: 'a' * 251
+        click_button '追加'
+        expect(page).to have_content 'タスク名 が長すぎます'
+      end
+    end
   end
 
-  it 'update task' do
-    visit tasks_edit_path(task)
+  describe 'when update task' do
+    context 'with valid attributes' do
+      it 'fill all items' do
+        visit tasks_edit_path(task)
 
-    expect(page).to have_field 'task_title', with: 'タスク１'
-    expect(page).to have_field 'task_description', with: 'タスク１詳細'
-    expect(page).to have_select 'task_status',
-                                selected: '未着手',
-                                options: %w[未着手 着手 完了]
+        expect(page).to have_field 'task_title', with: 'タスク１'
+        expect(page).to have_field 'task_description', with: 'タスク１詳細'
+        expect(page).to have_select 'task_status',
+                                    selected: '未着手',
+                                    options: %w[未着手 着手 完了]
 
-    fill_in 'task_title', with: 'タスク１修正'
-    fill_in 'task_description', with: 'タスク１詳細修正'
-    select '完了', from: 'task_status'
+        fill_in 'task_title', with: 'タスク１修正'
+        fill_in 'task_description', with: 'タスク１詳細修正'
+        select '完了', from: 'task_status'
 
-    click_button '更新'
+        click_button '更新'
 
-    expect(page).to have_content 'Success!'
-    expect(page).to have_content 'タスク１修正'
+        expect(page).to have_content 'Success!'
+        expect(page).to have_content 'タスク１修正'
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'no title' do
+        visit tasks_edit_path(task)
+        fill_in 'task_title', with: ''
+        click_button '更新'
+        expect(page).to have_content 'タスク名 を入力してください'
+      end
+
+      it 'too long title' do
+        visit tasks_edit_path(task)
+        fill_in 'task_title', with: 'a' * 251
+        click_button '更新'
+        expect(page).to have_content 'タスク名 が長すぎます'
+      end
+    end
   end
 
   it 'delete task' do
