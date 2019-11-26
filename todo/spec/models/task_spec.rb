4d041013f574a01eabf2f1bae927a4c4994b74e1
tana2034 <nanako.takahashi@rakuten.com>
2019-11-26 14:18:34 +0900

    step12 終了期限のバリデーションを追加

--- todo/spec/models/task_spec.rb
+++ todo/spec/models/task_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe Task, type: :model do
   let(:title) { 'title' }
   let(:status) { 0 }
   let(:description) { 'a' * 250 }
-  subject { Task.new(title: title, description: description, status: status) }
+  let(:due_date) { Time.zone.local(2100, 12, 31, 0, 0) }
+  subject { Task.new(title: title, description: description, status: status, due_date: due_date) }
 
   describe 'when valid' do
     context 'with valid attributes' do
@@ -20,6 +21,11 @@ RSpec.describe Task, type: :model do
 
     context 'with a valid status' do
       let(:status) { 2 }
+      it { expect(subject).to be_valid }
+    end
+
+    context 'with a due_date today' do
+      let(:due_date) { Time.zone.now }
       it { expect(subject).to be_valid }
     end
   end
@@ -40,6 +46,11 @@ RSpec.describe Task, type: :model do
 
     context 'with a title over max length' do
       let(:title) { 'a' * 251 }
+      it { expect(subject).to_not be_valid }
+    end
+
+    context 'with a past due_date' do
+      let(:due_date) { Time.zone.now.yesterday }
       it { expect(subject).to_not be_valid }
     end
   end
