96493df9046b85a486ecf6451e4ec58bc6896686
tana2034 <nanako.takahashi@rakuten.com>
2019-11-27 12:29:29 +0900

    step12 tasksのカラムdue_dateをstatusの後ろに移動する

--- todo/db/migrate/20191125051149_add_due_date_to_tasks.rb
+++ todo/db/migrate/20191125051149_add_due_date_to_tasks.rb
@@ -2,6 +2,6 @@
 
 class AddDueDateToTasks < ActiveRecord::Migration[6.0]
   def change
-    add_column :tasks, :due_date, :datetime
+    add_column :tasks, :due_date, :datetime, after: :status
   end
 end
--- todo/db/schema.rb
+++ todo/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_125_051_149) do
-  create_table 'tasks', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.string 'title', null: false
-    t.text 'description'
-    t.integer 'status', default: 0, null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.datetime 'due_date'
+ActiveRecord::Schema.define(version: 2019_11_25_051149) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.integer "status", default: 0, null: false
+    t.datetime "due_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end
