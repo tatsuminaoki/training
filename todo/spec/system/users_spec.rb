4907dced9eb20cd3c548c09518ddf2be25f21e84
tana2034 <nanako.takahashi@rakuten.com>
2019-12-05 18:05:19 +0900

    step17 `rubocop -a`

--- todo/spec/system/users_spec.rb
+++ todo/spec/system/users_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe 'Users', type: :system do
   before do
     driven_by(:rack_test)
   end
