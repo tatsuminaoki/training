daec4f7f6afbca39d517eebc05420c8fb283e325
tana2034 <nanako.takahashi@rakuten.com>
2019-12-06 11:53:13 +0900

    step17 `rails generate controller sessions new` `rails generate rspec:system session`

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'sessions#new'
   get 'signup', to: 'users#new', as: 'signup'
   post 'users/create', to: 'users#create'
   get 'tasks/new'
