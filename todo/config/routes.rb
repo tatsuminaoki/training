868e57bf37eabb20fcc0899d94712933660e08d3
tana2034 <nanako.takahashi@rakuten.com>
2019-12-11 17:01:53 +0900

    step18 tasks周りをresourcesを利用したルーティングに修正

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -9,11 +9,5 @@ Rails.application.routes.draw do
   post 'sessions/create'
   get 'signup', to: 'users#new', as: 'signup'
   post 'users/create', to: 'users#create'
-  get 'tasks/new'
-  post 'tasks/create'
-  get 'tasks/:id', to: 'tasks#show', as: 'tasks_show'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'tasks_edit'
-  patch 'tasks/:id', to: 'tasks#update', as: 'tasks_update'
-  get 'tasks/', to: 'tasks#index'
-  delete 'task/:id', to: 'tasks#destroy', as: 'tasks_destroy'
+  resources :tasks
 end
