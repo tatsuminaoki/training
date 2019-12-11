dbdf6efc22559df952a36b2bcea8b57c74b1444a
tana2034 <nanako.takahashi@rakuten.com>
2019-12-11 11:25:43 +0900

    step18 新しいユーザを追加するページを作成

--- todo/app/controllers/admin/users_controller.rb
+++ todo/app/controllers/admin/users_controller.rb
@@ -1,11 +1,22 @@
 class Admin::UsersController < ApplicationController
   before_action :require_login
 
+  # GET /admin/users
   def index
     @users = User.all.page(params[:page])
   end
 
+  # GET /admin/users/new
   def new
+    @user = User.new
+  end
+
+  # POST /admin/users
+  def create
+    @user = User.new(user_params)
+    return render :new unless @user.save
+    flash[:success] = 'Success!'
+    redirect_to admin_users_path
   end
 
   def edit
@@ -13,5 +24,11 @@ class Admin::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
--- todo/spec/system/admin/admin_users_spec.rb
+++ todo/spec/system/admin/admin_users_spec.rb
@@ -3,14 +3,22 @@ require 'rails_helper'
 RSpec.describe "Admin::Users", type: :system do
 
   let(:administrator) { create(:administrator) }
+  let(:user) { create(:user) }
 
   describe 'when access index' do
     context 'with logged-in user' do
-      it 'show admin users page' do
+      it 'success to show admin users page' do
         log_in_as administrator
         visit admin_users_path
         expect(page.current_path).to eq('/admin/users')
-        expect(page).to have_content('adminstrator')
+        expect(page).to have_content('administrator')
+      end
+
+      it 'success to click link' do
+        log_in_as administrator
+        visit admin_users_path
+        click_on 'Add New User'
+        expect(page.current_path).to eq('/admin/users/new')
       end
     end
 
@@ -36,6 +44,34 @@ RSpec.describe "Admin::Users", type: :system do
       it 'failed to access' do
         visit admin_user_path(administrator)
         expect(page.current_path).not_to eq("/admin/users/#{ administrator.id }")
+      end
+    end
+  end
+
+  describe 'when access new' do
+    context 'with logged-in user' do
+      it 'success to access' do
+        log_in_as administrator
+        visit new_admin_user_path
+        expect(page.current_path).to eq("/admin/users/new")
+      end
+
+      it 'success to add new user' do
+        log_in_as administrator
+        visit new_admin_user_path
+        fill_in 'user_name', with: 'test_user'
+        fill_in 'user_password', with: 'test_password'
+        fill_in 'user_password_confirmation', with: 'test_password'
+        click_on '登録'
+        expect(page.current_path).to eq('/admin/users')
+        expect(page).to have_content 'Success!'
+      end
+    end
+
+    context 'with logged-out user' do
+      it 'failed to access' do
+        visit new_admin_user_path
+        expect(page.current_path).not_to eq('/admin/users/new')
       end
     end
   end
