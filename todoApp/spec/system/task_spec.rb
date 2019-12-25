e820ec903259e338767db3a61d5393d6c5427e82
Shin, Jaeya <jaeya.shin@rakutem.com>
2019-12-25 15:39:30 +0900

    resolve merge conflict from step10

--- todoApp/spec/system/task_spec.rb
+++ todoApp/spec/system/task_spec.rb
@@ -1,79 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe 'Task management', type: :system, js: true do
-  before do
-    %w(first second).each do |nth|
-      Task.create!(title: "rspec #{nth} task", description: 'rspec Description')
+  context 'visit the tasks_path' do
+    before do
+      Task.create!(title: 'rspec first task', description: 'rspec Description')
+      Task.create!(title: 'rspec second task', description: 'rspec Description')
+    end
+
+    it 'shows the task list order by created recently.' do
+      visit tasks_path
+      recent_title = find(:xpath, ".//table/tbody/tr[1]/td[1]").text
+      old_title = find(:xpath, ".//table/tbody/tr[2]/td[1]").text
+      expect(recent_title).to have_content 'rspec second task'
+      expect(old_title).to have_content 'rspec first task'
     end
   end
 
-  scenario 'When user visit the tasks_path it shows the task list order by created recently.' do
-    visit tasks_path
-    recent_title = find(:xpath, ".//table/tbody/tr[1]/td[1]").text
-    old_title = find(:xpath, ".//table/tbody/tr[2]/td[1]").text
-    expect(recent_title).to have_content 'rspec second task'
-    expect(old_title).to have_content 'rspec first task'
-  end
-
-  scenario 'When user click New Task link it creates new task.' do
-    visit tasks_path
-    click_link 'New Task'
-    fill_in 'Title', :with => 'My Task'
-    fill_in 'Description', :with => 'My Task Description'
-    click_button 'Create Task'
-    expect(page).to have_content 'Task was successfully created.'
-  end
-
-  scenario 'Title must be exist when creates new task.' do
-    visit tasks_path
-    click_link 'New Task'
-    fill_in 'Description', :with => 'My Task Description'
-    click_button 'Create Task'
-    expect(page).to have_content "Title can't be blank"
-  end
-
-  scenario 'Title length must be less than 50 when creates new task' do
-    visit tasks_path
-    click_link 'New Task'
-    fill_in 'Title', :with => 'a' * 51
-    fill_in 'Description', :with => 'My Task Description'
-    click_button 'Create Task'
-    expect(page).to have_content 'Title is too long (maximum is 50 characters)'
-  end
-
-  scenario 'When user click Edit link it updates the selected task.' do
-    visit tasks_path
-    first(:link, 'Edit').click
-    fill_in 'Title', :with => 'My Edited Task'
-    fill_in 'Description', :with => 'Edit My Task Description'
-    click_button 'Update Task'
-    expect(page).to have_content 'Task was successfully updated.'
-  end
-
-  scenario 'Title must be exist when updates task.' do
-    visit tasks_path
-    first(:link, 'Edit').click
-    fill_in 'Title', :with => ''
-    fill_in 'Description', :with => 'Edit My Task Description'
-    click_button 'Update Task'
-    expect(page).to have_content "Title can't be blank"
-  end
-
-  scenario 'Title length must be less than 50 when when updates task.' do
-    visit tasks_path
-    first(:link, 'Edit').click
-    fill_in 'Title', :with => 'a' * 51
-    fill_in 'Description', :with => 'Edit My Task Description'
-    click_button 'Update Task'
-    expect(page).to have_content 'Title is too long (maximum is 50 characters)'
-  end
-
-  scenario 'When user click Destroy link it deletes the selected task.' do
-    visit tasks_path
-    accept_alert do
-      first(:link, 'Destroy').click
+  context 'user click the link' do
+    before do
+      Task.create!(title: 'rspec first task', description: 'rspec Description')
     end
-    expect(page).to have_content 'Task was successfully destroyed.'
-  end
 
+    it 'creates new task when click New Task' do
+      visit tasks_path
+      click_link 'New Task'
+      fill_in 'Title', :with => 'My Task'
+      fill_in 'Description', :with => 'My Task Description'
+      click_button 'Create Task'
+      expect(page).to have_content 'Task was successfully created.'
+    end
+
+    it 'Title must be exist when creates new task.' do
+      visit tasks_path
+      click_link 'New Task'
+      fill_in 'Description', :with => 'My Task Description'
+      click_button 'Create Task'
+      expect(page).to have_content "Title can't be blank"
+    end
+
+    it 'Title length must be less than 50 when creates new task' do
+      visit tasks_path
+      click_link 'New Task'
+      fill_in 'Title', :with => 'a' * 51
+      fill_in 'Description', :with => 'My Task Description'
+      click_button 'Create Task'
+      expect(page).to have_content 'Title is too long (maximum is 50 characters)'
+    end
+
+    it 'updates the selected task when click Edit' do
+      visit tasks_path
+      click_link 'Edit'
+      fill_in 'Title', :with => 'My Edited Task'
+      fill_in 'Description', :with => 'Edit My Task Description'
+      click_button 'Update Task'
+      expect(page).to have_content 'Task was successfully updated.'
+    end
+
+    it 'Title must be exist when updates task.' do
+      visit tasks_path
+      click_link 'Edit'
+      fill_in 'Title', :with => ''
+      fill_in 'Description', :with => 'Edit My Task Description'
+      click_button 'Update Task'
+      expect(page).to have_content "Title can't be blank"
+    end
+
+    it 'Title length must be less than 50 when updates task.' do
+      visit tasks_path
+      click_link 'Edit'
+      fill_in 'Title', :with => 'a' * 51
+      fill_in 'Description', :with => 'Edit My Task Description'
+      click_button 'Update Task'
+      expect(page).to have_content 'Title is too long (maximum is 50 characters)'
+    end
+
+    it 'deletes the selected task when click Destroy' do
+      visit tasks_path
+      accept_alert do
+        click_link 'Destroy'
+      end
+      expect(page).to have_content 'Task was successfully destroyed.'
+    end
+  end
 end
