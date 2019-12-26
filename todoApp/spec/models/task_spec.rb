ded1ccbf03301721e705a5cbfee756ab2a9ea071
Shin, Jaeya <jaeya.shin@rakutem.com>
2019-12-26 13:25:36 +0900

    テスト文章修正

--- todoApp/spec/models/task_spec.rb
+++ todoApp/spec/models/task_spec.rb
@@ -16,12 +16,14 @@ RSpec.describe Task, :type => :model do
   context 'When user edits task' do
     it 'ensures title presence' do
       task = Task.create(title: 'sample title', description: 'sample description')
+      expect(task).to be_valid
       task.update(title: '')
       expect(task).to be_invalid
     end
 
     it 'ensures title length is less than 50' do
       task = Task.create(title: 'short title', description: 'short description')
+      expect(task).to be_valid
       task.update(title: 'a' * 51)
       expect(task).to be_invalid
     end
