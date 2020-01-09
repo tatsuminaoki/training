b6daaa71eb9692633fa624048d310cc97a6bc751
Shin, Jaeya <jaeya.shin@rakutem.com>
2020-01-09 13:19:19 +0900

    fix to use to_sym method

--- todoApp/app/models/task.rb
+++ todoApp/app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
 
   def self.order_by_due_date_or_default(due_date_direction)
     if due_date_direction == 'DESC' || due_date_direction == 'ASC'
-      order("due_date #{due_date_direction}")
+      order(due_date: due_date_direction.to_sym)
     else
       order(created_at: :desc)
     end
