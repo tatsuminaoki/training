74f290a6cdbeeee63f6ca2acb717c411b05a28dd
Shin, Jaeya <jaeya.shin@rakutem.com>
2020-01-15 17:35:55 +0900

    scaffold user controller & view

--- todoApp/app/controllers/users_controller.rb
+++ todoApp/app/controllers/users_controller.rb
@@ -1,2 +1,53 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Thank you for signing up!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'create new sentence'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to users_url, notice: 'create new sentence'
+    else
+      redirect_to users_url, notice: 'create new sentence'
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
