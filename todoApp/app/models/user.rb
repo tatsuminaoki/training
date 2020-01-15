f3460c4f19580690123bab72e50bfcdd47963293
Shin, Jaeya <jaeya.shin@rakutem.com>
2020-01-15 14:52:28 +0900

    update validation

--- todoApp/app/models/user.rb
+++ todoApp/app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :tasks
 
   has_secure_password
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: true }
   validates_presence_of :name
 end
