98896a2aaa01336b219923c93debd6c855d90ea9
Shin, Jaeya <jaeya.shin@rakutem.com>
2020-01-17 18:16:17 +0900

    終了期限設定にDatePickerを活用するようにする

--- todoApp/app/javascript/packs/application.js
+++ todoApp/app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("jquery-ui")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +19,7 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import 'bootstrap'
+
+$( document ).on('turbolinks:load', function() {
+  $(".datepicker").datepicker();
+})
