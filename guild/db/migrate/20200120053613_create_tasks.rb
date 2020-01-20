d461d761a81e0fac61d871e5e41446efe2675092
keita-c-saito <keita.c.saito@rakuten.com>
2020-01-20 16:22:06 +0900

    fixed traning step6

---
+++ guild/db/migrate/20200120053613_create_tasks.rb
@@ -0,0 +1,16 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table "tasks", force: :cascade do |t|
+      t.integer  "user_id",     limit: 8,     null: false
+      t.text     "subject",     limit: 65535, null: false
+      t.text     "description", limit: 65535, null: false
+      t.integer  "state",       limit: 1,     null: false
+      t.integer  "priority",    limit: 1,     null: false
+      t.integer  "label",       limit: 1,     null: false
+      t.datetime "created_at",                null: false
+      t.datetime "updated_at",                null: false
+    end
+
+    add_index "tasks", ["user_id"], name: "fk_task_user1", using: :btree
+  end
+end
