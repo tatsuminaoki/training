84977e9acabddde367c548ab005719d22c59f2c0
uzushino <torattoria.mamiya@gmail.com>
2020-01-22 14:11:59 +0900

    検索とテンプレートの変更

--- training_app/app/models/task.rb
+++ training_app/app/models/task.rb
@@ -6,13 +6,24 @@
 #
 #  id         :bigint           not null, primary key
 #  body       :text(65535)      not null
+#  status     :integer          default("todo"), not null
 #  title      :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :integer
 #
+# Indexes
+#
+#  index_tasks_on_status  (status)
+#
 
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  enum status: {
+    todo: 0,
+    progress: 1,
+    done: 2,
+  }
 end
---
+++ training_app/db/migrate/20200122043856_add_column_status_on_tasks.rb
@@ -0,0 +1,6 @@
+class AddColumnStatusOnTasks < ActiveRecord::Migration[6.0]
+  def change
+    add_column :tasks, :status, :integer, null: false, default: 0
+    add_index :tasks, :status
+  end
+end
