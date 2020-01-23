576cc227d0ec32d77682fdbda7273d2c1cc1ec1d
Shin, Jaeya <jaeya.shin@rakutem.com>
2020-01-23 14:08:41 +0900

    シードファイル作成

--- todoApp/db/seeds.rb
+++ todoApp/db/seeds.rb
@@ -1,1 +1,7 @@
-User.create(name: 'John', email: 'test@example.com', roles: 'admin', password: 'mypassword')
+user1 = User.create!(name: 'John', email: 'test@example.com', roles: 'admin', password: 'mypassword')
+task1 = Task.create!(title: 'first task', description: 'first', user_id: user1.id)
+label1 = Label.create!(name: 'first label')
+label2 = Label.create!(name: 'second label')
+label3 = Label.create!(name: 'third label')
+TaskLabel.create!(task_id: task1.id, label_id: label1.id)
+TaskLabel.create!(task_id: task1.id, label_id: label2.id)
