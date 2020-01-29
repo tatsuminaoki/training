88b2fb2ea93aa734400f743bbdb3a19b675239a8
Shin, Jaeya <jaeya.shin@rakutem.com>
2020-01-29 13:21:55 +0900

    modify syntax

--- todoApp/app/models/user.rb
+++ todoApp/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   ## The :user role is added by default and shouldn't be included in this list.             ##
   ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
   ## The multiple option can be set to true if you need users to have multiple roles.       ##
-  petergate(roles: [:admin], multiple: false)                                      ##
+  petergate(roles: [:admin], multiple: false)                                               ##
   ############################################################################################
 
 
@@ -21,7 +21,7 @@ class User < ApplicationRecord
 
   def at_least_one_admin_should_be_exist
     if User.where(roles: 'admin').count <= 1
-      errors.add(:base, "At least one admin should be remain.")
+      errors.add(:base, I18n.t('need_at_least_one_admin'))
       throw :abort
     end
   end
