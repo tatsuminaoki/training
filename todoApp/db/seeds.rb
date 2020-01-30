e292be228a71a01dc4d36abef76cec3b979b3952
Shin, Jaeya <jaeya.shin@rakutem.com>
2020-01-30 15:50:51 +0900

    modify seeds

--- todoApp/db/seeds.rb
+++ todoApp/db/seeds.rb
@@ -1,5 +1,5 @@
 user1 = User.create!(name: 'John', email: 'test@example.com', roles: 'admin', password: 'mypassword')
-task1 = Task.create!(title: 'first task', description: 'first', user_id: user1.id)
+task1 = Task.find_or_create_by!(title: 'first task', description: 'first', user_id: user1.id)
 label1 = Label.create!(name: 'first label')
 label2 = Label.create!(name: 'second label')
 label3 = Label.create!(name: 'third label')
