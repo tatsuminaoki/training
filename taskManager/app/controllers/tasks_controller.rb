347816f92fd51c7a313401e10f0be54267502cc5
Nagano <kazuyoshi.nagano@rakuten.com>
2020-01-31 17:20:36 +0900

    set routing. edit controler and index of view.

--- taskManager/app/controllers/tasks_controller.rb
+++ taskManager/app/controllers/tasks_controller.rb
@@ -1,11 +1,16 @@
 class TasksController < ApplicationController
+
   def index
+    @tasks = Task.all
   end
 
   def new
+    @tasks = Task.new
   end
 
   def create
+    @tasks = Task.create(task_params)
+    redirect_to @task, notice: 'タスクを登録しました'
   end
 
   def show
@@ -15,8 +20,21 @@ class TasksController < ApplicationController
   end
 
   def update
+    @tasks.update(tasks_params)
+    redirect_to @task, notice: 'タスクを更新しました'
   end
 
   def destroy
+    @tasks.destroy
+    redirect_to tasks_url notice: 'タスクを削除しました'
   end
+
+  def task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :priority, :status, :due)
+  end
+
 end
--- taskManager/config/routes.rb
+++ taskManager/config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/create'
-  get 'tasks/show'
-  get 'tasks/edit'
-  get 'tasks/update'
-  get 'tasks/destroy'
+  resources :tasks
+  root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
