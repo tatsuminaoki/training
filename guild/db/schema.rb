85f876ce10a0badfd3a901e8a38059a16780318c
keita-c-saito <keita.c.saito@rakuten.com>
2020-02-04 14:08:38 +0900

    fixed miss commit

--- guild/db/schema.rb
+++ guild/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200129043749) do
+ActiveRecord::Schema.define(version: 20200130083613) do
+
+  create_table "logins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.bigint "user_id", null: false
+    t.string "email", limit: 45, null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "email_UNIQUE", unique: true
+    t.index ["user_id"], name: "user_id_UNIQUE", unique: true
+  end
 
   create_table "maintenances", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.datetime "start_at", null: false
@@ -19,7 +29,7 @@ ActiveRecord::Schema.define(version: 20200129043749) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tasks", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.bigint "user_id", null: false
     t.text "subject", null: false
     t.text "description", null: false
@@ -31,4 +41,12 @@ ActiveRecord::Schema.define(version: 20200129043749) do
     t.index ["user_id"], name: "fk_task_user1"
   end
 
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string "name", limit: 45, null: false
+    t.integer "authority", limit: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "logins", "users", name: "fk_logins_user", on_update: :cascade, on_delete: :cascade
 end
