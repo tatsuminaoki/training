621d2de92efb3032daf338271cc3aeb452cd7bc6
keita-c-saito <keita.c.saito@rakuten.com>
2020-02-10 18:01:08 +0900

    fixed redirect to login_top

--- guild/app/controllers/application_controller.rb
+++ guild/app/controllers/application_controller.rb
@@ -5,11 +5,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action -> { check_maintenance }
+  before_action :check_maintenance
+  before_action :session_timeout?
 
   def check_maintenance
     if controller_name != 'maintenance' && LogicMaintenance.doing?
       redirect_to controller: :maintenance, action: :index
     end
+  end
+
+  def session_timeout?
+    if controller_name != 'user' && action_name != 'login_top'
+        redirect_to controller: :user, action: :login_top if session[:me].blank?
+      end
   end
 end
