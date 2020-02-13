a6a45f09931cd494e12cc0178060947d39e55de1
Nagano <kazuyoshi.nagano@rakuten.com>
2020-02-13 10:44:12 +0900

    use i18n for selector

--- taskManager/app/models/task.rb
+++ taskManager/app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  enum status: { 'open': 1, 'in progress': 2, 'review': 3, 're:open': 4, 'done': 5 }
+  enum status: { 'open': 1, 'inprogress': 2, 'review': 3, 'reopen': 4, 'done': 5 }
   enum priority: { 'highest': 1, 'higher': 2, 'middle': 3, 'lower': 4, 'lowest': 5 }
 end
