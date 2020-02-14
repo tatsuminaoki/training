a210619d7dda1d3974078449b091ba51a435a0e1
Nagano <kazuyoshi.nagano@rakuten.com>
2020-02-14 17:31:37 +0900

    set constant variable for validation check

--- taskManager/app/models/task.rb
+++ taskManager/app/models/task.rb
@@ -2,12 +2,15 @@ class Task < ApplicationRecord
   enum status: { 'open': 1, 'inprogress': 2, 'review': 3, 'reopen': 4, 'done': 5 }
   enum priority: { 'highest': 1, 'higher': 2, 'middle': 3, 'lower': 4, 'lowest': 5 }
 
+  MAX_LENGTH_SUMMARY = 50
+  MAX_LENGTH_DESCRIPTION = 255
+
   validates :summary,
     presence: true,
-    length: { maximum: 50 }
+    length: { maximum: MAX_LENGTH_SUMMARY }
 
   validates :description,
-    length: { maximum: 3000 }
+    length: { maximum: MAX_LENGTH_DESCRIPTION }
 
   validates :priority,
     presence: true
