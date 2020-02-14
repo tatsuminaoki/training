d5e7d50fb609ff4c120af572186a16376b6c71d8
Nagano <kazuyoshi.nagano@rakuten.com>
2020-02-14 16:52:56 +0900

    add task validation

--- taskManager/app/models/task.rb
+++ taskManager/app/models/task.rb
@@ -1,4 +1,18 @@
 class Task < ApplicationRecord
   enum status: { 'open': 1, 'inprogress': 2, 'review': 3, 'reopen': 4, 'done': 5 }
   enum priority: { 'highest': 1, 'higher': 2, 'middle': 3, 'lower': 4, 'lowest': 5 }
+
+  validates :summary,
+    presence: true,
+    length: { maximum: 50 }
+
+  validates :description,
+    length: { maximum: 3000 }
+
+  validates :priority,
+    presence: true
+
+  validates :status,
+    presence: true
+
 end
