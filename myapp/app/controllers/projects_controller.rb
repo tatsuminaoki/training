ff4717c3b26245c75acd1cf4908efeeaf37a3f8c
leesanghun6310 <leesanghun6310@gmail.com>
2020-02-20 12:41:39 +0900

    STEP7: プロジェクトテーブルのCRUD作成

---
+++ myapp/app/controllers/projects_controller.rb
@@ -0,0 +1,17 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def delete
+  end
+end
