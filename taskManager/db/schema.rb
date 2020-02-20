abd213cd53d3045b94c85d4ff1b5274148e0ea63
Nagano <kazuyoshi.nagano@rakuten.com>
2020-02-21 08:24:40 +0900

    add index to status column.

--- taskManager/db/schema.rb
+++ taskManager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_075737) do
+ActiveRecord::Schema.define(version: 2020_02_20_105448) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "summary", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_075737) do
     t.integer "assignee_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
