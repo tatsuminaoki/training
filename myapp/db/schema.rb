2dd6ef6167d7b41ff292d82aa48ae52f3a166398
leesanghun6310 <leesanghun6310@gmail.com>
2020-02-21 00:53:56 +0900

    boostrapとfontawesome導入・projectのindexファイル作成

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_012442) do
+ActiveRecord::Schema.define(version: 2020_02_20_061320) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2020_02_20_012442) do
     t.index ["name"], name: "index_projects_on_name"
   end
 
+  create_table "task_labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.bigint "label_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["label_id"], name: "index_task_labels_on_label_id"
+    t.index ["task_id"], name: "index_task_labels_on_task_id"
+  end
+
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "priority", null: false
@@ -53,6 +62,8 @@ ActiveRecord::Schema.define(version: 2020_02_20_012442) do
 
   add_foreign_key "events", "projects"
   add_foreign_key "labels", "projects"
+  add_foreign_key "task_labels", "labels"
+  add_foreign_key "task_labels", "tasks"
   add_foreign_key "tasks", "events"
   add_foreign_key "tasks", "labels"
 end
