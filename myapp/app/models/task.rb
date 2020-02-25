a70f38f62425688b3eea7b8a61eb4484c5b7c6b5
leesanghun6310 <leesanghun6310@gmail.com>
2020-02-25 14:50:47 +0900

    eventsからgroupsにテーブル名前変更、taskのpriorityのデータ型をintに変更し、enumの実装をモデルに追加

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,2 +1,3 @@
 class Task < ApplicationRecord
+  enum priority: { HIGH: 0, MIDDLE: 1, LOW: 2 }
 end
--- myapp/db/migrate/20200219154848_create_tasks.rb
+++ myapp/db/migrate/20200219154848_create_tasks.rb
@@ -2,12 +2,12 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :name, null: false
-      t.string :priority, null: false
+      t.integer :priority, null: false
       t.datetime :end_period_at, null: true
       t.string :creator_name, null: true
       t.string :assignee_name, null: true
       t.text :description, null: true
-      t.references :event, foreign_key: true, null: false
+      t.references :group, foreign_key: true, null: false
       t.references :label, foreign_key: true
       t.timestamps
     end
