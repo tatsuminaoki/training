629a18d7c0d01d3bf80d0c378e9e66a40d70eeae
leesanghun6310 <leesanghun6310@gmail.com>
2020-02-25 13:45:31 +0900

    projectを作成・変更・削除の実装

--- myapp/app/controllers/projects_controller.rb
+++ myapp/app/controllers/projects_controller.rb
@@ -4,14 +4,23 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    project = Project.new(create_params)
+    redirect_to controller: 'projects', action: 'show', id: project.id
   end
 
   def update
   end
 
   def show
+    @projects = Project.all
   end
 
   def delete
+  end
+
+  private
+
+  def create_params
+    params.permit(:name)
   end
 end
