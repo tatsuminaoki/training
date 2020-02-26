809c6aef019d17d1234edb1f5a7501a888ca07fd
Nagano <kazuyoshi.nagano@rakuten.com>
2020-02-26 17:12:04 +0900

    add user seed

--- taskManager/db/seeds.rb
+++ taskManager/db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#coding: utf-8
+User.create!(
+  [
+    {
+      email:        'kazuyoshi.nagano@rakuten.com',
+      first_name:   '千義',
+      last_name:    '長野',
+      password:     'kazuyoshi123',
+      role:         0,
+      invalid_flg:  0,
+    },
+    {
+      email:        'ryo.hasebe@rakuten.com',
+      first_name:   '亮',
+      last_name:    '長谷部',
+      password:     'ryo123',
+      role:         0,
+      invalid_flg:  0,
+    },
+    {
+      email:        'admin@example.com',
+      first_name:   '管理者',
+      last_name:    'タスクマネージャ',
+      password:     'admin123',
+      role:         1,
+      invalid_flg:  0,
+    }
+  ]
+)
