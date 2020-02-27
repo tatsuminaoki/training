68eb6810fc7c9ce34d635e1c5375cc9a066e8974
leesanghun6310 <leesanghun6310@gmail.com>
2020-02-28 08:38:13 +0900

    task-crud作成

--- myapp/app/models/group.rb
+++ myapp/app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
+  has_many :tasks, dependent: :destroy
   belongs_to :project
 
   validates :name, presence: true
--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
+  belongs_to :group
+
   enum priority: { high: 0, middle: 1, low: 2 }
 end
