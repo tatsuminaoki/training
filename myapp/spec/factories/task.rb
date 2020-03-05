b9c059ceea1e0c09a3ce68d00c0c654453c45b41
leesanghun6310 <leesanghun6310@gmail.com>
2020-03-05 18:39:05 +0900

    test作成

--- myapp/spec/factories/task.rb
+++ myapp/spec/factories/task.rb
@@ -3,5 +3,8 @@ FactoryBot.define do
     name { 'test' }
     description { 'test' }
     priority { :high }
+    end_period_at { Time.current + 2.days }
+    creator_name { 'tester' }
+    assignee_name { 'maganeer' }
   end
 end
