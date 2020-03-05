7b37193eb38cc1d31fd2c7b2f1989def29420dbd
leesanghun6310 <leesanghun6310@gmail.com>
2020-03-05 14:25:14 +0900

    error_handle_conecer追加

---
+++ myapp/app/controllers/concerns/error_handle.rb
@@ -0,0 +1,16 @@
+module ErrorHandle
+  extend ActiveSupport::Concern
+  included do
+    rescue_from Exception, with: :render_500
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+
+    def render_500
+      render 'errors/error_500', status: :internal_server_error
+    end
+
+    def render_404
+      render 'errors/error_404', status: :not_found
+    end
+  end
+end
