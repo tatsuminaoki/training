263b3823798b04550c5eed77a0f94866caf3c664
Nagano <kazuyoshi.nagano@rakuten.com>
2020-03-05 15:35:09 +0900

    fix admin user controller

--- taskManager/app/controllers/admin/users_controller.rb
+++ taskManager/app/controllers/admin/users_controller.rb
@@ -20,36 +20,32 @@ module Admin
 
     def create
       @admin_user = Admin::User.new(admin_user_params)
-
-      respond_to do |format|
-        if @admin_user.save
-          format.html { redirect_to @admin_user, notice: 'User was successfully created.' }
-          format.json { render :show, status: :created, location: @admin_user }
-        else
-          format.html { render :new }
-          format.json { render json: @admin_user.errors, status: :unprocessable_entity }
-        end
+      if @admin_user.save
+        flash[:success] = t('flash.create.success')
+        redirect_to @admin_user
+      else
+        flash.now[:danger] = t('flash.create.danger')
+        render :new
       end
     end
 
     def update
-      respond_to do |format|
-        if @admin_user.update(admin_user_params)
-          format.html { redirect_to @admin_user, notice: 'User was successfully updated.' }
-          format.json { render :show, status: :ok, location: @admin_user }
-        else
-          format.html { render :edit }
-          format.json { render json: @admin_user.errors, status: :unprocessable_entity }
-        end
+      if @admin_user.update(admin_user_params)
+        flash[:success] = t('flash.update.success')
+        redirect_to @admin_user
+      else
+        flash.now[:danger] = t('flash.update.danger')
+        render :edit
       end
     end
 
     def destroy
-      @admin_user.destroy
-      respond_to do |format|
-        format.html { redirect_to admin_users_url, notice: 'User was successfully destroyed.' }
-        format.json { head :no_content }
+      if @admin_user.destory
+        flash[:success] = t('flash.remove.success')
+      else
+        flash[:danger] = t('flash.remove.danger')
       end
+      redirect_to admin_users_url
     end
 
     private
