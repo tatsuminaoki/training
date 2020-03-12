6d19aad073e515c09e5d93c5de602cb2c28b0312
shunshunsuke <shunsuke.oda@rakuten.com>
2020-03-12 11:30:44 +0900

    ・スペース追加（RubyMineのReformat codeオプションのチェックはずし忘れ）

--- pivote/app/models/task.rb
+++ pivote/app/models/task.rb
@@ -2,6 +2,6 @@
 
 class Task < ApplicationRecord
   # 間に差込で追加したいという要望に応えられるよう、値を10の倍数としている
-  enum priority: {high: 10, middle: 20, low: 30}
-  enum status: {waiting: 10, doing: 20, done: 30}
+  enum priority: { high: 10, middle: 20, low: 30 }
+  enum status: { waiting: 10, doing: 20, done: 30 }
 end
