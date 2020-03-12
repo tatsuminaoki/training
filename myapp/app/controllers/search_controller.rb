c544fbda698bfab561290a26e96930a04dcb5756
leesanghun6310 <leesanghun6310@gmail.com>
2020-03-12 11:04:53 +0900

    テスト作成

--- myapp/app/controllers/search_controller.rb
+++ myapp/app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    projects_list, tasks_list = Search.search(params[:query]) if params[:query].present?
+    projects_list, tasks_list = Search.find_by_nam(params[:query]) if params[:query].present?
     render status: 200, json: { projects: projects_list, tasks: tasks_list}
   end
 end
--- myapp/spec/models/search_spec.rb
+++ myapp/spec/models/search_spec.rb
@@ -1,50 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe Group, type: :model do
-  describe 'Validates' do
-    context 'Name is set correctly' do
-      it 'is validation pass' do
-        project = create(:project)
-        group = Group.new(name: 'test1 group', sort_number: 1, project: project)
-        expect(group.valid?).to eq true
+RSpec.describe Search, type: :model do
+  describe 'Method Search' do
+    context 'Data of project and task is exists' do
+      let(:project) { create(:project, :with_group, name: 'TEST1') }
+      context 'query is matching with data' do
+        it 'is get project and task' do
+          create(:task, group: project.groups.first, name: 'TEST1')
+          projects_list, tasks_List = Search.find_by_name('TEST1')
+          expect(projects_list.count).to eq 1
+          expect(tasks_List.count).to eq 1
+        end
+      end
+
+      context 'query is not matching with data' do
+        it 'could not get project and task' do
+          create(:task, group: project.groups.first, name: 'TEST1')
+          projects_list, tasks_List = Search.find_by_name('TEST2')
+          expect(projects_list.count).to eq 0
+          expect(tasks_List.count).to eq 0
+        end
       end
     end
 
-    context 'Name is set empty' do
-      it 'is validation no pass and it also show errors message' do
-        project = create(:project)
-        group = Group.new(name: 'test1 group', sort_number: 1, project: project)
-        group.valid?
-        expect(group.errors.messages[:name]).to eq []
+    context 'Just data of project is exists' do
+      it 'is get project' do
+        create(:project, :with_group, name: 'TEST1')
+        projects_list, tasks_List = Search.find_by_name('TEST1')
+        expect(projects_list.count).to eq 1
+        expect(tasks_List.count).to eq 0
       end
     end
 
-    context 'Sort_number' do
-      context 'Sort_number is set correctly' do
-        it 'is validation pass' do
-          project = create(:project)
-          group = Group.new(name: 'test1 group', sort_number: 1, project: project)
-          expect(group.valid?).to eq true
-        end
+    context 'Just matching data is task' do
+      let(:project) { create(:project, :with_group, name: 'TEST2') }
+      it 'is get task' do
+        create(:task, group: project.groups.first, name: 'TEST1')
+        projects_list, tasks_List = Search.find_by_name('TEST1')
+        expect(projects_list.count).to eq 0
+        expect(tasks_List.count).to eq 1
       end
+    end
 
-      context 'Sort_number is set empty' do
-        it 'is verify_end_period_at pass' do
-          project = create(:project)
-          group = Group.new(name: 'test1 group', sort_number: nil, project: project)
-          group.valid?
-          expect(group.errors.messages[:sort_number]).to eq ["can't be blank"]
-        end
-      end
-
-      context 'Same sort_number is already exists' do
-        it 'is got valid error' do
-          project = create(:project, :with_group)
-          group_1 = Group.new(name: 'test1 group', sort_number: 1, project: project)
-          group_2 = Group.new(name: 'test1 group', sort_number: 1, project: project)
-          group_2.valid?
-          expect(group_2.errors.messages[:sort_number]).to eq ["Already same sort_number"]
-        end
+    context 'both data of project and task is not exists' do
+      it 'could not get both data ' do
+        projects_list, tasks_List = Search.find_by_name('TEST1')
+        expect(projects_list.count).to eq 0
+        expect(tasks_List.count).to eq 0
       end
     end
   end
