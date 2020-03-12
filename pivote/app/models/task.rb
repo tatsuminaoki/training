055f917dc22404a6fac674fad4f0a4db848fe94f
shunshunsuke <shunsuke.oda@rakuten.com>
2020-03-12 11:23:21 +0900

    ・rubocopによるスペース不足指摘の解消 ・コメント追加

--- pivote/app/models/task.rb
+++ pivote/app/models/task.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  # 間に差込で追加したいという要望に応えられるよう、値を10の倍数としている
   enum priority: {high: 10, middle: 20, low: 30}
   enum status: {waiting: 10, doing: 20, done: 30}
 end
