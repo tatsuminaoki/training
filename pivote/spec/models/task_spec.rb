b289414b6c5f6483f1d41ff66a435616b13faa58
shunshunsuke <shunsuke.oda@rakuten.com>
2020-03-17 17:33:30 +0900

    ・taskモデルのspecを実装

---
+++ pivote/spec/models/task_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Task, type: :model do
+  describe 'バリデーション' do
+    describe '名称' do
+      let(:task) { FactoryBot.build(:task, title: title) }
+      subject { task }
+
+      context '30文字の場合' do
+        let(:title) { 'a' * 30 }
+        it { is_expected.to be_valid }
+      end
+
+      context '31文字の場合' do
+        let(:title) { 'a' * 31 }
+        it { is_expected.to be_invalid }
+      end
+    end
+
+    describe '説明' do
+      let(:task) { FactoryBot.build(:task, description: description) }
+      subject { task }
+
+      context '1000文字の場合' do
+        let(:description) { 'a' * 1000 }
+        it { is_expected.to be_valid }
+      end
+
+      context '1001文字の場合' do
+        let(:description) { 'a' * 1001 }
+        it { is_expected.to be_invalid }
+      end
+    end
+  end
+end
