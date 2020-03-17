844e1c228a84a22a880ec74554d9174416117474
leesanghun6310 <leesanghun6310@gmail.com>
2020-03-17 10:06:43 +0900

    home画面作成

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /en|ja/ do
-    root 'projects#index'
-    resources :projects
-    resources :tasks
+    root 'static#index'
+    resources :static, only: [:index]
+    resources :projects, only: [:show, :index, :create, :update, :destroy]
+    resources :tasks, only: [:create, :update, :destroy]
     resources :search, only: [:index]
   end
 end
