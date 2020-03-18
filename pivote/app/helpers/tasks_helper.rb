fe37ccb72f1525eb2120cb2f83aabc3d1d109a7a
shunshunsuke <shunsuke.oda@rakuten.com>
2020-03-18 17:31:03 +0900

    ・期限を設定できるようにした ・期限でソートできるようにした

--- pivote/app/helpers/tasks_helper.rb
+++ pivote/app/helpers/tasks_helper.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 module TasksHelper
+  # 表示するべきソート用アイコンを返す
+  # === 引数
+  # * self_column - このカラムにソートアイコンを表示する
+  # * sorted_column - 現在このカラムでソートされている
+  # * direction - 現在この順番でソートされている
+  def sort_icon(self_column, sorted_column, direction)
+    if direction && self_column == sorted_column.to_sym
+      if direction == 'asc'
+        '▲'
+      else
+        '▼'
+      end
+    else
+      ''
+    end
+  end
 end
--- pivote/app/models/task.rb
+++ pivote/app/models/task.rb
@@ -7,4 +7,8 @@ class Task < ApplicationRecord
   # 間に差込で追加したいという要望に応えられるよう、値を10の倍数としている
   enum priority: { high: 10, middle: 20, low: 30 }
   enum status: { waiting: 10, doing: 20, done: 30 }
+
+  def format_deadline
+    I18n.l self.deadline, format: :date if self.deadline
+  end
 end
