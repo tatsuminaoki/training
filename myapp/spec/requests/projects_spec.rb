14c79616167ea4aa6172c0ea0b158c9bff13ce14
leesanghun6310 <leesanghun6310@gmail.com>
2020-03-18 14:08:26 +0900

    テスト修正

--- myapp/spec/requests/projects_spec.rb
+++ myapp/spec/requests/projects_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Projects', type: :request do
   describe 'POST /projects/:id' do
     context 'Project creating is success' do
+      let!(:current_user) { create(:user) }
       it 'is creating 1 project and 4 Groups' do
         post projects_path, params: { project: {name: 'TEST1'} }
         expect(Project.count).to eq 1
@@ -24,7 +25,9 @@ RSpec.describe 'Projects', type: :request do
   end
 
   describe 'PATCH /projects/:id' do
+    let!(:current_user) { create(:user) }
     let(:project) { create(:project) }
+    let!(:user_project) { create(:user_project, user: current_user, project: project) }
     context 'Update success' do
       it 'is changing project name to test1' do
         patch project_path(locale: 'en', id: project.id), params: { project: {name: 'test1'} }
@@ -46,27 +49,16 @@ RSpec.describe 'Projects', type: :request do
   end
 
   describe 'DELETE /projects/:id' do
+    let!(:current_user) { create(:user) }
+    let(:project) { create(:project) }
+    let!(:user_project) { create(:user_project, user: current_user, project: project) }
     context 'Project deleting is success' do
       it 'is deleting project and reference groups' do
-        Project.new(name: 'test').create!
-        project = Project.first
-
         delete project_path(locale: 'en', id: project.id)
         expect(Project.count).to eq 0
         expect(Group.count).to eq 0
-        expect(flash[:alert]).to eq 'Closed test project'
+        expect(flash[:alert]).to eq 'Success to destroy project'
         expect(response.status).to eq 302
-      end
-    end
-
-    context 'Project deleting is failed, because project id is not correct' do
-      it 'is not delete project and reference groups' do
-        Project.new(name: 'test').create!
-
-        delete project_path(locale: 'en', id: 10000)
-        expect(Project.count).to eq 1
-        expect(Group.count).to eq 4
-        expect(response).to have_http_status(404)
       end
     end
   end
