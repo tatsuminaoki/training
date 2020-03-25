dca7a8e04e594bfe04604cbe09b97e21321fd0c4
shunshunsuke <shunsuke.oda@rakuten.com>
2020-03-25 17:47:18 +0900

    ・nilを省略 ・contextには条件のみ記載するように修正

--- pivote/spec/models/user_spec.rb
+++ pivote/spec/models/user_spec.rb
@@ -7,22 +7,22 @@ RSpec.describe User, type: :model do
     let(:user) { FactoryBot.build(:user, name: name) }
     subject { user }
 
-    context 'nilの場合は無効である' do
-      let(:name) { nil }
+    context 'nil' do
+      let(:name) {}
       it { is_expected.to be_invalid }
     end
 
-    context '0文字の場合は無効である' do
+    context '0文字' do
       let(:name) { '' }
       it { is_expected.to be_invalid }
     end
 
-    context '30文字の場合は有効である' do
+    context '30文字' do
       let(:name) { 'a' * 30 }
       it { is_expected.to be_valid }
     end
 
-    context '31文字の場合は無効である' do
+    context '31文字' do
       let(:name) { 'a' * 31 }
       it { is_expected.to be_invalid }
     end
@@ -32,22 +32,22 @@ RSpec.describe User, type: :model do
     let(:user) { FactoryBot.build(:user, email: email) }
     subject { user }
 
-    context 'nilの場合は無効である' do
-      let(:email) { nil }
+    context 'nil' do
+      let(:email) {}
       it { is_expected.to be_invalid }
     end
 
-    context '0文字の場合は無効である' do
+    context '0文字' do
       let(:email) { '' }
       it { is_expected.to be_invalid }
     end
 
-    context '存在する場合は有効である' do
+    context '値が存在する' do
       let(:email) { 'admin@example.com' }
       it { is_expected.to be_valid }
     end
 
-    context '重複したメールアドレスは無効である' do
+    context '重複したメールアドレス' do
       before do
         FactoryBot.create(:user, email: email)
       end
@@ -61,22 +61,22 @@ RSpec.describe User, type: :model do
     let(:user) { FactoryBot.build(:user, password: password) }
     subject { user }
 
-    context 'nilの場合は無効である' do
-      let(:password) { nil }
+    context 'nil' do
+      let(:password) {}
       it { is_expected.to be_invalid }
     end
 
-    context '0文字の場合は無効である' do
+    context '0文字' do
       let(:password) { '' }
       it { is_expected.to be_invalid }
     end
 
-    context '3文字の場合は無効である' do
+    context '3文字' do
       let(:password) { 'a' * 3 }
       it { is_expected.to be_invalid }
     end
 
-    context '4文字の場合は有効である' do
+    context '4文字' do
       let(:password) { 'a' * 4 }
       it { is_expected.to be_valid }
     end
