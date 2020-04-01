a3b36e802029fdf96368785002479424ad2d165e
shunshunsuke <shunsuke.oda@rakuten.com>
2020-04-01 10:26:49 +0900

    - 一般ユーザーのログイン処理と非ログイン時の処理をskip_beforeではなくdescribeで表現するように変更

--- pivote/spec/system/tasks_spec.rb
+++ pivote/spec/system/tasks_spec.rb
@@ -28,278 +28,278 @@ describe 'タスク管理機能', type: :system do
   let(:task5_i) { page.body.index(task5_title) }
   let(:task6_i) { page.body.index(task6_title) }
 
-  before do |example|
-    unless example.metadata[:skip_before]
+  describe 'ログイン前提の処理' do
+    before do
       visit login_path
       fill_in 'session_email', with: user_a.email
       fill_in 'session_password', with: user_a.password
       click_button 'ログイン'
     end
+
+    describe 'ソート機能' do
+      before do
+        tasks_1
+        visit tasks_path
+      end
+
+      context '何も指定していないとき' do
+        it '作成日時の降順で表示される' do
+          expect(task1_i).to be < task2_i
+          expect(task2_i).to be < task3_i
+        end
+      end
+
+      context '優先度のリンクを1回踏んだとき' do
+        it '優先度の降順で表示される' do
+          click_link '優先度'
+          sleep 0.1
+          expect(task1_i).to be < task2_i
+          expect(task2_i).to be < task3_i
+        end
+      end
+
+      context '優先度のリンクを2回踏んだとき' do
+        it '優先度の昇順で表示される' do
+          click_link '優先度'
+          click_link '優先度'
+          sleep 0.1
+          expect(task3_i).to be < task2_i
+          expect(task2_i).to be < task1_i
+        end
+      end
+
+      context '優先度のリンクを3回踏んだとき' do
+        it '作成日時の降順で表示される' do
+          click_link '優先度'
+          click_link '優先度'
+          click_link '優先度'
+          sleep 0.1
+          expect(task1_i).to be < task2_i
+          expect(task2_i).to be < task3_i
+        end
+      end
+
+      context 'ステータスのリンクを1回踏んだとき' do
+        it 'ステータスの降順で表示される' do
+          click_link 'ステータス'
+          sleep 0.1
+          expect(task1_i).to be < task2_i
+          expect(task2_i).to be < task3_i
+        end
+      end
+
+      context 'ステータスのリンクを2回踏んだとき' do
+        it 'ステータスの昇順で表示される' do
+          click_link 'ステータス'
+          click_link 'ステータス'
+          sleep 0.1
+          expect(task3_i).to be < task2_i
+          expect(task2_i).to be < task1_i
+        end
+      end
+
+      context 'ステータスのリンクを3回踏んだとき' do
+        it '作成日時の降順で表示される' do
+          click_link 'ステータス'
+          click_link 'ステータス'
+          click_link 'ステータス'
+          sleep 0.1
+          expect(task1_i).to be < task2_i
+          expect(task2_i).to be < task3_i
+        end
+      end
+
+      context '期限のリンクを1回踏んだとき' do
+        it '期限の降順で表示される' do
+          click_link '期限'
+          sleep 0.1
+          expect(task1_i).to be < task2_i
+          expect(task2_i).to be < task3_i
+        end
+      end
+
+      context '期限のリンクを2回踏んだとき' do
+        it '期限の昇順で表示される' do
+          click_link '期限'
+          click_link '期限'
+          sleep 0.1
+          expect(task3_i).to be < task2_i
+          expect(task2_i).to be < task1_i
+        end
+      end
+
+      context '期限のリンクを3回踏んだとき' do
+        it '作成日時の降順で表示される' do
+          click_link '期限'
+          click_link '期限'
+          click_link '期限'
+          sleep 0.1
+          expect(task1_i).to be < task2_i
+          expect(task2_i).to be < task3_i
+        end
+      end
+    end
+
+    describe '検索機能' do
+      before do
+        tasks_1
+        tasks_2
+        visit tasks_path
+      end
+
+      context '優先度で検索したとき' do
+        it '選択した優先度のタスクが表示される' do
+          select '高', from: 'search[priority]'
+          click_button '検索'
+          expect(page).to have_content task1_title
+          expect(page).to have_no_content task2_title
+          expect(page).to have_no_content task3_title
+        end
+      end
+
+      context 'ステータスで検索したとき' do
+        it '選択したステータスのタスクが表示される' do
+          select '着手', from: 'search[status]'
+          click_button '検索'
+          expect(page).to have_content task2_title
+          expect(page).to have_no_content task1_title
+          expect(page).to have_no_content task3_title
+        end
+      end
+
+      context '名称で検索したとき' do
+        it '選択した名称のタスクが表示される' do
+          fill_in 'search[title]', with: task3_title
+          click_button '検索'
+          expect(page).to have_content task3_title
+          expect(page).to have_no_content task1_title
+          expect(page).to have_no_content task2_title
+        end
+      end
+
+      context '優先度とステータスで検索したとき' do
+        it '選択した優先度とステータスのタスクが表示される' do
+          select '低', from: 'search[priority]'
+          select '未着手', from: 'search[status]'
+          click_button '検索'
+          expect(page).to have_content task6_title
+          expect(page).to have_no_content task3_title
+        end
+      end
+
+      context 'ステータスと名称で検索したとき' do
+        it '選択したステータスと名称のタスクが表示される' do
+          select '完了', from: 'search[status]'
+          fill_in 'search[title]', with: task4_title
+          click_button '検索'
+          expect(page).to have_content task4_title
+          expect(page).to have_no_content task3_title
+        end
+      end
+
+      context '優先度と名称で検索したとき' do
+        it '選択した優先度と名称のタスクが表示される' do
+          select '中', from: 'search[priority]'
+          fill_in 'search[title]', with: task5_title
+          click_button '検索'
+          expect(page).to have_content task5_title
+          expect(page).to have_no_content task2_title
+        end
+      end
+    end
+
+    describe 'ページネーション' do
+      before do
+        (1..100).each { |i|
+          FactoryBot.create(:task, title: "ページネーションタスク#{i}", created_at: i.days.ago, user: user_a)
+        }
+        visit tasks_path
+      end
+
+      context '「2」のリンクを踏んだとき' do
+        it '21番目のタスクが表示される' do
+          click_link '2'
+          expect(page).to have_content 'ページネーションタスク21'
+          expect(page).to have_no_content 'ページネーションタスク20'
+        end
+      end
+
+      context '「最後」のリンクを踏んだとき' do
+        it '100番目のタスクが表示される' do
+          click_link '最後 »'
+          expect(page).to have_content 'ページネーションタスク100'
+          expect(page).to have_no_content 'ページネーションタスク80'
+        end
+      end
+    end
+
+    describe '一覧表示' do
+      before do
+        user_b = FactoryBot.create(:user, name: 'Bさん', email: 'b@example.com')
+        FactoryBot.create(:task, title: 'Bさんのタスク', user: user_b)
+        visit tasks_path
+      end
+
+      it 'Bさんのタスクは表示されない' do
+        expect(page).to have_no_content 'Bさんのタスク'
+      end
+    end
+
+    describe '詳細表示' do
+      before do
+        visit task_path(single_task)
+      end
+
+      it '作成したタスクの詳細が表示される' do
+        expect(page).to have_content 'テストです'
+      end
+    end
+
+    describe '新規作成' do
+      before do
+        visit new_task_path
+        fill_in 'task_title', with: '新規タスク'
+        click_button '登録する'
+      end
+
+      it 'タスクが新規作成される' do
+        expect(page).to have_content '新規タスク'
+        expect(Task.count).to eq 1
+      end
+    end
+
+    describe '編集' do
+      before do
+        visit edit_task_path(single_task)
+        fill_in 'task_title', with: '編集した'
+        click_button '更新する'
+      end
+
+      it 'タスクが編集される' do
+        expect(page).to have_content '編集した'
+      end
+    end
+
+    describe '削除' do
+      before do
+        single_task
+        visit tasks_path
+        click_link '削除'
+        page.driver.browser.switch_to.alert.accept
+      end
+
+      it 'タスクが削除される' do
+        expect(page).to have_no_content single_task[:priority]
+        expect(Task.count).to eq 0
+      end
+    end
   end
 
-  context 'ログインしていない場合' do
-    it 'ログインページに戻される', :skip_before do
+  describe '非ログイン時の処理' do
+    it 'ログインページに戻される' do
       visit tasks_path
       expect(page).to have_content User.human_attribute_name(:password)
       expect(page).to have_no_content I18n.t('headline.task.index')
-    end
-  end
-
-  describe 'ソート機能' do
-    before do
-      tasks_1
-      visit tasks_path
-    end
-
-    context '何も指定していないとき' do
-      it '作成日時の降順で表示される' do
-        expect(task1_i).to be < task2_i
-        expect(task2_i).to be < task3_i
-      end
-    end
-
-    context '優先度のリンクを1回踏んだとき' do
-      it '優先度の降順で表示される' do
-        click_link '優先度'
-        sleep 0.1
-        expect(task1_i).to be < task2_i
-        expect(task2_i).to be < task3_i
-      end
-    end
-
-    context '優先度のリンクを2回踏んだとき' do
-      it '優先度の昇順で表示される' do
-        click_link '優先度'
-        click_link '優先度'
-        sleep 0.1
-        expect(task3_i).to be < task2_i
-        expect(task2_i).to be < task1_i
-      end
-    end
-
-    context '優先度のリンクを3回踏んだとき' do
-      it '作成日時の降順で表示される' do
-        click_link '優先度'
-        click_link '優先度'
-        click_link '優先度'
-        sleep 0.1
-        expect(task1_i).to be < task2_i
-        expect(task2_i).to be < task3_i
-      end
-    end
-
-    context 'ステータスのリンクを1回踏んだとき' do
-      it 'ステータスの降順で表示される' do
-        click_link 'ステータス'
-        sleep 0.1
-        expect(task1_i).to be < task2_i
-        expect(task2_i).to be < task3_i
-      end
-    end
-
-    context 'ステータスのリンクを2回踏んだとき' do
-      it 'ステータスの昇順で表示される' do
-        click_link 'ステータス'
-        click_link 'ステータス'
-        sleep 0.1
-        expect(task3_i).to be < task2_i
-        expect(task2_i).to be < task1_i
-      end
-    end
-
-    context 'ステータスのリンクを3回踏んだとき' do
-      it '作成日時の降順で表示される' do
-        click_link 'ステータス'
-        click_link 'ステータス'
-        click_link 'ステータス'
-        sleep 0.1
-        expect(task1_i).to be < task2_i
-        expect(task2_i).to be < task3_i
-      end
-    end
-
-    context '期限のリンクを1回踏んだとき' do
-      it '期限の降順で表示される' do
-        click_link '期限'
-        sleep 0.1
-        expect(task1_i).to be < task2_i
-        expect(task2_i).to be < task3_i
-      end
-    end
-
-    context '期限のリンクを2回踏んだとき' do
-      it '期限の昇順で表示される' do
-        click_link '期限'
-        click_link '期限'
-        sleep 0.1
-        expect(task3_i).to be < task2_i
-        expect(task2_i).to be < task1_i
-      end
-    end
-
-    context '期限のリンクを3回踏んだとき' do
-      it '作成日時の降順で表示される' do
-        click_link '期限'
-        click_link '期限'
-        click_link '期限'
-        sleep 0.1
-        expect(task1_i).to be < task2_i
-        expect(task2_i).to be < task3_i
-      end
-    end
-  end
-
-  describe '検索機能' do
-    before do
-      tasks_1
-      tasks_2
-      visit tasks_path
-    end
-
-    context '優先度で検索したとき' do
-      it '選択した優先度のタスクが表示される' do
-        select '高', from: 'search[priority]'
-        click_button '検索'
-        expect(page).to have_content task1_title
-        expect(page).to have_no_content task2_title
-        expect(page).to have_no_content task3_title
-      end
-    end
-
-    context 'ステータスで検索したとき' do
-      it '選択したステータスのタスクが表示される' do
-        select '着手', from: 'search[status]'
-        click_button '検索'
-        expect(page).to have_content task2_title
-        expect(page).to have_no_content task1_title
-        expect(page).to have_no_content task3_title
-      end
-    end
-
-    context '名称で検索したとき' do
-      it '選択した名称のタスクが表示される' do
-        fill_in 'search[title]', with: task3_title
-        click_button '検索'
-        expect(page).to have_content task3_title
-        expect(page).to have_no_content task1_title
-        expect(page).to have_no_content task2_title
-      end
-    end
-
-    context '優先度とステータスで検索したとき' do
-      it '選択した優先度とステータスのタスクが表示される' do
-        select '低', from: 'search[priority]'
-        select '未着手', from: 'search[status]'
-        click_button '検索'
-        expect(page).to have_content task6_title
-        expect(page).to have_no_content task3_title
-      end
-    end
-
-    context 'ステータスと名称で検索したとき' do
-      it '選択したステータスと名称のタスクが表示される' do
-        select '完了', from: 'search[status]'
-        fill_in 'search[title]', with: task4_title
-        click_button '検索'
-        expect(page).to have_content task4_title
-        expect(page).to have_no_content task3_title
-      end
-    end
-
-    context '優先度と名称で検索したとき' do
-      it '選択した優先度と名称のタスクが表示される' do
-        select '中', from: 'search[priority]'
-        fill_in 'search[title]', with: task5_title
-        click_button '検索'
-        expect(page).to have_content task5_title
-        expect(page).to have_no_content task2_title
-      end
-    end
-  end
-
-  describe 'ページネーション' do
-    before do
-      (1..100).each { |i|
-        FactoryBot.create(:task, title: "ページネーションタスク#{i}", created_at: i.days.ago, user: user_a)
-      }
-      visit tasks_path
-    end
-
-    context '「2」のリンクを踏んだとき' do
-      it '21番目のタスクが表示される' do
-        click_link '2'
-        expect(page).to have_content 'ページネーションタスク21'
-        expect(page).to have_no_content 'ページネーションタスク20'
-      end
-    end
-
-    context '「最後」のリンクを踏んだとき' do
-      it '100番目のタスクが表示される' do
-        click_link '最後 »'
-        expect(page).to have_content 'ページネーションタスク100'
-        expect(page).to have_no_content 'ページネーションタスク80'
-      end
-    end
-  end
-
-  describe '一覧表示' do
-    before do
-      user_b = FactoryBot.create(:user, name: 'Bさん', email: 'b@example.com')
-      FactoryBot.create(:task, title: 'Bさんのタスク', user: user_b)
-      visit tasks_path
-    end
-
-    it 'Bさんのタスクは表示されない' do
-      expect(page).to have_no_content 'Bさんのタスク'
-    end
-  end
-
-  describe '詳細表示' do
-    before do
-      visit task_path(single_task)
-    end
-
-    it '作成したタスクの詳細が表示される' do
-      expect(page).to have_content 'テストです'
-    end
-  end
-
-  describe '新規作成' do
-    before do
-      visit new_task_path
-      fill_in 'task_title', with: '新規タスク'
-      click_button '登録する'
-    end
-
-    it 'タスクが新規作成される' do
-      expect(page).to have_content '新規タスク'
-      expect(Task.count).to eq 1
-    end
-  end
-
-  describe '編集' do
-    before do
-      visit edit_task_path(single_task)
-      fill_in 'task_title', with: '編集した'
-      click_button '更新する'
-    end
-
-    it 'タスクが編集される' do
-      expect(page).to have_content '編集した'
-    end
-  end
-
-  describe '削除' do
-    before do
-      single_task
-      visit tasks_path
-      click_link '削除'
-      page.driver.browser.switch_to.alert.accept
-    end
-
-    it 'タスクが削除される' do
-      expect(page).to have_no_content single_task[:priority]
-      expect(Task.count).to eq 0
     end
   end
 end
