60c870286fb573c2724c10a79cc567b79215ef37
fmot <fumiya.otake@rakuten.com>
2020-05-01 13:10:50 +0900

    refactor

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
-  require 'pry'
   include ErrorHandlers
 end
