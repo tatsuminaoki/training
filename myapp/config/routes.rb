a049aead50304af5e6aad792a04484bf3a1de8eb
fmot <fumiya.otake@rakuten.com>
2020-05-01 13:22:46 +0900

    fix routes

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks
   root to: 'tasks#index'
-  get '*anything' => 'errors#routing_error' 
+  get '*anything', to: 'errors#routing_error' 
 end
